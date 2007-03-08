47e4864314b2fa9bc537568913f98e0a8d892960
Jean-Philippe Lang <jp_lang@yahoo.fr>
2007-03-08 18:46:40 +0000

    added migrations for wiki
    git-svn-id: http://redmine.rubyforge.org/svn/branches/work@314 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- wiki/db/migrate/027_create_wiki_pages.rb
+++ wiki/db/migrate/027_create_wiki_pages.rb
@@ -1,7 +1,11 @@
 class CreateWikiPages < ActiveRecord::Migration
   def self.up
     create_table :wiki_pages do |t|
+      t.column :wiki_id, :integer, :null => false
+      t.column :title, :string, :limit => 255, :null => false
+      t.column :created_on, :datetime, :null => false    
     end
+    add_index :wiki_pages, [:wiki_id, :title], :name => :wiki_pages_wiki_id_title
   end
 
   def self.down
