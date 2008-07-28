d6808130dcfe044f78d1b8b08ae1fcd9a27c6660
Eric Davis <edavis@littlestreamsoftware.com>
2008-07-28 16:26:46 -0700

    Added test stubs for testing the Plugin API

---
+++ test/unit/lib/redmine/plugin_hook_test.rb
@@ -0,0 +1,13 @@
+require File.dirname(__FILE__) + '/../../../test_helper'
+
+class Redmine::Plugin::Hook::ManagerTest < Test::Unit::TestCase
+  def test_sanity
+    assert true
+  end
+end
+
+class Redmine::Plugin::Hook::BaseTest < Test::Unit::TestCase
+  def test_sanity
+    assert true
+  end
+end
---
+++ test/unit/lib/redmine/plugin_test.rb
@@ -0,0 +1,8 @@
+require File.dirname(__FILE__) + '/../../../test_helper'
+
+class Redmine::PluginTest < Test::Unit::TestCase
+  def test_sanity
+    assert true
+  end
+end
+
