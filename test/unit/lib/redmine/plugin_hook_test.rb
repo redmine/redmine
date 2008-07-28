e7309d8c5730f49bfc681eef6ebf3d1b961fba32
Eric Davis <edavis@littlestreamsoftware.com>
2008-07-28 16:40:18 -0700

    Added tests for Redmine::Plugin::Hook::Base

--- test/unit/lib/redmine/plugin_hook_test.rb
+++ test/unit/lib/redmine/plugin_hook_test.rb
@@ -10,4 +10,21 @@ class Redmine::Plugin::Hook::BaseTest < Test::Unit::TestCase
   def test_sanity
     assert true
   end
+  
+  def test_help_should_be_a_singleton
+    assert Redmine::Plugin::Hook::Base::Helper.include?(Singleton)
+  end
+  
+  def test_helper_should_include_actionview_helpers
+    [ActionView::Helpers::TagHelper,
+     ActionView::Helpers::FormHelper,
+     ActionView::Helpers::FormTagHelper,
+     ActionView::Helpers::FormOptionsHelper,
+     ActionView::Helpers::JavaScriptHelper, 
+     ActionView::Helpers::PrototypeHelper,
+     ActionView::Helpers::NumberHelper,
+     ActionView::Helpers::UrlHelper].each do |helper|
+      assert Redmine::Plugin::Hook::Base::Helper.include?(helper), "#{helper} wasn't included."
+    end
+  end
 end
