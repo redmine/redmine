99ea01ec936aaa9bfc131435fad1834900547f58
Eric Davis <edavis@littlestreamsoftware.com>
2008-07-28 17:30:32 -0700

    Added test coverage for Redmine::Plugin.add_hook

--- test/unit/lib/redmine/plugin_test.rb
+++ test/unit/lib/redmine/plugin_test.rb
@@ -4,5 +4,17 @@ class Redmine::PluginTest < Test::Unit::TestCase
   def test_sanity
     assert true
   end
+  
+  def test_add_hook
+    assert_equal false, Redmine::Plugin::Hook::Manager.hook_registered?(:issue_show)
+    Redmine::Plugin.add_hook(:issue_show, Proc.new { })
+    assert Redmine::Plugin::Hook::Manager.hook_registered?(:issue_show)
+  end
+  
+  def test_add_hook_invalid
+    assert_equal false, Redmine::Plugin::Hook::Manager.hook_registered?(:invalid)
+    Redmine::Plugin.add_hook(:invalid, Proc.new { })
+    assert Redmine::Plugin::Hook::Manager.hook_registered?(:invalid)
+  end
 end
 
