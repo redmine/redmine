dfe3c5b9788f8110246ed4516e1ee1de0fe6161a
Jean-Philippe Lang <jp_lang@yahoo.fr>
2009-01-04 18:17:54 +0000

    Branches cleaning.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/branches/work@2240 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- time/db/migrate/023_add_tracker_is_in_roadmap.rb
+++
@@ -1,9 +0,0 @@
-class AddTrackerIsInRoadmap < ActiveRecord::Migration
-  def self.up
-    add_column :trackers, :is_in_roadmap, :boolean, :default => true, :null => false
-  end
-
-  def self.down
-    remove_column :trackers, :is_in_roadmap
-  end
-end
