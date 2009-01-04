d4c761748bee3b6ceb0593fc1aedc756fb88ef51
Jean-Philippe Lang <jp_lang@yahoo.fr>
2009-01-04 18:17:21 +0000

    Branches cleaning.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/branches/work@2238 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- wiki/db/migrate/009_add_hide_mail_pref.rb
+++
@@ -1,9 +0,0 @@
-class AddHideMailPref < ActiveRecord::Migration
-  def self.up
-    add_column :user_preferences, :hide_mail, :boolean, :default => false
-  end
-
-  def self.down
-    remove_column :user_preferences, :hide_mail
-  end
-end
