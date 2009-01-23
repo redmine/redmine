f1a123add60257a3e6506761dd20885592956312
Jean-Philippe Lang <jp_lang@yahoo.fr>
2009-01-23 18:33:19 +0000

    Slight change to the project combo for adding membership on user administration screen. All projects are listed, and the ones that the user belongs to are disabled.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/branches/work@2301 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- nested_projects/app/helpers/users_helper.rb
+++ nested_projects/app/helpers/users_helper.rb
@@ -25,9 +25,11 @@ module UsersHelper
   end
   
   # Options for the new membership projects combo-box
-  def projects_options_for_select(projects)
+  def options_for_membership_project_select(user, projects)
     options = content_tag('option', "--- #{l(:actionview_instancetag_blank_option)} ---")
-    options << project_tree_options_for_select(projects)
+    options << project_tree_options_for_select(projects) do |p|
+      {:disabled => (user.projects.include?(p))}
+    end
     options
   end
   
