b366375e7ee1e2d76ec3c257e45cb793ae30a6a9
Jean-Philippe Lang <jp_lang@yahoo.fr>
2009-03-07 11:22:56 +0000

    Merged r2553 from trunk.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/branches/0.8-stable@2557 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/mail_handler.rb
+++ app/models/mail_handler.rb
@@ -40,7 +40,7 @@ class MailHandler < ActionMailer::Base
   # Processes incoming emails
   def receive(email)
     @email = email
-    @user = User.active.find(:first, :conditions => ["LOWER(mail) = ?", email.from.first.to_s.strip.downcase])
+    @user = User.active.find(:first, :conditions => ["LOWER(mail) = ?", email.from.to_a.first.to_s.strip.downcase])
     unless @user
       # Unknown user => the email is ignored
       # TODO: ability to create the user's account
