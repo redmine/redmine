f60881518a7a1b12e0c604d1ae1ba3c0fc99944f
Eric Davis <edavis@littlestreamsoftware.com>
2010-01-14 23:33:48 +0000

    Merged r3315 from trunk.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/branches/0.9-stable@3316 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -202,7 +202,7 @@ class IssuesController < ApplicationController
           flash[:notice] = l(:notice_successful_update)
         end
         call_hook(:controller_issues_edit_after_save, { :params => params, :issue => @issue, :time_entry => @time_entry, :journal => journal})
-        redirect_to(params[:back_to] || {:action => 'show', :id => @issue})
+        redirect_back_or_default({:action => 'show', :id => @issue})
       end
     end
   rescue ActiveRecord::StaleObjectError
@@ -269,7 +269,7 @@ class IssuesController < ApplicationController
                                                          :total => @issues.size,
                                                          :ids => '#' + unsaved_issue_ids.join(', #'))
       end
-      redirect_to(params[:back_to] || {:controller => 'issues', :action => 'index', :project_id => @project})
+      redirect_back_or_default({:controller => 'issues', :action => 'index', :project_id => @project})
       return
     end
     @available_statuses = Workflow.available_statuses(@project)
