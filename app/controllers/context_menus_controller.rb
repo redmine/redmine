ab31a114fd9a972e0d850c3438abaf031ebf84cd
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-04-04 11:53:03 +0000

    add controller for bulk edit time entries (#7996).
    Contributed by Adam Soltys.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@5312 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/context_menus_controller.rb
+++ app/controllers/context_menus_controller.rb
@@ -40,5 +40,18 @@ class ContextMenusController < ApplicationController
     
     render :layout => false
   end
-  
+
+  def time_entries
+    @time_entries = TimeEntry.all(
+       :conditions => {:id => params[:ids]}, :include => :project)
+    @projects = @time_entries.collect(&:project).compact.uniq
+    @project = @projects.first if @projects.size == 1
+    @activities = TimeEntryActivity.shared.active
+    @can = {:edit   => User.current.allowed_to?(:log_time, @projects),
+            :update => User.current.allowed_to?(:log_time, @projects),
+            :delete => User.current.allowed_to?(:log_time, @projects)
+            }
+    @back = back_url
+    render :layout => false
+  end  
 end
