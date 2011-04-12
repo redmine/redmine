6968d0da1934b83dc524f6c972b897c4f8b01244
Jean-Philippe Lang <jp_lang@yahoo.fr>
2011-04-12 19:50:48 +0000

    Fixed no method error due to typo.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@5446 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -105,7 +105,7 @@ class IssuesController < ApplicationController
   def show
     @journals = @issue.journals.find(:all, :include => [:user, :details], :order => "#{Journal.table_name}.created_on ASC")
     @journals.each_with_index {|j,i| j.indice = i+1}
-    @journals.reverse! if User.current.wants_comments_in_reverse_order
+    @journals.reverse! if User.current.wants_comments_in_reverse_order?
     
     if User.current.allowed_to?(:view_changesets, @project)
       @changesets = @issue.changesets.visible.all
