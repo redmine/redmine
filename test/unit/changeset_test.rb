d7fccb0d8e489bf68ee57c1ad8492e983292b277
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-04-15 14:22:51 +0000

    scm: add test of empty committer at unit changeset test.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@5472 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/changeset_test.rb
+++ test/unit/changeset_test.rb
@@ -346,15 +346,18 @@ class ChangesetTest < ActiveSupport::TestCase
             :project => proj, :url => '/tmp/test/bazaar',
             :log_encoding => 'ISO-8859-1' )
       assert r
-      c = Changeset.new(:repository => r,
+      c = Changeset.new(:repository   => r,
                         :committed_on => Time.now,
-                        :revision => '123',
-                        :scmid => '12345',
-                        :comments => "")
+                        :revision     => '123',
+                        :scmid        => '12345',
+                        :comments     => "",
+                        :committer    => "")
       assert( c.save )
       assert_equal "", c.comments
+      assert_equal "", c.committer
       if c.comments.respond_to?(:force_encoding)
         assert_equal "UTF-8", c.comments.encoding.to_s
+        assert_equal "UTF-8", c.committer.encoding.to_s
       end
   end
 
