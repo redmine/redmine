c33be450e7181fdd47c61572472a8c2a68a587f8
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-05-06 01:11:50 +0000

    scm: code clean up unit repository test.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@5658 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/repository_test.rb
+++ test/unit/repository_test.rb
@@ -1,16 +1,16 @@
-# redMine - project management software
-# Copyright (C) 2006-2007  Jean-Philippe Lang
+# Redmine - project management software
+# Copyright (C) 2006-2011  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -41,7 +41,7 @@ class RepositoryTest < ActiveSupport::TestCase
   def test_create
     repository = Repository::Subversion.new(:project => Project.find(3))
     assert !repository.save
-  
+
     repository.url = "svn://localhost"
     assert repository.save
     repository.reload
@@ -164,22 +164,22 @@ class RepositoryTest < ActiveSupport::TestCase
 
   def test_auto_user_mapping_by_username
     c = Changeset.create!(
-          :repository => @repository,
-          :committer => 'jsmith',
+          :repository   => @repository,
+          :committer    => 'jsmith',
           :committed_on => Time.now,
-          :revision => 100,
-          :comments => 'Committed by john.'
+          :revision     => 100,
+          :comments     => 'Committed by john.'
         )
     assert_equal User.find(2), c.user
   end
 
   def test_auto_user_mapping_by_email
     c = Changeset.create!(
-          :repository => @repository,
-          :committer => 'john <jsmith@somenet.foo>',
+          :repository   => @repository,
+          :committer    => 'john <jsmith@somenet.foo>',
           :committed_on => Time.now,
-          :revision => 100,
-          :comments => 'Committed by john.'
+          :revision     => 100,
+          :comments     => 'Committed by john.'
         )
     assert_equal User.find(2), c.user
   end
