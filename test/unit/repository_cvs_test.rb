12bad10573d1cf6b05225fb3dca329f04160c168
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-05-08 05:08:44 +0000

    scm: cvs: remove trailing white-spaces from unit model test.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@5702 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/repository_cvs_test.rb
+++ test/unit/repository_cvs_test.rb
@@ -1,16 +1,16 @@
-# redMine - project management software
-# Copyright (C) 2006-2007  Jean-Philippe Lang
+# Redmine - project management software
+# Copyright (C) 2006-2011  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -29,14 +29,14 @@ class RepositoryCvsTest < ActiveSupport::TestCase
 
   def setup
     @project = Project.find(3)
-    @repository = Repository::Cvs.create(:project => @project, 
+    @repository = Repository::Cvs.create(:project  => @project,
                                          :root_url => REPOSITORY_PATH,
-                                         :url => MODULE_NAME,
+                                         :url      => MODULE_NAME,
                                          :log_encoding => 'UTF-8')
     assert @repository
   end
 
-  if File.directory?(REPOSITORY_PATH)  
+  if File.directory?(REPOSITORY_PATH)
     def test_fetch_changesets_from_scratch
       assert_equal 0, @repository.changesets.count
       @repository.fetch_changesets
