3a5cf541280ceb9187ad7c943cc152cc46c7644e
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-05-08 02:49:41 +0000

    scm: git: remove trailing white-spaces from unit model test.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@5700 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/repository_git_test.rb
+++ test/unit/repository_git_test.rb
@@ -1,16 +1,16 @@
-# redMine - project management software
-# Copyright (C) 2006-2007  Jean-Philippe Lang
+# Redmine - project management software
+# Copyright (C) 2006-2011  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -40,7 +40,7 @@ class RepositoryGitTest < ActiveSupport::TestCase
       assert klass.scm_adapter_class
       assert_not_equal "", klass.scm_command
       assert_equal true, klass.scm_available
-  
+
       @project = Project.find(3)
       @repository = Repository::Git.create(
                         :project       => @project,
@@ -53,7 +53,7 @@ class RepositoryGitTest < ActiveSupport::TestCase
         @char_1.force_encoding('UTF-8')
       end
     end
-    
+
     def test_fetch_changesets_from_scratch
       @repository.fetch_changesets
       @repository.reload
@@ -75,7 +75,7 @@ class RepositoryGitTest < ActiveSupport::TestCase
       assert_equal "README", change.path
       assert_equal "A", change.action
     end
-    
+
     def test_fetch_changesets_incremental
       @repository.fetch_changesets
       # Remove the 3 latest changesets
