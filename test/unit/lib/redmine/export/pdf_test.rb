a5d528c6c2810d52a042571fdf8451538f8a26a5
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-05-09 06:44:59 +0000

    PDF: call Redmine::Export::PDF::RDMPdfEncoding::rdm_pdf_iconv() directly in unit test.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@5714 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/lib/redmine/export/pdf_test.rb
+++ test/unit/lib/redmine/export/pdf_test.rb
@@ -16,6 +16,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 require File.expand_path('../../../../../test_helper', __FILE__)
+require 'iconv'
 
 class PdfTest < ActiveSupport::TestCase
   include Redmine::I18n
@@ -23,23 +24,25 @@ class PdfTest < ActiveSupport::TestCase
   def test_fix_text_encoding_nil
     set_language_if_valid 'ja'
     assert_equal 'CP932', l(:general_pdf_encoding)
-    pdf = Redmine::Export::PDF::IFPDF.new('ja')
-    assert pdf
-    assert_equal '', pdf.fix_text_encoding(nil)
+    if RUBY_VERSION < '1.9'
+      ic = Iconv.new(l(:general_pdf_encoding), 'UTF-8')
+    end
+    assert_equal '', Redmine::Export::PDF::RDMPdfEncoding::rdm_pdf_iconv(ic, nil)
   end
 
-  def test_fix_text_encoding_cannot_convert_ja_cp932
+  def test_rdm_pdf_iconv_cannot_convert_ja_cp932
     set_language_if_valid 'ja'
     assert_equal 'CP932', l(:general_pdf_encoding)
-    pdf = Redmine::Export::PDF::IFPDF.new('ja')
-    assert pdf
+    if RUBY_VERSION < '1.9'
+      ic = Iconv.new(l(:general_pdf_encoding), 'UTF-8')
+    end
     utf8_txt_1  = "\xe7\x8b\x80\xe6\x85\x8b"
     utf8_txt_2  = "\xe7\x8b\x80\xe6\x85\x8b\xe7\x8b\x80"
     utf8_txt_3  = "\xe7\x8b\x80\xe7\x8b\x80\xe6\x85\x8b\xe7\x8b\x80"
     if utf8_txt_1.respond_to?(:force_encoding)
-      txt_1 = pdf.fix_text_encoding(utf8_txt_1)
-      txt_2 = pdf.fix_text_encoding(utf8_txt_2)
-      txt_3 = pdf.fix_text_encoding(utf8_txt_3)
+      txt_1 = Redmine::Export::PDF::RDMPdfEncoding::rdm_pdf_iconv(ic, utf8_txt_1)
+      txt_2 = Redmine::Export::PDF::RDMPdfEncoding::rdm_pdf_iconv(ic, utf8_txt_2)
+      txt_3 = Redmine::Export::PDF::RDMPdfEncoding::rdm_pdf_iconv(ic, utf8_txt_3)
       assert_equal "?\x91\xd4", txt_1
       assert_equal "?\x91\xd4?", txt_2
       assert_equal "??\x91\xd4?", txt_3
@@ -48,11 +51,11 @@ class PdfTest < ActiveSupport::TestCase
       assert_equal "ASCII-8BIT", txt_3.encoding.to_s
     else
       assert_equal "???\x91\xd4",
-                   pdf.fix_text_encoding(utf8_txt_1)
+                   Redmine::Export::PDF::RDMPdfEncoding::rdm_pdf_iconv(ic, utf8_txt_1)
       assert_equal "???\x91\xd4???",
-                   pdf.fix_text_encoding(utf8_txt_2)
+                   Redmine::Export::PDF::RDMPdfEncoding::rdm_pdf_iconv(ic, utf8_txt_2)
       assert_equal "??????\x91\xd4???",
-                   pdf.fix_text_encoding(utf8_txt_3)
+                   Redmine::Export::PDF::RDMPdfEncoding::rdm_pdf_iconv(ic, utf8_txt_3)
     end
   end
 end
