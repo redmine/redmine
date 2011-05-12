b5130b17a0d076821a205ad41ea4a592f4030710
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-05-12 07:41:08 +0000

    scm: filesystem: code clean up model.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@5754 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/repository/filesystem.rb
+++ app/models/repository/filesystem.rb
@@ -1,5 +1,5 @@
-# redMine - project management software
-# Copyright (C) 2006-2007  Jean-Philippe Lang
+# Redmine - project management software
+# Copyright (C) 2006-2011  Jean-Philippe Lang
 #
 # FileSystem adapter
 # File written by Paul Rivier, at Demotera.
@@ -8,12 +8,12 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -51,5 +51,4 @@ class Repository::Filesystem < Repository
   def fetch_changesets
     nil
   end
-  
 end
