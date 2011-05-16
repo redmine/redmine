d3a953450ec59cb06d668d1e0f46035e511aba1c
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-05-16 22:55:14 +0000

    remove trailing white-spaces from functional journals controller test.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@5800 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/journals_controller_test.rb
+++ test/functional/journals_controller_test.rb
@@ -5,12 +5,12 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -24,26 +24,26 @@ class JournalsController; def rescue_action(e) raise e end; end
 class JournalsControllerTest < ActionController::TestCase
   fixtures :projects, :users, :members, :member_roles, :roles, :issues, :journals, :journal_details, :enabled_modules,
     :trackers, :issue_statuses, :enumerations, :custom_fields, :custom_values, :custom_fields_projects
-  
+
   def setup
     @controller = JournalsController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
     User.current = nil
   end
-  
+
   def test_index
     get :index, :project_id => 1
     assert_response :success
     assert_not_nil assigns(:journals)
     assert_equal 'application/atom+xml', @response.content_type
   end
-  
+
   def test_diff
     get :diff, :id => 3, :detail_id => 4
     assert_response :success
     assert_template 'diff'
-    
+
     assert_tag 'span',
       :attributes => {:class => 'diff_out'},
       :content => /removed/
@@ -51,14 +51,14 @@ class JournalsControllerTest < ActionController::TestCase
       :attributes => {:class => 'diff_in'},
       :content => /added/
   end
-  
+
   def test_reply_to_issue
     @request.session[:user_id] = 2
     get :new, :id => 6
     assert_response :success
     assert_select_rjs :show, "update"
   end
-  
+
   def test_reply_to_issue_without_permission
     @request.session[:user_id] = 7
     get :new, :id => 6
@@ -81,7 +81,7 @@ class JournalsControllerTest < ActionController::TestCase
       assert_select 'textarea'
     end
   end
-  
+
   def test_post_edit
     @request.session[:user_id] = 1
     xhr :post, :edit, :id => 2, :notes => 'Updated notes'
@@ -89,7 +89,7 @@ class JournalsControllerTest < ActionController::TestCase
     assert_select_rjs :replace, 'journal-2-notes'
     assert_equal 'Updated notes', Journal.find(2).notes
   end
-  
+
   def test_post_edit_with_empty_notes
     @request.session[:user_id] = 1
     xhr :post, :edit, :id => 2, :notes => ''
