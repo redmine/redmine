596670d61512665f985dee3955f5a8fe96de9344
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-05-16 00:09:58 +0000

    remove trailing white-spaces from functional activities controller test.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@5796 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/activities_controller_test.rb
+++ test/functional/activities_controller_test.rb
@@ -8,8 +8,8 @@ class ActivitiesControllerTest < ActionController::TestCase
     assert_response :success
     assert_template 'index'
     assert_not_nil assigns(:events_by_day)
-    
-    assert_tag :tag => "h3", 
+
+    assert_tag :tag => "h3",
                :content => /#{2.days.ago.to_date.day}/,
                :sibling => { :tag => "dl",
                  :child => { :tag => "dt",
@@ -20,14 +20,14 @@ class ActivitiesControllerTest < ActionController::TestCase
                  }
                }
   end
-  
+
   def test_previous_project_index
     get :index, :id => 1, :from => 3.days.ago.to_date
     assert_response :success
     assert_template 'index'
     assert_not_nil assigns(:events_by_day)
-               
-    assert_tag :tag => "h3", 
+
+    assert_tag :tag => "h3",
                :content => /#{3.day.ago.to_date.day}/,
                :sibling => { :tag => "dl",
                  :child => { :tag => "dt",
@@ -38,14 +38,14 @@ class ActivitiesControllerTest < ActionController::TestCase
                  }
                }
   end
-  
+
   def test_global_index
     get :index
     assert_response :success
     assert_template 'index'
     assert_not_nil assigns(:events_by_day)
-    
-    assert_tag :tag => "h3", 
+
+    assert_tag :tag => "h3",
                :content => /#{5.day.ago.to_date.day}/,
                :sibling => { :tag => "dl",
                  :child => { :tag => "dt",
@@ -56,14 +56,14 @@ class ActivitiesControllerTest < ActionController::TestCase
                  }
                }
   end
-  
+
   def test_user_index
     get :index, :user_id => 2
     assert_response :success
     assert_template 'index'
     assert_not_nil assigns(:events_by_day)
-    
-    assert_tag :tag => "h3", 
+
+    assert_tag :tag => "h3",
                :content => /#{3.day.ago.to_date.day}/,
                :sibling => { :tag => "dl",
                  :child => { :tag => "dt",
@@ -74,7 +74,7 @@ class ActivitiesControllerTest < ActionController::TestCase
                  }
                }
   end
-  
+
   def test_index_atom_feed
     get :index, :format => 'atom'
     assert_response :success
@@ -83,5 +83,4 @@ class ActivitiesControllerTest < ActionController::TestCase
       :tag => 'link',
       :attributes => {:href => 'http://test.host/issues/11'}}
   end
-  
 end
