32cb715db9135a65d3ed2d42377bbc48b833f992
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-05-17 03:58:19 +0000

    remove trailing white-spaces from news model source.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@5819 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/news.rb
+++ app/models/news.rb
@@ -5,12 +5,12 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -19,7 +19,7 @@ class News < ActiveRecord::Base
   belongs_to :project
   belongs_to :author, :class_name => 'User', :foreign_key => 'author_id'
   has_many :comments, :as => :commented, :dependent => :delete_all, :order => "created_on"
-  
+
   validates_presence_of :title, :description
   validates_length_of :title, :maximum => 60
   validates_length_of :summary, :maximum => 255
@@ -29,25 +29,25 @@ class News < ActiveRecord::Base
   acts_as_activity_provider :find_options => {:include => [:project, :author]},
                             :author_key => :author_id
   acts_as_watchable
-  
+
   after_create :add_author_as_watcher
-  
-  named_scope :visible, lambda {|*args| { 
+
+  named_scope :visible, lambda {|*args| {
     :include => :project,
-    :conditions => Project.allowed_to_condition(args.shift || User.current, :view_news, *args) 
+    :conditions => Project.allowed_to_condition(args.shift || User.current, :view_news, *args)
   }}
-  
+
   def visible?(user=User.current)
     !user.nil? && user.allowed_to?(:view_news, project)
   end
-  
+
   # returns latest news for projects visible by user
   def self.latest(user = User.current, count = 5)
     find(:all, :limit => count, :conditions => Project.allowed_to_condition(user, :view_news), :include => [ :author, :project ], :order => "#{News.table_name}.created_on DESC")	
   end
-  
+
   private
-  
+
   def add_author_as_watcher
     Watcher.create(:watchable => self, :user => author)
   end
