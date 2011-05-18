7111bdd42646631f4ef2fa308e30420ad9183f1d
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-05-18 11:13:45 +0000

    remove trailing white-spaces from reports helper source.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@5850 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,22 +1,22 @@
-# redMine - project management software
-# Copyright (C) 2006  Jean-Philippe Lang
+# Redmine - project management software
+# Copyright (C) 2006-2011  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 module ReportsHelper
-  
+
   def aggregate(data, criteria)
     a = 0
     data.each { |row|
@@ -28,9 +28,9 @@ module ReportsHelper
     } unless data.nil?
     a
   end
-  
+
   def aggregate_link(data, criteria, *args)
     a = aggregate data, criteria
     a > 0 ? link_to(a, *args) : '-'
-  end  
+  end
 end
