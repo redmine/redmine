083b2864c7996df0615dc70b4bfa20353f40fee3
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-05-24 01:47:53 +0000

    scm: bazaar: remove trailing white-spaces from functional test.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@5884 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/repositories_bazaar_controller_test.rb
+++ test/functional/repositories_bazaar_controller_test.rb
@@ -1,16 +1,16 @@
-# redMine - project management software
-# Copyright (C) 2006-2008  Jean-Philippe Lang
+# Redmine - project management software
+# Copyright (C) 2006-2011  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -72,7 +72,7 @@ class RepositoriesBazaarControllerTest < ActionController::TestCase
       assert_equal 'file', entry.kind
       assert_equal 'directory/edit.png', entry.path
     end
-    
+
     def test_browse_at_given_revision
       get :show, :id => PRJ_ID, :path => [], :rev => 3
       assert_response :success
@@ -81,14 +81,14 @@ class RepositoriesBazaarControllerTest < ActionController::TestCase
       assert_equal ['directory', 'doc-deleted.txt', 'doc-ls.txt', 'doc-mkdir.txt'],
                    assigns(:entries).collect(&:name)
     end
-    
+
     def test_changes
       get :changes, :id => PRJ_ID, :path => ['doc-mkdir.txt']
       assert_response :success
       assert_template 'changes'
       assert_tag :tag => 'h2', :content => 'doc-mkdir.txt'
     end
-    
+
     def test_entry_show
       get :entry, :id => PRJ_ID, :path => ['directory', 'doc-ls.txt']
       assert_response :success
@@ -99,14 +99,14 @@ class RepositoriesBazaarControllerTest < ActionController::TestCase
                  :attributes => { :class => /line-num/ },
                  :sibling => { :tag => 'td', :content => /Show help message/ }
     end
-    
+
     def test_entry_download
       get :entry, :id => PRJ_ID, :path => ['directory', 'doc-ls.txt'], :format => 'raw'
       assert_response :success
       # File content
       assert @response.body.include?('Show help message')
     end
-  
+
     def test_directory_entry
       get :entry, :id => PRJ_ID, :path => ['directory']
       assert_response :success
@@ -123,11 +123,11 @@ class RepositoriesBazaarControllerTest < ActionController::TestCase
       # Line 11 removed
       assert_tag :tag => 'th',
                  :content => /11/,
-                 :sibling => { :tag => 'td', 
+                 :sibling => { :tag => 'td',
                                :attributes => { :class => /diff_out/ },
                                :content => /Display more information/ }
     end
-    
+
     def test_annotate
       get :annotate, :id => PRJ_ID, :path => ['doc-mkdir.txt']
       assert_response :success
