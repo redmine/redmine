22e143f0b397595eb5a58f1efc6b794f74a6be9c
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-06-10 02:15:47 +0000

    Merged r6035 from trunk.
    PDF: use DejaVuSans fonts in Vietnamese.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/branches/1.2-stable@6037 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/export/pdf.rb
+++ lib/redmine/export/pdf.rb
@@ -40,33 +40,39 @@ module Redmine
             @ic = Iconv.new(pdf_encoding, 'UTF-8')
           end
           super('P', 'mm', 'A4', (pdf_encoding == 'UTF-8'), pdf_encoding)
-          case pdf_encoding
-          when 'UTF-8'
-            @font_for_content = 'FreeSans'
-            @font_for_footer  = 'FreeSans'
-          when 'CP949'
-            extend(PDF_Korean)
-            AddUHCFont()
-            @font_for_content = 'UHC'
-            @font_for_footer  = 'UHC'
-          when 'CP932', 'SJIS', 'SHIFT_JIS'
-            extend(PDF_Japanese)
-            AddSJISFont()
-            @font_for_content = 'SJIS'
-            @font_for_footer  = 'SJIS'
-          when 'GB18030'
-            extend(PDF_Chinese)
-            AddGBFont()
-            @font_for_content = 'GB'
-            @font_for_footer  = 'GB'
-          when 'BIG5'
-            extend(PDF_Chinese)
-            AddBig5Font()
-            @font_for_content = 'Big5'
-            @font_for_footer  = 'Big5'
+          case current_language.to_s.downcase
+          when 'vi'
+            @font_for_content = 'DejaVuSans'
+            @font_for_footer  = 'DejaVuSans'
           else
-            @font_for_content = 'Arial'
-            @font_for_footer  = 'Helvetica'
+            case pdf_encoding
+            when 'UTF-8'
+              @font_for_content = 'FreeSans'
+              @font_for_footer  = 'FreeSans'
+            when 'CP949'
+              extend(PDF_Korean)
+              AddUHCFont()
+              @font_for_content = 'UHC'
+              @font_for_footer  = 'UHC'
+            when 'CP932', 'SJIS', 'SHIFT_JIS'
+              extend(PDF_Japanese)
+              AddSJISFont()
+              @font_for_content = 'SJIS'
+              @font_for_footer  = 'SJIS'
+            when 'GB18030'
+              extend(PDF_Chinese)
+              AddGBFont()
+              @font_for_content = 'GB'
+              @font_for_footer  = 'GB'
+            when 'BIG5'
+              extend(PDF_Chinese)
+              AddBig5Font()
+              @font_for_content = 'Big5'
+              @font_for_footer  = 'Big5'
+            else
+              @font_for_content = 'Arial'
+              @font_for_footer  = 'Helvetica'
+            end
           end
           SetCreator(Redmine::Info.app_name)
           SetFont(@font_for_content)
