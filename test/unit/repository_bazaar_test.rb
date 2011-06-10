685aab7fcb6081de123c5ded95a93eb7081ef178
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-06-10 04:57:42 +0000

    scm: bazaar: remove trailing white-spaces from unit model test.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6039 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/repository_bazaar_test.rb
+++ test/unit/repository_bazaar_test.rb
@@ -1,16 +1,16 @@
-# redMine - project management software
-# Copyright (C) 2006-2007  Jean-Philippe Lang
+# Redmine - project management software
+# Copyright (C) 2006-2011  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -33,11 +33,11 @@ class RepositoryBazaarTest < ActiveSupport::TestCase
     assert @repository
   end
 
-  if File.directory?(REPOSITORY_PATH)  
+  if File.directory?(REPOSITORY_PATH)
     def test_fetch_changesets_from_scratch
       @repository.fetch_changesets
       @repository.reload
-      
+
       assert_equal 4, @repository.changesets.count
       assert_equal 9, @repository.changes.count
       assert_equal 'Initial import', @repository.changesets.find_by_revision('1').comments
@@ -49,7 +49,7 @@ class RepositoryBazaarTest < ActiveSupport::TestCase
       @repository.changesets.find(:all).each {|c| c.destroy if c.revision.to_i > 2}
       @repository.reload
       assert_equal 2, @repository.changesets.count
-      
+
       @repository.fetch_changesets
       assert_equal 4, @repository.changesets.count
     end
@@ -57,14 +57,14 @@ class RepositoryBazaarTest < ActiveSupport::TestCase
     def test_entries
       entries = @repository.entries
       assert_equal 2, entries.size
-      
+
       assert_equal 'dir', entries[0].kind
       assert_equal 'directory', entries[0].name
-      
+
       assert_equal 'file', entries[1].kind
       assert_equal 'doc-mkdir.txt', entries[1].name
     end
-    
+
     def test_entries_in_subdirectory
       entries = @repository.entries('directory')
       assert_equal 3, entries.size
