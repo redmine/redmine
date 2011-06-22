b49487e9a1deaa9e76b6e74e57d8dcc38c629191
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-06-22 03:56:24 +0000

    use ActionView::TestCase instead of HelperTestCase at sort_helper_test.rb.
    ActionView::Helpers::ActiveRecordHelper at test/helper_testcase.rb
    does not exist on Rails 3.0.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6113 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/helpers/sort_helper_test.rb
+++ test/unit/helpers/sort_helper_test.rb
@@ -17,7 +17,7 @@
 
 require File.expand_path('../../../test_helper', __FILE__)
 
-class SortHelperTest < HelperTestCase
+class SortHelperTest < ActionView::TestCase
   include SortHelper
   
   def setup
