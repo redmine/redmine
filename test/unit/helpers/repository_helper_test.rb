fe566992f04125500ea71f4e8bc59f6527c55c4d
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-06-22 00:57:17 +0000

    scm: use ActionView::TestCase instead of HelperTestCase at repository_helper_test.rb.
    ActionView::Helpers::ActiveRecordHelper at test/helper_testcase.rb
    does not exist on Rails 3.0.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6107 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/helpers/repository_helper_test.rb
+++ test/unit/helpers/repository_helper_test.rb
@@ -5,19 +5,19 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 require File.expand_path('../../../test_helper', __FILE__)
 
-class RepositoryHelperTest < HelperTestCase
+class RepositoryHelperTest < ActionView::TestCase
   include RepositoriesHelper
 
   def test_from_latin1_to_utf8
