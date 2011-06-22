b0dfcca23045517b29f90adc8b8b7b1abe9a3154
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-06-22 07:54:18 +0000

    back out r6117. use ActionView::TestCase instead of HelperTestCase at IssuesHelperTest.
    Tests on CI server fail.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6119 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/helpers/issues_helper_test.rb
+++ test/unit/helpers/issues_helper_test.rb
@@ -17,7 +17,7 @@
 
 require File.expand_path('../../../test_helper', __FILE__)
 
-class IssuesHelperTest < ActionView::TestCase
+class IssuesHelperTest < HelperTestCase
   include ApplicationHelper
   include IssuesHelper
 
