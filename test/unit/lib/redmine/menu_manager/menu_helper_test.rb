2871d56045080a52dc6217a7cba5a2a1c239ffe1
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-06-22 04:10:43 +0000

    use ActionView::TestCase instead of HelperTestCase at test/unit/lib/redmine/menu_manager/menu_helper_test.rb.
    ActionView::Helpers::ActiveRecordHelper at test/helper_testcase.rb
    does not exist on Rails 3.0.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6118 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/lib/redmine/menu_manager/menu_helper_test.rb
+++ test/unit/lib/redmine/menu_manager/menu_helper_test.rb
@@ -17,9 +17,7 @@
 
 require File.expand_path('../../../../../test_helper', __FILE__)
 
-
-
-class Redmine::MenuManager::MenuHelperTest < HelperTestCase
+class Redmine::MenuManager::MenuHelperTest < ActionView::TestCase
   include Redmine::MenuManager::MenuHelper
   include ActionController::Assertions::SelectorAssertions
   fixtures :users, :members, :projects, :enabled_modules
