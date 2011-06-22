4bc0f9a04aa0af2dcf7bb677112a2ec9d1e01ab1
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-06-22 03:57:10 +0000

    use ActionView::TestCase instead of HelperTestCase at timelog_helper_test.rb.
    ActionView::Helpers::ActiveRecordHelper at test/helper_testcase.rb
    does not exist on Rails 3.0.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6114 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/helpers/timelog_helper_test.rb
+++ test/unit/helpers/timelog_helper_test.rb
@@ -17,7 +17,7 @@
 
 require File.expand_path('../../../test_helper', __FILE__)
 
-class TimelogHelperTest < HelperTestCase
+class TimelogHelperTest < ActionView::TestCase
   include TimelogHelper
   include ActionView::Helpers::TextHelper
   include ActionView::Helpers::DateHelper
