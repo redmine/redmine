0b404184d5aa5387511f11040eb51019e5a8a0b2
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-06-22 03:56:00 +0000

    use ActionView::TestCase instead of HelperTestCase at search_helper_test.rb.
    ActionView::Helpers::ActiveRecordHelper at test/helper_testcase.rb
    does not exist on Rails 3.0.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6112 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/helpers/search_helper_test.rb
+++ test/unit/helpers/search_helper_test.rb
@@ -19,7 +19,7 @@
 
 require File.expand_path('../../../test_helper', __FILE__)
 
-class SearchHelperTest < HelperTestCase
+class SearchHelperTest < ActionView::TestCase
   include SearchHelper
   
   def test_highlight_single_token
