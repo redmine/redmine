67cbda91ed0c97478e07b08b7acbe71846b730fd
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-07-10 23:12:08 +0000

    scm: bazaar: add functional test of destroying valid repository (#6713, #4725).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6235 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/repositories_bazaar_controller_test.rb
+++ test/functional/repositories_bazaar_controller_test.rb
@@ -146,6 +146,18 @@ class RepositoriesBazaarControllerTest < ActionController::TestCase
       assert_tag :tag => 'th', :content => '2',
                  :sibling => { :tag => 'td', :content => /Main purpose/ }
     end
+
+    def test_destroy_valid_repository
+      @request.session[:user_id] = 1 # admin
+      @repository.fetch_changesets
+      @repository.reload
+      assert @repository.changesets.count > 0
+
+      get :destroy, :id => PRJ_ID
+      assert_response 302
+      @project.reload
+      assert_nil @project.repository
+    end
   else
     puts "Bazaar test repository NOT FOUND. Skipping functional tests !!!"
     def test_fake; assert true end
