bbb6048f659825b3e7bf9baa03ec11425a5cdb34
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-19 09:41:12 +0000

    Ruby 1.9: set Encoding.default_external = 'UTF-8' (#4050).
    Without this setting, Redmine cannot boot on Japanese Windows
    nor on non UTF-8 locale Linux (e.g. LANG=en_US.ISO-8859-1).
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6461 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,10 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
+if RUBY_VERSION >= '1.9'
+  Encoding.default_external = 'UTF-8'
+end
+
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
