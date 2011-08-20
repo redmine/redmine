af20566bf48d887649371fb02430a029535ed725
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-20 06:51:36 +0000

    remove trailing white-spaces from lib/redmine/helpers/diff.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6470 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/helpers/diff.rb
+++ lib/redmine/helpers/diff.rb
@@ -5,12 +5,12 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -22,15 +22,15 @@ module Redmine
       include ActionView::Helpers::TagHelper
       include ActionView::Helpers::TextHelper
       attr_reader :diff, :words
-      
+
       def initialize(content_to, content_from)
         @words = content_to.to_s.split(/(\s+)/)
         @words = @words.select {|word| word != ' '}
         words_from = content_from.to_s.split(/(\s+)/)
-        words_from = words_from.select {|word| word != ' '}    
+        words_from = words_from.select {|word| word != ' '}
         @diff = words_from.diff @words
       end
-  
+
       def to_html
         words = self.words.collect{|word| h(word)}
         words_add = 0
@@ -41,7 +41,7 @@ module Redmine
           add_at = nil
           add_to = nil
           del_at = nil
-          deleted = ""      
+          deleted = ""
           diff.each do |change|
             pos = change[1]
             if change[0] == "+"
