2794019d64f3dc779794fb86f21eb87521511514
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-20 23:11:23 +0000

    Rails3: use String#html_safe for render_issue_tooltip() at IssuesHelper.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6495 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -46,13 +46,13 @@ module IssuesHelper
     @cached_label_priority ||= l(:field_priority)
     @cached_label_project ||= l(:field_project)
 
-    link_to_issue(issue) + "<br /><br />" +
+    (link_to_issue(issue) + "<br /><br />" +
       "<strong>#{@cached_label_project}</strong>: #{link_to_project(issue.project)}<br />" +
       "<strong>#{@cached_label_status}</strong>: #{h(issue.status.name)}<br />" +
       "<strong>#{@cached_label_start_date}</strong>: #{format_date(issue.start_date)}<br />" +
       "<strong>#{@cached_label_due_date}</strong>: #{format_date(issue.due_date)}<br />" +
       "<strong>#{@cached_label_assigned_to}</strong>: #{h(issue.assigned_to)}<br />" +
-      "<strong>#{@cached_label_priority}</strong>: #{h(issue.priority.name)}"
+      "<strong>#{@cached_label_priority}</strong>: #{h(issue.priority.name)}").html_safe
   end
 
   def issue_heading(issue)
