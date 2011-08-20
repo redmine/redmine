89fd138503ba5cc99028fcdeafcb264516a70438
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-20 06:54:00 +0000

    Rails3: use String#html_safe for label_for_field() at lib/tabular_form_builder.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6473 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/tabular_form_builder.rb
+++ lib/tabular_form_builder.rb
@@ -40,11 +40,11 @@ class TabularFormBuilder < ActionView::Helpers::FormBuilder
   
   # Returns a label tag for the given field
   def label_for_field(field, options = {})
-      return '' if options.delete(:no_label)
+      return ''.html_safe if options.delete(:no_label)
       text = options[:label].is_a?(Symbol) ? l(options[:label]) : options[:label]
       text ||= l(("field_" + field.to_s.gsub(/\_id$/, "")).to_sym)
       text += @template.content_tag("span", " *", :class => "required") if options.delete(:required)
-      @template.content_tag("label", text, 
+      @template.content_tag("label", text.html_safe, 
                                      :class => (@object && @object.errors[field] ? "error" : nil), 
                                      :for => (@object_name.to_s + "_" + field.to_s))
   end
