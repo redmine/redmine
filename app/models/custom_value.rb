aa84f622c7a5cf80dba6425a512a9e72dda05634
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-21 04:45:20 +0000

    remove trailing white-spaces from app/models/custom_value.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6514 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/custom_value.rb
+++ app/models/custom_value.rb
@@ -1,16 +1,16 @@
-# redMine - project management software
-# Copyright (C) 2006  Jean-Philippe Lang
+# Redmine - project management software
+# Copyright (C) 2006-2011  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -24,37 +24,37 @@ class CustomValue < ActiveRecord::Base
       self.value ||= custom_field.default_value
     end
   end
-  
+
   # Returns true if the boolean custom value is true
   def true?
     self.value == '1'
   end
-  
+
   def editable?
     custom_field.editable?
   end
-  
+
   def visible?
     custom_field.visible?
   end
-  
+
   def required?
     custom_field.is_required?
   end
-  
+
   def to_s
     value.to_s
   end
-  
+
 protected
   def validate
     if value.blank?
-      errors.add(:value, :blank) if custom_field.is_required? and value.blank?    
+      errors.add(:value, :blank) if custom_field.is_required? and value.blank?
     else
       errors.add(:value, :invalid) unless custom_field.regexp.blank? or value =~ Regexp.new(custom_field.regexp)
       errors.add(:value, :too_short, :count => custom_field.min_length) if custom_field.min_length > 0 and value.length < custom_field.min_length
       errors.add(:value, :too_long, :count => custom_field.max_length) if custom_field.max_length > 0 and value.length > custom_field.max_length
-    
+
       # Format specific validations
       case custom_field.field_format
       when 'int'
