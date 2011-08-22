9bd9988d9bac2faf5fe5acb8b8639572f8468c57
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-22 02:08:37 +0000

    remove trailing white-spaces from app/models/tracker.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6523 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/tracker.rb
+++ app/models/tracker.rb
@@ -1,29 +1,29 @@
-# redMine - project management software
-# Copyright (C) 2006  Jean-Philippe Lang
+# Redmine - project management software
+# Copyright (C) 2006-2011  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 class Tracker < ActiveRecord::Base
-  before_destroy :check_integrity  
+  before_destroy :check_integrity
   has_many :issues
   has_many :workflows, :dependent => :delete_all do
     def copy(source_tracker)
       Workflow.copy(source_tracker, nil, proxy_owner, nil)
     end
   end
-  
+
   has_and_belongs_to_many :projects
   has_and_belongs_to_many :custom_fields, :class_name => 'IssueCustomField', :join_table => "#{table_name_prefix}custom_fields_trackers#{table_name_suffix}", :association_foreign_key => 'custom_field_id'
   acts_as_list
@@ -33,9 +33,9 @@ class Tracker < ActiveRecord::Base
   validates_length_of :name, :maximum => 30
 
   named_scope :named, lambda {|arg| { :conditions => ["LOWER(#{table_name}.name) = LOWER(?)", arg.to_s.strip]}}
-  
+
   def to_s; name end
-  
+
   def <=>(tracker)
     name <=> tracker.name
   end
@@ -43,24 +43,24 @@ class Tracker < ActiveRecord::Base
   def self.all
     find(:all, :order => 'position')
   end
-  
+
   # Returns an array of IssueStatus that are used
   # in the tracker's workflows
   def issue_statuses
     if @issue_statuses
-      return @issue_statuses 
+      return @issue_statuses
     elsif new_record?
       return []
     end
-    
+
     ids = Workflow.
             connection.select_rows("SELECT DISTINCT old_status_id, new_status_id FROM #{Workflow.table_name} WHERE tracker_id = #{id}").
             flatten.
             uniq
-    
+
     @issue_statuses = IssueStatus.find_all_by_id(ids).sort
   end
-  
+
 private
   def check_integrity
     raise "Can't delete tracker" if Issue.find(:first, :conditions => ["tracker_id=?", self.id])
