5f4b076284f5d407da46c0946ec8e2afabf8d097
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-24 06:45:13 +0000

    remove trailing white-spaces from test/functional/boards_controller_test.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6581 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/boards_controller_test.rb
+++ test/functional/boards_controller_test.rb
@@ -1,16 +1,16 @@
 # Redmine - project management software
-# Copyright (C) 2006-2009  Jean-Philippe Lang
+# Copyright (C) 2006-2011  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -23,14 +23,14 @@ class BoardsController; def rescue_action(e) raise e end; end
 
 class BoardsControllerTest < ActionController::TestCase
   fixtures :projects, :users, :members, :member_roles, :roles, :boards, :messages, :enabled_modules
-  
+
   def setup
     @controller = BoardsController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
     User.current = nil
   end
-  
+
   def test_index
     get :index, :project_id => 1
     assert_response :success
@@ -38,21 +38,21 @@ class BoardsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:boards)
     assert_not_nil assigns(:project)
   end
-  
+
   def test_index_not_found
     get :index, :project_id => 97
     assert_response 404
   end
-  
+
   def test_index_should_show_messages_if_only_one_board
     Project.find(1).boards.slice(1..-1).each(&:destroy)
-    
+
     get :index, :project_id => 1
     assert_response :success
     assert_template 'show'
     assert_not_nil assigns(:topics)
   end
-  
+
   def test_post_new
     @request.session[:user_id] = 2
     assert_difference 'Board.count' do
@@ -60,7 +60,7 @@ class BoardsControllerTest < ActionController::TestCase
     end
     assert_redirected_to '/projects/ecookbook/settings/boards'
   end
-  
+
   def test_show
     get :show, :project_id => 1, :id => 1
     assert_response :success
@@ -69,7 +69,7 @@ class BoardsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:project)
     assert_not_nil assigns(:topics)
   end
-  
+
   def test_show_atom
     get :show, :project_id => 1, :id => 1, :format => 'atom'
     assert_response :success
@@ -78,7 +78,7 @@ class BoardsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:project)
     assert_not_nil assigns(:messages)
   end
-  
+
   def test_post_edit
     @request.session[:user_id] = 2
     assert_no_difference 'Board.count' do
@@ -87,7 +87,7 @@ class BoardsControllerTest < ActionController::TestCase
     assert_redirected_to '/projects/ecookbook/settings/boards'
     assert_equal 'Testing', Board.find(2).name
   end
-  
+
   def test_post_destroy
     @request.session[:user_id] = 2
     assert_difference 'Board.count', -1 do
