78e62c69cc0395b80dba8175d8a6f4fdf12d07c5
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-24 07:19:25 +0000

    remove trailing white-spaces from test/functional/enumerations_controller_test.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6592 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/enumerations_controller_test.rb
+++ test/functional/enumerations_controller_test.rb
@@ -1,16 +1,16 @@
 # Redmine - project management software
-# Copyright (C) 2006-2009  Jean-Philippe Lang
+# Copyright (C) 2006-2011  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -23,7 +23,7 @@ class EnumerationsController; def rescue_action(e) raise e end; end
 
 class EnumerationsControllerTest < ActionController::TestCase
   fixtures :enumerations, :issues, :users
-  
+
   def setup
     @controller = EnumerationsController.new
     @request    = ActionController::TestRequest.new
@@ -36,20 +36,20 @@ class EnumerationsControllerTest < ActionController::TestCase
     assert_response :success
     assert_template 'list'
   end
-  
+
   def test_destroy_enumeration_not_in_use
     post :destroy, :id => 7
     assert_redirected_to :controller => 'enumerations', :action => 'index'
     assert_nil Enumeration.find_by_id(7)
   end
-  
+
   def test_destroy_enumeration_in_use
     post :destroy, :id => 4
     assert_response :success
     assert_template 'destroy'
     assert_not_nil Enumeration.find_by_id(4)
   end
-  
+
   def test_destroy_enumeration_in_use_with_reassignment
     issue = Issue.find(:first, :conditions => {:priority_id => 4})
     post :destroy, :id => 4, :reassign_to_id => 6
