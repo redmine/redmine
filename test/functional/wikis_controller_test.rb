56f4b162db3890fc5be102badfd1cc9b3f144075
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-24 02:42:02 +0000

    remove trailing white-spaces from test/functional/wikis_controller_test.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6555 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/wikis_controller_test.rb
+++ test/functional/wikis_controller_test.rb
@@ -1,16 +1,16 @@
-# redMine - project management software
-# Copyright (C) 2006-2007  Jean-Philippe Lang
+# Redmine - project management software
+# Copyright (C) 2006-2011  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -23,14 +23,14 @@ class WikisController; def rescue_action(e) raise e end; end
 
 class WikisControllerTest < ActionController::TestCase
   fixtures :projects, :users, :roles, :members, :member_roles, :enabled_modules, :wikis
-  
+
   def setup
     @controller = WikisController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
     User.current = nil
   end
-  
+
   def test_create
     @request.session[:user_id] = 1
     assert_nil Project.find(3).wiki
@@ -40,17 +40,17 @@ class WikisControllerTest < ActionController::TestCase
     assert_not_nil wiki
     assert_equal 'Start page', wiki.start_page
   end
-  
+
   def test_destroy
     @request.session[:user_id] = 1
     post :destroy, :id => 1, :confirm => 1
     assert_redirected_to :controller => 'projects', :action => 'settings', :id => 'ecookbook', :tab => 'wiki'
     assert_nil Project.find(1).wiki
   end
-  
+
   def test_not_found
     @request.session[:user_id] = 1
     post :destroy, :id => 999, :confirm => 1
     assert_response 404
   end
-end 
+end
