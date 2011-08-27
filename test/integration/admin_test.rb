201ad03ce1cf1443e70a621864cd2966df142867
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-27 04:52:39 +0000

    remove trailing white-spaces from test/integration/admin_test.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6662 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/integration/admin_test.rb
+++ test/integration/admin_test.rb
@@ -1,16 +1,16 @@
-# redMine - project management software
-# Copyright (C) 2006  Jean-Philippe Lang
+# Redmine - project management software
+# Copyright (C) 2006- 2011  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -26,15 +26,15 @@ class AdminTest < ActionController::IntegrationTest
     assert_response :success
     assert_template "users/new"
     post "/users/create", :user => { :login => "psmith", :firstname => "Paul", :lastname => "Smith", :mail => "psmith@somenet.foo", :language => "en", :password => "psmith09", :password_confirmation => "psmith09" }
-    
+
     user = User.find_by_login("psmith")
     assert_kind_of User, user
     assert_redirected_to "/users/#{ user.id }/edit"
-    
+
     logged_user = User.try_to_login("psmith", "psmith09")
     assert_kind_of User, logged_user
     assert_equal "Paul", logged_user.firstname
-    
+
     put "users/#{user.id}", :id => user.id, :user => { :status => User::STATUS_LOCKED }
     assert_redirected_to "/users/#{ user.id }/edit"
     locked_user = User.try_to_login("psmith", "psmith09")
