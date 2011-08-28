6ed34affa0d669090d853e0bca608d4cacb02109
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-28 07:13:37 +0000

    remove trailing white-spaces from config/initializers/10-patches.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6692 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- config/initializers/10-patches.rb
+++ config/initializers/10-patches.rb
@@ -4,7 +4,7 @@ require 'active_record'
 module ActiveRecord
   class Base
     include Redmine::I18n
-    
+
     # Translate attribute names for validation errors display
     def self.human_attribute_name(attr)
       l("field_#{attr.to_s.gsub(/_id$/, '')}", :default => attr)
@@ -72,7 +72,7 @@ module AsynchronousMailer
     define_method("perform_delivery_async_#{type}") do |mail|
       Thread.start do
         send "perform_delivery_#{type}", mail
-      end      
+      end
     end
   end
 end
