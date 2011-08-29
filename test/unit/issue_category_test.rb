18f36b7d9f34f34ea79c4d6798b7e9b1ee1c1c4a
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-29 12:44:29 +0000

    remove trailing white-spaces from test/unit/issue_category_test.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6743 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/issue_category_test.rb
+++ test/unit/issue_category_test.rb
@@ -5,12 +5,12 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -23,27 +23,27 @@ class IssueCategoryTest < ActiveSupport::TestCase
   def setup
     @category = IssueCategory.find(1)
   end
-  
+
   def test_create
     assert IssueCategory.new(:project_id => 2, :name => 'New category').save
     category = IssueCategory.first(:order => 'id DESC')
     assert_equal 'New category', category.name
   end
-  
+
   def test_create_with_group_assignment
     assert IssueCategory.new(:project_id => 2, :name => 'Group assignment', :assigned_to_id => 11).save
     category = IssueCategory.first(:order => 'id DESC')
     assert_kind_of Group, category.assigned_to
     assert_equal Group.find(11), category.assigned_to
   end
-  
+
   def test_destroy
     issue = @category.issues.first
     @category.destroy
     # Make sure the category was nullified on the issue
     assert_nil issue.reload.category
   end
-  
+
   def test_destroy_with_reassign
     issue = @category.issues.first
     reassign_to = IssueCategory.find(2)
