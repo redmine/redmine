f04435e85b631c0cdbbcf7f7432197c8923b71c6
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-29 21:51:13 +0000

    remove trailing white-spaces from test/unit/token_test.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6751 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/token_test.rb
+++ test/unit/token_test.rb
@@ -1,16 +1,16 @@
 # Redmine - project management software
-# Copyright (C) 2006-2009  Jean-Philippe Lang
+# Copyright (C) 2006-2011  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -26,7 +26,7 @@ class TokenTest < ActiveSupport::TestCase
     assert_equal 40, token.value.length
     assert !token.expired?
   end
-  
+
   def test_create_should_remove_existing_tokens
     user = User.find(1)
     t1 = Token.create(:user => user, :action => 'autologin')
