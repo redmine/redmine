38ba617f99f2008f0ffbfc4dde26178581fabdd9
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-29 05:48:11 +0000

    Rails3: replace deprecated 'validate_on_create' to declared validation method at Repository model.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6717 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -32,7 +32,13 @@ class Repository < ActiveRecord::Base
 
   validates_length_of :password, :maximum => 255, :allow_nil => true
   # Checks if the SCM is enabled when creating a repository
-  validate_on_create { |r| r.errors.add(:type, :invalid) unless Setting.enabled_scm.include?(r.class.name.demodulize) }
+  validate :repo_create_validation, :on => :create
+
+  def repo_create_validation
+    unless Setting.enabled_scm.include?(self.class.name.demodulize)
+      errors.add(:type, :invalid)
+    end
+  end
 
   def self.human_attribute_name(attribute_key_name)
     attr_name = attribute_key_name
