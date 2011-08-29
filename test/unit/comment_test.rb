04864726622cea888742356b0b7849ed2b3788a9
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-29 10:09:46 +0000

    remove trailing white-spaces from test/unit/comment_test.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6723 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/comment_test.rb
+++ test/unit/comment_test.rb
@@ -5,12 +5,12 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -24,18 +24,18 @@ class CommentTest < ActiveSupport::TestCase
     @jsmith = User.find(2)
     @news = News.find(1)
   end
-  
+
   def test_create
     comment = Comment.new(:commented => @news, :author => @jsmith, :comments => "my comment")
     assert comment.save
     @news.reload
     assert_equal 2, @news.comments_count
   end
-  
+
   def test_create_should_send_notification
     Setting.notified_events << 'news_comment_added'
     Watcher.create!(:watchable => @news, :user => @jsmith)
-    
+
     assert_difference 'ActionMailer::Base.deliveries.size' do
       Comment.create!(:commented => @news, :author => @jsmith, :comments => "my comment")
     end
@@ -46,7 +46,7 @@ class CommentTest < ActiveSupport::TestCase
     assert !comment.save
     assert_equal 2, comment.errors.length
   end
-  
+
   def test_destroy
     comment = Comment.find(1)
     assert comment.destroy
