ba7830063b304c0e4a172421ed525bf902cbd6ef
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-29 12:43:43 +0000

    remove trailing white-spaces from test/unit/group_test.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6742 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/group_test.rb
+++ test/unit/group_test.rb
@@ -5,12 +5,12 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -24,27 +24,27 @@ class GroupTest < ActiveSupport::TestCase
     g = Group.new(:lastname => 'New group')
     assert g.save
   end
-  
+
   def test_roles_given_to_new_user
     group = Group.find(11)
     user = User.find(9)
     project = Project.first
-    
+
     Member.create!(:principal => group, :project => project, :role_ids => [1, 2])
     group.users << user
     assert user.member_of?(project)
   end
-  
+
   def test_roles_given_to_existing_user
     group = Group.find(11)
     user = User.find(9)
     project = Project.first
-    
+
     group.users << user
     m = Member.create!(:principal => group, :project => project, :role_ids => [1, 2])
     assert user.member_of?(project)
   end
-  
+
   def test_roles_updated
     group = Group.find(11)
     user = User.find(9)
@@ -52,13 +52,13 @@ class GroupTest < ActiveSupport::TestCase
     group.users << user
     m = Member.create!(:principal => group, :project => project, :role_ids => [1])
     assert_equal [1], user.reload.roles_for_project(project).collect(&:id).sort
-    
+
     m.role_ids = [1, 2]
     assert_equal [1, 2], user.reload.roles_for_project(project).collect(&:id).sort
-    
+
     m.role_ids = [2]
     assert_equal [2], user.reload.roles_for_project(project).collect(&:id).sort
-    
+
     m.role_ids = [1]
     assert_equal [1], user.reload.roles_for_project(project).collect(&:id).sort
   end
@@ -74,14 +74,14 @@ class GroupTest < ActiveSupport::TestCase
     User.find(8).groups.clear
     assert !User.find(8).member_of?(Project.find(5))
   end
-  
+
   def test_destroy_should_unassign_issues
     group = Group.first
     Issue.update_all(["assigned_to_id = ?", group.id], 'id = 1')
-    
+
     assert group.destroy
     assert group.destroyed?
-    
+
     assert_equal nil, Issue.find(1).assigned_to_id
   end
 end
