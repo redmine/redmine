cf733e7db7b60e060178d5c2fc4e151c3735a73c
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-29 21:51:46 +0000

    remove trailing white-spaces from test/unit/time_entry_test.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6752 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/time_entry_test.rb
+++ test/unit/time_entry_test.rb
@@ -1,16 +1,16 @@
-# redMine - project management software
-# Copyright (C) 2006-2008  Jean-Philippe Lang
+# Redmine - project management software
+# Copyright (C) 2006-2011  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -38,47 +38,47 @@ class TimeEntryTest < ActiveSupport::TestCase
                    "3 hours"  => 3.0,
                    "12min"    => 0.2,
                   }
-    
+
     assertions.each do |k, v|
       t = TimeEntry.new(:hours => k)
       assert_equal v, t.hours, "Converting #{k} failed:"
     end
   end
-  
+
   def test_hours_should_default_to_nil
     assert_nil TimeEntry.new.hours
   end
-  
+
   def test_spent_on_with_blank
     c = TimeEntry.new
     c.spent_on = ''
     assert_nil c.spent_on
   end
-  
+
   def test_spent_on_with_nil
     c = TimeEntry.new
     c.spent_on = nil
     assert_nil c.spent_on
   end
-  
+
   def test_spent_on_with_string
     c = TimeEntry.new
     c.spent_on = "2011-01-14"
     assert_equal Date.parse("2011-01-14"), c.spent_on
   end
-  
+
   def test_spent_on_with_invalid_string
     c = TimeEntry.new
     c.spent_on = "foo"
     assert_nil c.spent_on
   end
-  
+
   def test_spent_on_with_date
     c = TimeEntry.new
     c.spent_on = Date.today
     assert_equal Date.today, c.spent_on
   end
-  
+
   def test_spent_on_with_time
     c = TimeEntry.new
     c.spent_on = Time.now
@@ -94,7 +94,7 @@ class TimeEntryTest < ActiveSupport::TestCase
                           :issue => @issue,
                           :project => @public_project)
     end
-    
+
     context "without a project" do
       should "return the lowest spent_on value that is visible to the current user" do
         assert_equal "2007-03-12", TimeEntry.earilest_date_for_project.to_s
@@ -106,7 +106,7 @@ class TimeEntryTest < ActiveSupport::TestCase
         assert_equal "2010-01-01", TimeEntry.earilest_date_for_project(@public_project).to_s
       end
     end
-      
+
   end
 
   context "#latest_date_for_project" do
@@ -131,5 +131,5 @@ class TimeEntryTest < ActiveSupport::TestCase
         assert_equal "2007-04-22", TimeEntry.latest_date_for_project(project).to_s
       end
     end
-  end  
+  end
 end
