578d8cc63e3298727d55b4ab36f604abd91739f6
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-29 10:09:21 +0000

    remove trailing white-spaces from test/unit/custom_field_test.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6722 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/custom_field_test.rb
+++ test/unit/custom_field_test.rb
@@ -5,12 +5,12 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -19,39 +19,39 @@ require File.expand_path('../../test_helper', __FILE__)
 
 class CustomFieldTest < ActiveSupport::TestCase
   fixtures :custom_fields
-  
+
   def test_create
     field = UserCustomField.new(:name => 'Money money money', :field_format => 'float')
     assert field.save
   end
-  
+
   def test_regexp_validation
     field = IssueCustomField.new(:name => 'regexp', :field_format => 'text', :regexp => '[a-z0-9')
     assert !field.save
     assert_equal I18n.t('activerecord.errors.messages.invalid'), field.errors.on(:regexp)
-    
+
     field.regexp = '[a-z0-9]'
     assert field.save
   end
-  
+
   def test_possible_values_should_accept_an_array
     field = CustomField.new
     field.possible_values = ["One value", ""]
     assert_equal ["One value"], field.possible_values
   end
-  
+
   def test_possible_values_should_accept_a_string
     field = CustomField.new
     field.possible_values = "One value"
     assert_equal ["One value"], field.possible_values
   end
-  
+
   def test_possible_values_should_accept_a_multiline_string
     field = CustomField.new
     field.possible_values = "One value\nAnd another one  \r\n \n"
     assert_equal ["One value", "And another one"], field.possible_values
   end
-  
+
   def test_destroy
     field = CustomField.find(1)
     assert field.destroy
