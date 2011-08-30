c6c6087284b832c928f98deff03ec58a0e3816ab
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-30 14:10:37 +0000

    remove trailing white-spaces from app/controllers/trackers_controller.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6805 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/trackers_controller.rb
+++ app/controllers/trackers_controller.rb
@@ -1,23 +1,23 @@
 # Redmine - project management software
-# Copyright (C) 2006-2009  Jean-Philippe Lang
+# Copyright (C) 2006-2011  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 class TrackersController < ApplicationController
   layout 'admin'
-  
+
   before_filter :require_admin
 
   verify :method => :post, :only => :destroy, :redirect_to => { :action => :index }
@@ -51,7 +51,7 @@ class TrackersController < ApplicationController
     end
     @projects = Project.find(:all)
   end
-  
+
   def destroy
     @tracker = Tracker.find(params[:id])
     unless @tracker.issues.empty?
@@ -60,5 +60,5 @@ class TrackersController < ApplicationController
       @tracker.destroy
     end
     redirect_to :action => 'index'
-  end  
+  end
 end
