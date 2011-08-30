8f13c41f19ee125f018b276ea81b9d6b99407887
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-30 04:12:40 +0000

    remove trailing white-spaces from test/unit/user_preference_test.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6757 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/user_preference_test.rb
+++ test/unit/user_preference_test.rb
@@ -1,16 +1,16 @@
-# redMine - project management software
-# Copyright (C) 2006-2007  Jean-Philippe Lang
+# Redmine - project management software
+# Copyright (C) 2006-2011  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -25,18 +25,18 @@ class UserPreferenceTest < ActiveSupport::TestCase
     user.login = "newuser"
     user.password, user.password_confirmation = "password", "password"
     assert user.save
-    
+
     assert_kind_of UserPreference, user.pref
     assert_kind_of Hash, user.pref.others
     assert user.pref.save
   end
-  
+
   def test_update
     user = User.find(1)
     assert_equal true, user.pref.hide_mail
     user.pref['preftest'] = 'value'
     assert user.pref.save
-    
+
     user.reload
     assert_equal 'value', user.pref['preftest']
   end
