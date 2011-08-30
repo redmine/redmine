a60788aa5ac70d6c49c3fe1f285f780133d50c3e
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-30 15:56:37 +0000

    remove trailing white-spaces from test/functional/comments_controller_test.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6821 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/comments_controller_test.rb
+++ test/functional/comments_controller_test.rb
@@ -1,16 +1,16 @@
-# redMine - project management software
-# Copyright (C) 2006-2007  Jean-Philippe Lang
+# Redmine - project management software
+# Copyright (C) 2006-2011  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -19,22 +19,22 @@ require File.expand_path('../../test_helper', __FILE__)
 
 class CommentsControllerTest < ActionController::TestCase
   fixtures :projects, :users, :roles, :members, :member_roles, :enabled_modules, :news, :comments
-  
+
   def setup
     User.current = nil
   end
-  
+
   def test_add_comment
     @request.session[:user_id] = 2
     post :create, :id => 1, :comment => { :comments => 'This is a test comment' }
     assert_redirected_to '/news/1'
-    
+
     comment = News.find(1).comments.find(:first, :order => 'created_on DESC')
     assert_not_nil comment
     assert_equal 'This is a test comment', comment.comments
     assert_equal User.find(2), comment.author
   end
-  
+
   def test_empty_comment_should_not_be_added
     @request.session[:user_id] = 2
     assert_no_difference 'Comment.count' do
@@ -52,6 +52,4 @@ class CommentsControllerTest < ActionController::TestCase
     assert_nil Comment.find_by_id(2)
     assert_equal comments_count - 1, News.find(1).comments.size
   end
-  
-
 end
