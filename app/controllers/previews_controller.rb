44ea52dac35aa0913d04ea7458a79d3a78185f91
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-30 14:13:09 +0000

    remove trailing white-spaces from app/controllers/previews_controller.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6808 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/previews_controller.rb
+++ app/controllers/previews_controller.rb
@@ -5,12 +5,12 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -39,12 +39,12 @@ class PreviewsController < ApplicationController
   end
 
   private
-  
+
   def find_project
     project_id = (params[:issue] && params[:issue][:project_id]) || params[:project_id]
     @project = Project.find(project_id)
   rescue ActiveRecord::RecordNotFound
     render_404
   end
-  
+
 end
