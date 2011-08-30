d137106c4ea9c27c1690156ad7e6684f1a223685
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-30 15:58:10 +0000

    remove trailing white-spaces from test/functional/auto_completes_controller_test.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6823 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/auto_completes_controller_test.rb
+++ test/functional/auto_completes_controller_test.rb
@@ -9,14 +9,14 @@ class AutoCompletesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:issues)
     assert assigns(:issues).detect {|issue| issue.subject.match /recipe/}
   end
-  
+
   def test_issues_should_return_issue_with_given_id
     get :issues, :project_id => 'subproject1', :q => '13'
     assert_response :success
     assert_not_nil assigns(:issues)
     assert assigns(:issues).include?(Issue.find(13))
   end
-  
+
   def test_auto_complete_with_scope_all_and_cross_project_relations
     Setting.cross_project_issue_relations = '1'
     get :issues, :project_id => 'ecookbook', :q => '13', :scope => 'all'
@@ -24,7 +24,7 @@ class AutoCompletesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:issues)
     assert assigns(:issues).include?(Issue.find(13))
   end
-     
+
   def test_auto_complete_with_scope_all_without_cross_project_relations
     Setting.cross_project_issue_relations = '0'
     get :issues, :project_id => 'ecookbook', :q => '13', :scope => 'all'
