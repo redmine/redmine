24718defcd4c300abffd451090fcb28576aa7cfa
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-30 16:02:38 +0000

    remove trailing white-spaces from test/functional/previews_controller_test.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6829 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/previews_controller_test.rb
+++ test/functional/previews_controller_test.rb
@@ -5,12 +5,12 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -27,7 +27,7 @@ class PreviewsControllerTest < ActionController::TestCase
     assert_template 'preview'
     assert_not_nil assigns(:description)
   end
-                              
+
   def test_preview_issue_notes
     @request.session[:user_id] = 2
     post :issue, :project_id => '1', :id => 1, :issue => {:description => Issue.find(1).description}, :notes => 'Foo'
@@ -35,7 +35,7 @@ class PreviewsControllerTest < ActionController::TestCase
     assert_template 'preview'
     assert_not_nil assigns(:notes)
   end
-  
+
   def test_preview_journal_notes_for_update
     @request.session[:user_id] = 2
     post :issue, :project_id => '1', :id => 1, :notes => 'Foo'
