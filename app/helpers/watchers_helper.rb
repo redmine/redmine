42aac56a558243a7ee01a92e7a180954b3e7abcb
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-31 16:04:07 +0000

    remove trailing white-spaces from app/helpers/watchers_helper.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6920 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/watchers_helper.rb
+++ app/helpers/watchers_helper.rb
@@ -5,22 +5,22 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 module WatchersHelper
-  
+
   def watcher_tag(object, user, options={})
     content_tag("span", watcher_link(object, user), :class => watcher_css(object))
   end
-  
+
   def watcher_link(object, user)
     return '' unless user && user.logged? && object.respond_to?('watched_by?')
     watched = object.watched_by?(user)
@@ -32,14 +32,14 @@ module WatchersHelper
                    {:url => url},
                    :href => url_for(url),
                    :class => (watched ? 'icon icon-fav' : 'icon icon-fav-off'))
-  
+
   end
-  
+
   # Returns the css class used to identify watch links for a given +object+
   def watcher_css(object)
     "#{object.class.to_s.underscore}-#{object.id}-watcher"
   end
-  
+
   # Returns a comma separated list of users watching the given object
   def watchers_list(object)
     remove_allowed = User.current.allowed_to?("delete_#{object.class.name.underscore}_watchers".to_sym, object.project)
