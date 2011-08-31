cc454e0f1d5eee01dc9e5c99989f7bfc3b92b1e8
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-31 12:02:01 +0000

    remove trailing white-spaces from app/controllers/queries_controller.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6892 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -5,12 +5,12 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -19,9 +19,9 @@ class QueriesController < ApplicationController
   menu_item :issues
   before_filter :find_query, :except => [:new, :index]
   before_filter :find_optional_project, :only => :new
-  
+
   accept_api_auth :index
-  
+
   def index
     case params[:format]
     when 'xml', 'json'
@@ -29,28 +29,28 @@ class QueriesController < ApplicationController
     else
       @limit = per_page_option
     end
-    
+
     @query_count = Query.visible.count
     @query_pages = Paginator.new self, @query_count, @limit, params['page']
     @queries = Query.visible.all(:limit => @limit, :offset => @offset, :order => "#{Query.table_name}.name")
-    
+
     respond_to do |format|
       format.html { render :nothing => true }
       format.api
     end
   end
-  
+
   def new
     @query = Query.new(params[:query])
     @query.project = params[:query_is_for_all] ? nil : @project
     @query.user = User.current
     @query.is_public = false unless User.current.allowed_to?(:manage_public_queries, @project) || User.current.admin?
-    
+
     @query.add_filters(params[:fields] || params[:f], params[:operators] || params[:op], params[:values] || params[:v]) if params[:fields] || params[:f]
     @query.group_by ||= params[:group_by]
     @query.column_names = params[:c] if params[:c]
     @query.column_names = nil if params[:default_columns]
-    
+
     if request.post? && params[:confirm] && @query.save
       flash[:notice] = l(:notice_successful_create)
       redirect_to :controller => 'issues', :action => 'index', :project_id => @project, :query_id => @query
@@ -58,7 +58,7 @@ class QueriesController < ApplicationController
     end
     render :layout => false if request.xhr?
   end
-  
+
   def edit
     if request.post?
       @query.filters = {}
@@ -69,7 +69,7 @@ class QueriesController < ApplicationController
       @query.group_by ||= params[:group_by]
       @query.column_names = params[:c] if params[:c]
       @query.column_names = nil if params[:default_columns]
-      
+
       if @query.save
         flash[:notice] = l(:notice_successful_update)
         redirect_to :controller => 'issues', :action => 'index', :project_id => @project, :query_id => @query
@@ -81,7 +81,7 @@ class QueriesController < ApplicationController
     @query.destroy if request.post?
     redirect_to :controller => 'issues', :action => 'index', :project_id => @project, :set_filter => 1
   end
-  
+
 private
   def find_query
     @query = Query.find(params[:id])
@@ -90,7 +90,7 @@ private
   rescue ActiveRecord::RecordNotFound
     render_404
   end
-  
+
   def find_optional_project
     @project = Project.find(params[:project_id]) if params[:project_id]
     render_403 unless User.current.allowed_to?(:save_queries, @project, :global => true)
