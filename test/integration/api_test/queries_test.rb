51e2d89608e998db659dfc6ad62137e9c1d9a7e4
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-31 23:16:38 +0000

    remove trailing white-spaces from test/integration/api_test/queries_test.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6935 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/integration/api_test/queries_test.rb
+++ test/integration/api_test/queries_test.rb
@@ -5,12 +5,12 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -26,10 +26,10 @@ class ApiTest::QueriesTest < ActionController::IntegrationTest
 
   context "/queries" do
     context "GET" do
-      
+
       should "return queries" do
         get '/queries.xml'
-        
+
         assert_response :success
         assert_equal 'application/xml', @response.content_type
         assert_tag :tag => 'queries',
@@ -48,7 +48,7 @@ class ApiTest::QueriesTest < ActionController::IntegrationTest
       end
     end
   end
-  
+
   def credentials(user, password=nil)
     ActionController::HttpAuthentication::Basic.encode_credentials(user, password || user)
   end
