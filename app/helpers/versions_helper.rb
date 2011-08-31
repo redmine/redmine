32baf5d6e17feb0a94cb6a98ea12da0fd4d6e020
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-31 16:05:03 +0000

    remove trailing white-spaces from app/helpers/versions_helper.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6921 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/versions_helper.rb
+++ app/helpers/versions_helper.rb
@@ -5,12 +5,12 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -18,10 +18,10 @@
 module VersionsHelper
 
   STATUS_BY_CRITERIAS = %w(category tracker status priority author assigned_to)
-  
+
   def render_issue_status_by(version, criteria)
     criteria = 'category' unless STATUS_BY_CRITERIAS.include?(criteria)
-    
+
     h = Hash.new {|k,v| k[v] = [0, 0]}
     begin
       # Total issue count
@@ -36,10 +36,10 @@ module VersionsHelper
     end
     counts = h.keys.compact.sort.collect {|k| {:group => k, :total => h[k][0], :open => h[k][1], :closed => (h[k][0] - h[k][1])}}
     max = counts.collect {|c| c[:total]}.max
-    
+
     render :partial => 'issue_counts', :locals => {:version => version, :criteria => criteria, :counts => counts, :max => max}
   end
-  
+
   def status_by_options_for_select(value)
     options_for_select(STATUS_BY_CRITERIAS.collect {|criteria| [l("field_#{criteria}".to_sym), criteria]}, value)
   end
