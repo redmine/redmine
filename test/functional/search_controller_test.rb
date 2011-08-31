1fe58f503c56dd5106d6b4e387fe40e099a5d94d
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-31 22:57:34 +0000

    remove trailing white-spaces from test/functional/search_controller_test.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6924 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/search_controller_test.rb
+++ test/functional/search_controller_test.rb
@@ -124,7 +124,7 @@ class SearchControllerTest < ActionController::TestCase
 
   def test_search_content
     Issue.update_all("description = 'This is a searchkeywordinthecontent'", "id=1")
-    
+
     get :index, :id => 1, :q => 'searchkeywordinthecontent', :titles_only => ''
     assert_equal false, assigns(:titles_only)
     results = assigns(:results)
