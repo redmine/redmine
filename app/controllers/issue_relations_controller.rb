cd410eb38ea7f96c5aec13a1f3c7b579904368dc
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-31 11:14:13 +0000

    remove trailing white-spaces from app/controllers/issue_relations_controller.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6889 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/issue_relations_controller.rb
+++ app/controllers/issue_relations_controller.rb
@@ -5,12 +5,12 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -18,18 +18,18 @@
 class IssueRelationsController < ApplicationController
   before_filter :find_issue, :find_project_from_association, :authorize, :only => [:index, :create]
   before_filter :find_relation, :except => [:index, :create]
-  
+
   accept_api_auth :index, :show, :create, :destroy
-  
+
   def index
     @relations = @issue.relations
-    
+
     respond_to do |format|
       format.html { render :nothing => true }
       format.api
     end
   end
-  
+
   def show
     raise Unauthorized unless @relation.visible?
 
@@ -40,7 +40,7 @@ class IssueRelationsController < ApplicationController
   rescue ActiveRecord::RecordNotFound
     render_404
   end
-  
+
   verify :method => :post, :only => :create, :render => {:nothing => true, :status => :method_not_allowed }
   def create
     @relation = IssueRelation.new(params[:relation])
@@ -49,7 +49,7 @@ class IssueRelationsController < ApplicationController
       @relation.issue_to = Issue.visible.find_by_id(m[1].to_i)
     end
     saved = @relation.save
-    
+
     respond_to do |format|
       format.html { redirect_to :controller => 'issues', :action => 'show', :id => @issue }
       format.js do
@@ -62,7 +62,7 @@ class IssueRelationsController < ApplicationController
           end
         end
       end
-      format.api { 
+      format.api {
         if saved
           render :action => 'show', :status => :created, :location => relation_url(@relation)
         else
@@ -71,12 +71,12 @@ class IssueRelationsController < ApplicationController
       }
     end
   end
-  
+
   verify :method => :delete, :only => :destroy, :render => {:nothing => true, :status => :method_not_allowed }
   def destroy
     raise Unauthorized unless @relation.deletable?
     @relation.destroy
-    
+
     respond_to do |format|
       format.html { redirect_to :controller => 'issues', :action => 'show', :id => @issue }
       format.js   { render(:update) {|page| page.remove "relation-#{@relation.id}"} }
@@ -85,14 +85,14 @@ class IssueRelationsController < ApplicationController
   rescue ActiveRecord::RecordNotFound
     render_404
   end
-  
+
 private
   def find_issue
     @issue = @object = Issue.find(params[:issue_id])
   rescue ActiveRecord::RecordNotFound
     render_404
   end
-  
+
   def find_relation
     @relation = IssueRelation.find(params[:id])
   rescue ActiveRecord::RecordNotFound
