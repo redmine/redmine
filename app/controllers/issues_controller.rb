698fc53c94b58b34c6b3c312bbf28ccce6bcb9ef
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-31 10:10:56 +0000

    remove trailing white-spaces from app/controllers/issues_controller.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6884 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -90,7 +90,7 @@ class IssuesController < ApplicationController
 
       respond_to do |format|
         format.html { render :template => 'issues/index.rhtml', :layout => !request.xhr? }
-        format.api  { 
+        format.api  {
           Issue.load_relations(@issues) if include_in_api_response?('relations')
         }
         format.atom { render_feed(@issues, :title => "#{@project || Setting.app_title}: #{l(:label_issue_plural)}") }
