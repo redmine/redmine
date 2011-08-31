1fda93a02fad6b51c44fe943a05e62f0d2eaf5a5
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-31 04:09:11 +0000

    remove trailing white-spaces from app/models/wiki.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6837 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,16 +1,16 @@
 # Redmine - project management software
-# Copyright (C) 2006-2009  Jean-Philippe Lang
+# Copyright (C) 2006-2011  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -19,29 +19,29 @@ class Wiki < ActiveRecord::Base
   belongs_to :project
   has_many :pages, :class_name => 'WikiPage', :dependent => :destroy, :order => 'title'
   has_many :redirects, :class_name => 'WikiRedirect', :dependent => :delete_all
-  
+
   acts_as_watchable
-  
+
   validates_presence_of :start_page
   validates_format_of :start_page, :with => /^[^,\.\/\?\;\|\:]*$/
-  
+
   def visible?(user=User.current)
     !user.nil? && user.allowed_to?(:view_wiki_pages, project)
   end
-  
+
   # Returns the wiki page that acts as the sidebar content
   # or nil if no such page exists
   def sidebar
     @sidebar ||= find_page('Sidebar', :with_redirect => false)
   end
-  
+
   # find the page with the given title
   # if page doesn't exist, return a new page
   def find_or_new_page(title)
     title = start_page if title.blank?
     find_page(title) || WikiPage.new(:wiki => self, :title => Wiki.titleize(title))
   end
-  
+
   # find the page with the given title
   def find_page(title, options = {})
     @page_found_with_redirect = false
@@ -58,7 +58,7 @@ class Wiki < ActiveRecord::Base
     end
     page
   end
-  
+
   # Returns true if the last page was found with a redirect
   def page_found_with_redirect?
     @page_found_with_redirect
@@ -82,7 +82,7 @@ class Wiki < ActiveRecord::Base
       end
     end
   end
-  
+
   # turn a string into a valid page title
   def self.titleize(title)
     # replace spaces with _ and remove unwanted caracters
@@ -90,5 +90,5 @@ class Wiki < ActiveRecord::Base
     # upcase the first letter
     title = (title.slice(0..0).upcase + (title.slice(1..-1) || '')) if title
     title
-  end  
+  end
 end
