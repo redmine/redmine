a611039e27197569058878ffd9badfc727c1708a
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-31 13:08:28 +0000

    remove trailing white-spaces from test/functional/issue_relations_controller_test.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6907 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/issue_relations_controller_test.rb
+++ test/functional/issue_relations_controller_test.rb
@@ -5,12 +5,12 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -34,27 +34,27 @@ class IssueRelationsControllerTest < ActionController::TestCase
            :enabled_modules,
            :enumerations,
            :trackers
-  
+
   def setup
     @controller = IssueRelationsController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
     User.current = nil
   end
-  
+
   def test_create
     assert_difference 'IssueRelation.count' do
       @request.session[:user_id] = 3
-      post :create, :issue_id => 1, 
+      post :create, :issue_id => 1,
                  :relation => {:issue_to_id => '2', :relation_type => 'relates', :delay => ''}
     end
   end
-  
+
   def test_create_xhr
     assert_difference 'IssueRelation.count' do
       @request.session[:user_id] = 3
       xhr :post, :create,
-        :issue_id => 3, 
+        :issue_id => 3,
         :relation => {:issue_to_id => '1', :relation_type => 'relates', :delay => ''}
       assert_select_rjs 'relations' do
         assert_select 'table', 1
@@ -62,51 +62,51 @@ class IssueRelationsControllerTest < ActionController::TestCase
       end
     end
   end
-  
+
   def test_create_should_accept_id_with_hash
     assert_difference 'IssueRelation.count' do
       @request.session[:user_id] = 3
-      post :create, :issue_id => 1, 
+      post :create, :issue_id => 1,
                  :relation => {:issue_to_id => '#2', :relation_type => 'relates', :delay => ''}
     end
   end
-  
+
   def test_create_should_not_break_with_non_numerical_id
     assert_no_difference 'IssueRelation.count' do
       assert_nothing_raised do
         @request.session[:user_id] = 3
-        post :create, :issue_id => 1, 
+        post :create, :issue_id => 1,
                    :relation => {:issue_to_id => 'foo', :relation_type => 'relates', :delay => ''}
       end
     end
   end
-  
+
   def test_should_create_relations_with_visible_issues_only
     Setting.cross_project_issue_relations = '1'
     assert_nil Issue.visible(User.find(3)).find_by_id(4)
-    
+
     assert_no_difference 'IssueRelation.count' do
       @request.session[:user_id] = 3
-      post :create, :issue_id => 1, 
+      post :create, :issue_id => 1,
                  :relation => {:issue_to_id => '4', :relation_type => 'relates', :delay => ''}
     end
   end
 
   should "prevent relation creation when there's a circular dependency"
-  
+
   def test_destroy
     assert_difference 'IssueRelation.count', -1 do
       @request.session[:user_id] = 3
       delete :destroy, :id => '2'
     end
   end
-  
+
   def test_destroy_xhr
     IssueRelation.create!(:relation_type => IssueRelation::TYPE_RELATES) do |r|
       r.issue_from_id = 3
       r.issue_to_id = 1
     end
-    
+
     assert_difference 'IssueRelation.count', -1 do
       @request.session[:user_id] = 3
       xhr :delete, :destroy, :id => '2'
