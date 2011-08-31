2230dfda51cd3926a4089ad5c235ce0a16a7ba83
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-08-31 11:12:32 +0000

    remove trailing white-spaces from app/helpers/queries_helper.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6887 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/queries_helper.rb
+++ app/helpers/queries_helper.rb
@@ -5,31 +5,31 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 module QueriesHelper
-  
+
   def operators_for_select(filter_type)
     Query.operators_by_filter_type[filter_type].collect {|o| [l(Query.operators[o]), o]}
   end
-  
+
   def column_header(column)
     column.sortable ? sort_header_tag(column.name.to_s, :caption => column.caption,
-                                                        :default_order => column.default_order) : 
+                                                        :default_order => column.default_order) :
                       content_tag('th', h(column.caption))
   end
-  
+
   def column_content(column, issue)
     value = column.value(issue)
-    
+
     case value.class.name
     when 'String'
       if column.name == :subject
