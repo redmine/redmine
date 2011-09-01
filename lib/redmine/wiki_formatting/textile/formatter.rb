1556481ab1cae53b092ce77c8a87f54069b6fc34
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-01 02:04:13 +0000

    remove trailing white-spaces from lib/redmine/wiki_formatting/textile/formatter.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6950 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/wiki_formatting/textile/formatter.rb
+++ lib/redmine/wiki_formatting/textile/formatter.rb
@@ -1,16 +1,16 @@
 # Redmine - project management software
-# Copyright (C) 2006-2010  Jean-Philippe Lang
+# Copyright (C) 2006-2011  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -22,30 +22,30 @@ module Redmine
     module Textile
       class Formatter < RedCloth3
         include ActionView::Helpers::TagHelper
-        
+
         # auto_link rule after textile rules so that it doesn't break !image_url! tags
         RULES = [:textile, :block_markdown_rule, :inline_auto_link, :inline_auto_mailto]
-        
+
         def initialize(*args)
           super
           self.hard_breaks=true
           self.no_span_caps=true
           self.filter_styles=true
         end
-        
+
         def to_html(*rules)
           @toc = []
           super(*RULES).to_s
         end
-  
+
       private
-  
+
         # Patch for RedCloth.  Fixed in RedCloth r128 but _why hasn't released it yet.
         # <a href="http://code.whytheluckystiff.net/redcloth/changeset/128">http://code.whytheluckystiff.net/redcloth/changeset/128</a>
-        def hard_break( text ) 
+        def hard_break( text )
           text.gsub!( /(.)\n(?!\n|\Z| *([#*=]+(\s|$)|[{|]))/, "\\1<br />" ) if hard_breaks
         end
-        
+
         # Patch to add code highlighting support to RedCloth
         def smooth_offtags( text )
           unless @pre_list.empty?
@@ -53,18 +53,18 @@ module Redmine
             text.gsub!(/<redpre#(\d+)>/) do
               content = @pre_list[$1.to_i]
               if content.match(/<code\s+class="(\w+)">\s?(.+)/m)
-                content = "<code class=\"#{$1} syntaxhl\">" + 
+                content = "<code class=\"#{$1} syntaxhl\">" +
                   Redmine::SyntaxHighlighting.highlight_by_language($2, $1)
               end
               content
             end
           end
         end
-        
+
         AUTO_LINK_RE = %r{
                         (                          # leading text
                           <\w+.*?>|                # leading HTML tag, or
-                          [^=<>!:'"/]|             # leading punctuation, or 
+                          [^=<>!:'"/]|             # leading punctuation, or
                           ^                        # beginning of line
                         )
                         (
@@ -79,7 +79,7 @@ module Redmine
                         ((?:&gt;)?|[^\w\=\/;\(\)]*?)               # post
                         (?=<|\s|$)
                        }x unless const_defined?(:AUTO_LINK_RE)
-  
+
         # Turns all urls into clickable links (code from Rails).
         def inline_auto_link(text)
           text.gsub!(AUTO_LINK_RE) do
@@ -100,7 +100,7 @@ module Redmine
             end
           end
         end
-  
+
         # Turns all email addresses into clickable links (code from Rails).
         def inline_auto_mailto(text)
           text.gsub!(/([\w\.!#\$%\-+.]+@[A-Za-z0-9\-]+(\.[A-Za-z0-9\-]+)+)/) do
