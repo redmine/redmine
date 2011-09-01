9be5f2aad6080e69c9cd0780cfdf171fd85b6fa6
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-01 02:03:23 +0000

    remove trailing white-spaces from lib/redmine/wiki_formatting/textile/helper.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6949 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/wiki_formatting/textile/helper.rb
+++ lib/redmine/wiki_formatting/textile/helper.rb
@@ -5,12 +5,12 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -25,14 +25,14 @@ module Redmine
           url = "#{Redmine::Utils.relative_url_root}/help/wiki_syntax.html"
           help_link = link_to(l(:setting_text_formatting), url,
             :onclick => "window.open(\"#{ url }\", \"\", \"resizable=yes, location=no, width=300, height=640, menubar=no, status=no, scrollbars=yes\"); return false;")
-      
+
           javascript_tag("var wikiToolbar = new jsToolBar($('#{field_id}')); wikiToolbar.setHelpLink('#{escape_javascript help_link}'); wikiToolbar.draw();")
         end
-      
+
         def initial_page_content(page)
           "h1. #{@page.pretty_title}"
         end
-      
+
         def heads_for_wiki_formatter
           unless @heads_for_wiki_formatter_included
             content_for :header_tags do
