4ce8f90c032941b6f16e69efa2d93ccfa82942d9
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-01 00:46:33 +0000

    remove trailing white-spaces from app/helpers/wiki_helper.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6941 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/wiki_helper.rb
+++ app/helpers/wiki_helper.rb
@@ -5,18 +5,18 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 module WikiHelper
-  
+
   def wiki_page_options_for_select(pages, selected = nil, parent = nil, level = 0)
     pages = pages.group_by(&:parent) unless pages.is_a?(Hash)
     s = ''
@@ -25,14 +25,14 @@ module WikiHelper
         attrs = "value='#{page.id}'"
         attrs << " selected='selected'" if selected == page
         indent = (level > 0) ? ('&nbsp;' * level * 2 + '&#187; ') : nil
-        
-        s << "<option #{attrs}>#{indent}#{h page.pretty_title}</option>\n" + 
+
+        s << "<option #{attrs}>#{indent}#{h page.pretty_title}</option>\n" +
                wiki_page_options_for_select(pages, selected, page, level + 1)
       end
     end
     s
   end
-  
+
   def wiki_page_breadcrumb(page)
     breadcrumb(page.ancestors.reverse.collect {|parent|
       link_to(h(parent.pretty_title), {:controller => 'wiki', :action => 'show', :id => parent.title, :project_id => parent.project})
