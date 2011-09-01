28c4e0494eb14889dd75871c0c387b4822e26ed6
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-01 02:07:24 +0000

    remove trailing white-spaces from lib/redmine/core_ext/string/conversions.rb.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@6955 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/core_ext/string/conversions.rb
+++ lib/redmine/core_ext/string/conversions.rb
@@ -1,16 +1,16 @@
-# redMine - project management software
-# Copyright (C) 2008  Jean-Philippe Lang
+# Redmine - project management software
+# Copyright (C) 2006-2011  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -36,7 +36,7 @@ module Redmine #:nodoc:
           s.gsub!(',', '.')
           begin; Kernel.Float(s); rescue; nil; end
         end
-        
+
         # Object#to_a removed in ruby1.9
         if RUBY_VERSION > '1.9'
           def to_a
