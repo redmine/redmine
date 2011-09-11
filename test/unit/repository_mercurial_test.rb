81f2cb00b61957628bc9d7628163ccac14048304
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-11 23:13:12 +0000

    Rails3: scm: mercurial: fix error of test_next_nil at unit model test
    On Rails 3.1, ActiveRecord::HasManyThroughCantAssociateThroughHasOneOrManyReflection error raises.
    For more details, see r7062.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7106 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/repository_mercurial_test.rb
+++ test/unit/repository_mercurial_test.rb
@@ -292,8 +292,10 @@ class RepositoryMercurialTest < ActiveSupport::TestCase
     end
 
     def test_next_nil
+      assert_equal 0, @repository.changesets.count
       @repository.fetch_changesets
-      @repository.reload
+      @project.reload
+      assert_equal NUM_REV, @repository.changesets.count
       %w|28 3ae45e2d177d 3ae45|.each do |r1|
         changeset = @repository.find_changeset_by_name(r1)
         assert_nil changeset.next
