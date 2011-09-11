edb5b069e74feb2001ea863c20943ddd60bc1fc3
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-11 23:21:18 +0000

    Rails3: scm: mercurial: fix error of test_format_identifier at unit model test
    On Rails 3.1, ActiveRecord::HasManyThroughCantAssociateThroughHasOneOrManyReflection error raises.
    For more details, see r7062.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7114 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/repository_mercurial_test.rb
+++ test/unit/repository_mercurial_test.rb
@@ -234,8 +234,10 @@ class RepositoryMercurialTest < ActiveSupport::TestCase
     end
 
     def test_format_identifier
+      assert_equal 0, @repository.changesets.count
       @repository.fetch_changesets
-      @repository.reload
+      @project.reload
+      assert_equal NUM_REV, @repository.changesets.count
       c = @repository.changesets.find_by_revision('2')
       assert_equal '2:400bb8672109', c.format_identifier
     end
