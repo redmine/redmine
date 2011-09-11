c4618ec14117a4dc6bf0a5a77c8f70a4a281e446
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-11 23:20:37 +0000

    Rails3: scm: mercurial: fix error of test_identifier at unit model test
    On Rails 3.1, ActiveRecord::HasManyThroughCantAssociateThroughHasOneOrManyReflection error raises.
    For more details, see r7062.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7113 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/repository_mercurial_test.rb
+++ test/unit/repository_mercurial_test.rb
@@ -225,8 +225,10 @@ class RepositoryMercurialTest < ActiveSupport::TestCase
     end
 
     def test_identifier
+      assert_equal 0, @repository.changesets.count
       @repository.fetch_changesets
-      @repository.reload
+      @project.reload
+      assert_equal NUM_REV, @repository.changesets.count
       c = @repository.changesets.find_by_revision('2')
       assert_equal c.scmid, c.identifier
     end
