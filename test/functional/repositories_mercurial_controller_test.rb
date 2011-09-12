cf8a8720cef0c4ed1546682c57187f54147be93f
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-12 14:02:24 +0000

    Rails3: scm: mercurial: fix error of test_destroy_valid_repository at functional test
    On Rails 3.1, ActiveRecord::HasManyThroughCantAssociateThroughHasOneOrManyReflection error raises.
    For more details, see r7062.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7205 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/repositories_mercurial_controller_test.rb
+++ test/functional/repositories_mercurial_controller_test.rb
@@ -443,9 +443,10 @@ class RepositoriesMercurialControllerTest < ActionController::TestCase
 
     def test_destroy_valid_repository
       @request.session[:user_id] = 1 # admin
+      assert_equal 0, @repository.changesets.count
       @repository.fetch_changesets
-      @repository.reload
-      assert @repository.changesets.count > 0
+      @project.reload
+      assert_equal NUM_REV, @repository.changesets.count
 
       get :destroy, :id => PRJ_ID
       assert_response 302
