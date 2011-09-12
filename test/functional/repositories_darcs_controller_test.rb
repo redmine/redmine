bb53890dbb1406d00f2c8b93080736ac51da7d2b
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-12 04:08:51 +0000

    Rails3: scm: darcs: fix error of test_browse_directory at functional test
    On Rails 3.1, ActiveRecord::HasManyThroughCantAssociateThroughHasOneOrManyReflection error raises.
    For more details, see r7062.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7156 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/repositories_darcs_controller_test.rb
+++ test/functional/repositories_darcs_controller_test.rb
@@ -60,8 +60,10 @@ class RepositoriesDarcsControllerTest < ActionController::TestCase
     end
 
     def test_browse_directory
+      assert_equal 0, @repository.changesets.count
       @repository.fetch_changesets
-      @repository.reload
+      @project.reload
+      assert_equal NUM_REV, @repository.changesets.count
       get :show, :id => PRJ_ID, :path => ['images']
       assert_response :success
       assert_template 'show'
