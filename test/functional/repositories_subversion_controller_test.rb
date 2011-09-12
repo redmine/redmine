887c27bb915211934b395764e9065fd8a74f2446
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-12 13:09:06 +0000

    Rails3: scm: subversion: fix error of test_entry_download at functional test
    On Rails 3.1, ActiveRecord::HasManyThroughCantAssociateThroughHasOneOrManyReflection error raises.
    For more details, see r7062.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7204 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/repositories_subversion_controller_test.rb
+++ test/functional/repositories_subversion_controller_test.rb
@@ -188,8 +188,10 @@ class RepositoriesSubversionControllerTest < ActionController::TestCase
     end
 
     def test_entry_download
+      assert_equal 0, @repository.changesets.count
       @repository.fetch_changesets
-      @repository.reload
+      @project.reload
+      assert_equal NUM_REV, @repository.changesets.count
       get :entry, :id => PRJ_ID, :path => ['subversion_test', 'helloworld.c'], :format => 'raw'
       assert_response :success
       assert_template ''
