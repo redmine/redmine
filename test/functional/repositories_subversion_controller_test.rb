62a1e1dc50662f8914ef372c79e8ee0e2254e6d9
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-12 13:06:38 +0000

    Rails3: scm: subversion: fix error of test_file_changes at functional test
    On Rails 3.1, ActiveRecord::HasManyThroughCantAssociateThroughHasOneOrManyReflection error raises.
    For more details, see r7062.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7198 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/repositories_subversion_controller_test.rb
+++ test/functional/repositories_subversion_controller_test.rb
@@ -102,8 +102,10 @@ class RepositoriesSubversionControllerTest < ActionController::TestCase
     end
 
     def test_file_changes
+      assert_equal 0, @repository.changesets.count
       @repository.fetch_changesets
-      @repository.reload
+      @project.reload
+      assert_equal NUM_REV, @repository.changesets.count
       get :changes, :id => PRJ_ID, :path => ['subversion_test', 'folder', 'helloworld.rb' ]
       assert_response :success
       assert_template 'changes'
