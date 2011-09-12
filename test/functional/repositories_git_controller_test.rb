a3184147127d4722c9e2b6320c7eccd16e829fe8
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-12 02:56:23 +0000

    Rails3: scm: git: fix error of test_diff at functional test
    On Rails 3.1, ActiveRecord::HasManyThroughCantAssociateThroughHasOneOrManyReflection error raises.
    For more details, see r7062.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7139 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/repositories_git_controller_test.rb
+++ test/functional/repositories_git_controller_test.rb
@@ -214,8 +214,10 @@ class RepositoriesGitControllerTest < ActionController::TestCase
     end
 
     def test_diff
+      assert_equal 0, @repository.changesets.count
       @repository.fetch_changesets
-      @repository.reload
+      @project.reload
+      assert_equal NUM_REV, @repository.changesets.count
       # Full diff of changeset 2f9c0091
       ['inline', 'sbs'].each do |dt|
         get :diff,
