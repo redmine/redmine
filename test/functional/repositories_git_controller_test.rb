1cae0ae4157d1c581ee1977f5fde48515c1648a9
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-12 02:55:40 +0000

    Rails3: scm: git: fix error of test_browse_at_given_revision at functional test
    On Rails 3.1, ActiveRecord::HasManyThroughCantAssociateThroughHasOneOrManyReflection error raises.
    For more details, see r7062.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7138 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/repositories_git_controller_test.rb
+++ test/functional/repositories_git_controller_test.rb
@@ -143,8 +143,10 @@ class RepositoriesGitControllerTest < ActionController::TestCase
     end
 
     def test_browse_at_given_revision
+      assert_equal 0, @repository.changesets.count
       @repository.fetch_changesets
-      @repository.reload
+      @project.reload
+      assert_equal NUM_REV, @repository.changesets.count
       get :show, :id => PRJ_ID, :path => ['images'],
           :rev => '7234cb2750b63f47bff735edc50a1c0a433c2518'
       assert_response :success
