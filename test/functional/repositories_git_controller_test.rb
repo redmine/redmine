4ff3d0d639c2e5ac08f457f5a0a4cd518bc0965a
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-12 02:58:33 +0000

    Rails3: scm: git: fix error of test_revision at functional test
    On Rails 3.1, ActiveRecord::HasManyThroughCantAssociateThroughHasOneOrManyReflection error raises.
    For more details, see r7062.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7143 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/repositories_git_controller_test.rb
+++ test/functional/repositories_git_controller_test.rb
@@ -375,8 +375,10 @@ class RepositoriesGitControllerTest < ActionController::TestCase
     end
 
     def test_revision
+      assert_equal 0, @repository.changesets.count
       @repository.fetch_changesets
-      @repository.reload
+      @project.reload
+      assert_equal NUM_REV, @repository.changesets.count
       ['61b685fbe55ab05b5ac68402d5720c1a6ac973d1', '61b685f'].each do |r|
         get :revision, :id => PRJ_ID, :rev => r
         assert_response :success
