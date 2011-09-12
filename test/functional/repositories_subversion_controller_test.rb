5d187b4fa553b9ee72b8fd0c13e05be226a3a37b
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-12 11:15:18 +0000

    Rails3: scm: subversion: fix error of test_show at unit model test
    On Rails 3.1, ActiveRecord::HasManyThroughCantAssociateThroughHasOneOrManyReflection error raises.
    For more details, see r7062.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7173 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/repositories_subversion_controller_test.rb
+++ test/functional/repositories_subversion_controller_test.rb
@@ -44,8 +44,10 @@ class RepositoriesSubversionControllerTest < ActionController::TestCase
 
   if repository_configured?('subversion')
     def test_show
+      assert_equal 0, @repository.changesets.count
       @repository.fetch_changesets
-      @repository.reload
+      @project.reload
+      assert_equal NUM_REV, @repository.changesets.count
       get :show, :id => PRJ_ID
       assert_response :success
       assert_template 'show'
