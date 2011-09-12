08380e78b633ecb57a5c2962cd206bb813ebce66
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-12 11:16:10 +0000

    Rails3: scm: cvs: fix error of test_annotate at unit model test
    On Rails 3.1, ActiveRecord::HasManyThroughCantAssociateThroughHasOneOrManyReflection error raises.
    For more details, see r7062.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7175 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/repositories_cvs_controller_test.rb
+++ test/functional/repositories_cvs_controller_test.rb
@@ -181,8 +181,10 @@ class RepositoriesCvsControllerTest < ActionController::TestCase
     end
 
     def test_annotate
+      assert_equal 0, @repository.changesets.count
       @repository.fetch_changesets
-      @repository.reload
+      @project.reload
+      assert_equal NUM_REV, @repository.changesets.count
       get :annotate, :id => PRJ_ID, :path => ['sources', 'watchers_controller.rb']
       assert_response :success
       assert_template 'annotate'
