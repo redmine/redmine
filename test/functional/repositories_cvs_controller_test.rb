7b3a576443aa221f9e1c3bcfebab2270b62e7164
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-12 11:15:45 +0000

    Rails3: scm: cvs: fix error of test_diff_new_files at unit model test
    On Rails 3.1, ActiveRecord::HasManyThroughCantAssociateThroughHasOneOrManyReflection error raises.
    For more details, see r7062.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7174 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/repositories_cvs_controller_test.rb
+++ test/functional/repositories_cvs_controller_test.rb
@@ -159,8 +159,10 @@ class RepositoriesCvsControllerTest < ActionController::TestCase
     end
 
     def test_diff_new_files
+      assert_equal 0, @repository.changesets.count
       @repository.fetch_changesets
-      @repository.reload
+      @project.reload
+      assert_equal NUM_REV, @repository.changesets.count
       ['inline', 'sbs'].each do |dt|
         get :diff, :id => PRJ_ID, :rev => 1, :type => dt
         assert_response :success
