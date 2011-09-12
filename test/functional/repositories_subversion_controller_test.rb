89476adc7f5b844d09747fbf4e4ecf6f1a923ddc
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-12 13:05:14 +0000

    Rails3: scm: subversion: fix error of test_browse_root at functional test
    On Rails 3.1, ActiveRecord::HasManyThroughCantAssociateThroughHasOneOrManyReflection error raises.
    For more details, see r7062.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7195 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/repositories_subversion_controller_test.rb
+++ test/functional/repositories_subversion_controller_test.rb
@@ -56,8 +56,10 @@ class RepositoriesSubversionControllerTest < ActionController::TestCase
     end
 
     def test_browse_root
+      assert_equal 0, @repository.changesets.count
       @repository.fetch_changesets
-      @repository.reload
+      @project.reload
+      assert_equal NUM_REV, @repository.changesets.count
       get :show, :id => PRJ_ID
       assert_response :success
       assert_template 'show'
