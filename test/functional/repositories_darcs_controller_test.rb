4bff1e94ce08f41c260827f2a228a6f4d0540356
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-12 04:10:19 +0000

    Rails3: scm: darcs: fix error of test_changes at functional test
    On Rails 3.1, ActiveRecord::HasManyThroughCantAssociateThroughHasOneOrManyReflection error raises.
    For more details, see r7062.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7158 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/repositories_darcs_controller_test.rb
+++ test/functional/repositories_darcs_controller_test.rb
@@ -88,8 +88,10 @@ class RepositoriesDarcsControllerTest < ActionController::TestCase
     end
 
     def test_changes
+      assert_equal 0, @repository.changesets.count
       @repository.fetch_changesets
-      @repository.reload
+      @project.reload
+      assert_equal NUM_REV, @repository.changesets.count
       get :changes, :id => PRJ_ID, :path => ['images', 'edit.png']
       assert_response :success
       assert_template 'changes'
