38a0cac5d061abcfa31817740c443d91fab4cfdd
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-12 02:44:31 +0000

    Rails3: scm: cvs: fix error of test_entries_invalid_revision at unit model test
    On Rails 3.1, ActiveRecord::HasManyThroughCantAssociateThroughHasOneOrManyReflection error raises.
    For more details, see r7062.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7135 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/repository_cvs_test.rb
+++ test/unit/repository_cvs_test.rb
@@ -114,8 +114,10 @@ class RepositoryCvsTest < ActiveSupport::TestCase
     end
 
     def test_entries_invalid_revision
+      assert_equal 0, @repository.changesets.count
       @repository.fetch_changesets
-      @repository.reload
+      @project.reload
+      assert_equal CHANGESETS_NUM, @repository.changesets.count
       assert_nil @repository.entries('', '123')
     end
 
