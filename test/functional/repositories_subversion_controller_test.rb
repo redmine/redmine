345b0238fc3ca54e3b840be39c0119c801912961
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-12 12:53:34 +0000

    Rails3: scm: subversion: fix error of test_empty_revision at functional test
    On Rails 3.1, ActiveRecord::HasManyThroughCantAssociateThroughHasOneOrManyReflection error raises.
    For more details, see r7062.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7188 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/repositories_subversion_controller_test.rb
+++ test/functional/repositories_subversion_controller_test.rb
@@ -228,8 +228,10 @@ class RepositoriesSubversionControllerTest < ActionController::TestCase
     end
 
     def test_empty_revision
+      assert_equal 0, @repository.changesets.count
       @repository.fetch_changesets
-      @repository.reload
+      @project.reload
+      assert_equal NUM_REV, @repository.changesets.count
       ['', ' ', nil].each do |r|
         get :revision, :id => PRJ_ID, :rev => r
         assert_response 404
