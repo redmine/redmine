f7bdf375cd7b55cd0e987c6ef360fcab90ac7d06
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-23 11:58:39 +0000

    code layout clean up of test_errors_full_messages_should_include_custom_fields_errors at test/unit/issue_test.rb
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7473 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/issue_test.rb
+++ test/unit/issue_test.rb
@@ -185,7 +185,9 @@ class IssueTest < ActiveSupport::TestCase
   def test_errors_full_messages_should_include_custom_fields_errors
     field = IssueCustomField.find_by_name('Database')
 
-    issue = Issue.new(:project_id => 1, :tracker_id => 1, :author_id => 1, :status_id => 1, :subject => 'test_create', :description => 'IssueTest#test_create_with_required_custom_field')
+    issue = Issue.new(:project_id => 1, :tracker_id => 1, :author_id => 1,
+                      :status_id => 1, :subject => 'test_create',
+                      :description => 'IssueTest#test_create_with_required_custom_field')
     assert issue.available_custom_fields.include?(field)
     # Invalid value
     issue.custom_field_values = { field.id => 'SQLServer' }
