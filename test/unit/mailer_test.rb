b3c5f812c94b571f5c2be9fde893bd3f40e883f0
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-24 11:11:03 +0000

    code layout clean up of test_generated_links_in_emails at test/unit/mailer_test.rb
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7498 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/mailer_test.rb
+++ test/unit/mailer_test.rb
@@ -47,11 +47,19 @@ class MailerTest < ActiveSupport::TestCase
 
     assert_select_email do
       # link to the main ticket
-      assert_select "a[href=?]", "https://mydomain.foo/issues/1", :text => "Bug #1: Can't print recipes"
+      assert_select "a[href=?]",
+                    "https://mydomain.foo/issues/1",
+                    :text => "Bug #1: Can't print recipes"
       # link to a referenced ticket
-      assert_select "a[href=?][title=?]", "https://mydomain.foo/issues/2", "Add ingredients categories (Assigned)", :text => "#2"
+      assert_select "a[href=?][title=?]",
+                    "https://mydomain.foo/issues/2",
+                    "Add ingredients categories (Assigned)",
+                    :text => "#2"
       # link to a changeset
-      assert_select "a[href=?][title=?]", "https://mydomain.foo/projects/ecookbook/repository/revisions/2", "This commit fixes #1, #2 and references #1 &amp; #3", :text => "r2"
+      assert_select "a[href=?][title=?]",
+                    "https://mydomain.foo/projects/ecookbook/repository/revisions/2",
+                    "This commit fixes #1, #2 and references #1 &amp; #3",
+                    :text => "r2"
     end
   end
 
