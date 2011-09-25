82c86513ad1879049c7188e9e0c1cec2aedc27c3
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-25 06:13:23 +0000

    Rails3: replace "all" fixtures at test/functional/files_controller_test.rb
    Following error raises.
    Fixture::FormatError: Bad data for Configuration::Empty fixture named default (nil)
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7522 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/files_controller_test.rb
+++ test/functional/files_controller_test.rb
@@ -1,7 +1,18 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class FilesControllerTest < ActionController::TestCase
-  fixtures :all
+  fixtures :projects, :trackers, :issue_statuses, :issues,
+           :enumerations, :users, :issue_categories,
+           :projects_trackers,
+           :roles,
+           :member_roles,
+           :members,
+           :enabled_modules,
+           :workflows,
+           :journals, :journal_details,
+           :issues, :issue_statuses, :issue_categories,
+           :attachments,
+           :versions
 
   def setup
     @controller = FilesController.new
