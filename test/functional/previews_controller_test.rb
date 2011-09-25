9c650540baa026da37af7316fa14662d61ce70ec
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-25 10:46:44 +0000

    code layout clean up of test_preview_issue_notes at test/functional/previews_controller_test.rb
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7533 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/previews_controller_test.rb
+++ test/functional/previews_controller_test.rb
@@ -38,7 +38,9 @@ class PreviewsControllerTest < ActionController::TestCase
 
   def test_preview_issue_notes
     @request.session[:user_id] = 2
-    post :issue, :project_id => '1', :id => 1, :issue => {:description => Issue.find(1).description}, :notes => 'Foo'
+    post :issue, :project_id => '1', :id => 1,
+         :issue => {:description => Issue.find(1).description},
+         :notes => 'Foo'
     assert_response :success
     assert_template 'preview'
     assert_not_nil assigns(:notes)
