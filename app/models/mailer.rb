a4c4edacfdf7665c29115b8707cb2814c11abaaa
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-09-26 16:22:38 +0000

    Rails3: mail: rename app/views/mailer/*.text.plain.rhtml to app/views/mailer/*.text.erb (#6317)
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7536 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -420,13 +420,13 @@ class Mailer < ActionMailer::Base
   def render_multipart(method_name, body)
     if Setting.plain_text_mail?
       content_type "text/plain"
-      body render(:file => "#{method_name}.text.plain.rhtml",
+      body render(:file => "#{method_name}.text.erb",
                   :body => body,
                   :layout => 'mailer.text.erb')
     else
       content_type "multipart/alternative"
       part :content_type => "text/plain",
-           :body => render(:file => "#{method_name}.text.plain.rhtml",
+           :body => render(:file => "#{method_name}.text.erb",
                            :body => body, :layout => 'mailer.text.erb')
       part :content_type => "text/html",
            :body => render_message("#{method_name}.text.html.rhtml", body)
