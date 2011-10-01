d817b063ece4045182cd7b5007dea2b723767d6f
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-10-01 01:10:18 +0000

    code layout clean up of test/unit/message_test.rb
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7553 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/message_test.rb
+++ test/unit/message_test.rb
@@ -29,7 +29,9 @@ class MessageTest < ActiveSupport::TestCase
     topics_count = @board.topics_count
     messages_count = @board.messages_count
 
-    message = Message.new(:board => @board, :subject => 'Test message', :content => 'Test message content', :author => @user)
+    message = Message.new(:board => @board, :subject => 'Test message',
+                          :content => 'Test message content',
+                          :author => @user)
     assert message.save
     @board.reload
     # topics count incremented
@@ -48,7 +50,9 @@ class MessageTest < ActiveSupport::TestCase
     replies_count = @message.replies_count
 
     reply_author = User.find(2)
-    reply = Message.new(:board => @board, :subject => 'Test reply', :content => 'Test reply content', :parent => @message, :author => reply_author)
+    reply = Message.new(:board => @board, :subject => 'Test reply',
+                        :content => 'Test reply content',
+                        :parent => @message, :author => reply_author)
     assert reply.save
     @board.reload
     # same topics count
