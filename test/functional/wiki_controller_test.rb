fe386fe50d4d465e921f11b4a5be266901eb42c3
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-11-06 09:17:10 +0000

    code layout clean up of test/functional/wiki_controller_test.rb
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7738 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/wiki_controller_test.rb
+++ test/functional/wiki_controller_test.rb
@@ -22,7 +22,9 @@ require 'wiki_controller'
 class WikiController; def rescue_action(e) raise e end; end
 
 class WikiControllerTest < ActionController::TestCase
-  fixtures :projects, :users, :roles, :members, :member_roles, :enabled_modules, :wikis, :wiki_pages, :wiki_contents, :wiki_content_versions, :attachments
+  fixtures :projects, :users, :roles, :members, :member_roles,
+           :enabled_modules, :wikis, :wiki_pages, :wiki_contents,
+           :wiki_content_versions, :attachments
 
   def setup
     @controller = WikiController.new
