8c560fa5811b4f79c22232a94ae686d0ebf1ae8b
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-11-06 09:16:26 +0000

    remove trailing white-spaces from db/migrate/028_create_wiki_pages.rb (#9510)
    Contributed by Igor Zubkov.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7737 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- db/migrate/028_create_wiki_pages.rb
+++ db/migrate/028_create_wiki_pages.rb
@@ -3,7 +3,7 @@ class CreateWikiPages < ActiveRecord::Migration
     create_table :wiki_pages do |t|
       t.column :wiki_id, :integer, :null => false
       t.column :title, :string, :limit => 255, :null => false
-      t.column :created_on, :datetime, :null => false    
+      t.column :created_on, :datetime, :null => false
     end
     add_index :wiki_pages, [:wiki_id, :title], :name => :wiki_pages_wiki_id_title
   end
