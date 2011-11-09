cedeee2e16d21a7bc6e9ad387eb21734deaf374a
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-11-09 08:11:36 +0000

    replace tabs to spaces at db/migrate/096_add_commit_access_permission.rb (#9510)
    Contributed by Igor Zubkov.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7757 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- db/migrate/096_add_commit_access_permission.rb
+++ db/migrate/096_add_commit_access_permission.rb
@@ -1,14 +1,13 @@
 class AddCommitAccessPermission < ActiveRecord::Migration
-
   def self.up
-	Role.find(:all).select { |r| not r.builtin? }.each do |r|
-	     r.add_permission!(:commit_access)
-  	end
+    Role.find(:all).select { |r| not r.builtin? }.each do |r|
+      r.add_permission!(:commit_access)
+    end
   end
 
   def self.down
-	Role.find(:all).select { |r| not r.builtin? }.each do |r|
-	     r.remove_permission!(:commit_access)
-  	end
+    Role.find(:all).select { |r| not r.builtin? }.each do |r|
+      r.remove_permission!(:commit_access)
+    end
   end
 end
