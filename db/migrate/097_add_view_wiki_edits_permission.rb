47bcd7f3c31d8c779c7b3e4b3937e813c22a01c0
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-11-09 08:11:58 +0000

    replace tabs to spaces at db/migrate/097_add_view_wiki_edits_permission.rb (#9510)
    Contributed by Igor Zubkov.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7758 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- db/migrate/097_add_view_wiki_edits_permission.rb
+++ db/migrate/097_add_view_wiki_edits_permission.rb
@@ -1,13 +1,13 @@
 class AddViewWikiEditsPermission < ActiveRecord::Migration
   def self.up
-	Role.find(:all).each do |r|
-	  r.add_permission!(:view_wiki_edits) if r.has_permission?(:view_wiki_pages)
-  	end
+    Role.find(:all).each do |r|
+      r.add_permission!(:view_wiki_edits) if r.has_permission?(:view_wiki_pages)
+    end
   end
 
   def self.down
-	Role.find(:all).each do |r|
-	  r.remove_permission!(:view_wiki_edits)
-  	end
+    Role.find(:all).each do |r|
+      r.remove_permission!(:view_wiki_edits)
+    end
   end
 end
