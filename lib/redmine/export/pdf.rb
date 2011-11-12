c4d9c29de94dfbd229baae0c9fe0b406315b6a5b
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-11-12 11:16:26 +0000

    pdf: remove unused "require 'rfpdf/fpdf'" (#8312)
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7785 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/export/pdf.rb
+++ lib/redmine/export/pdf.rb
@@ -18,7 +18,6 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 require 'iconv'
-require 'rfpdf/fpdf'
 require 'fpdf/chinese'
 require 'fpdf/japanese'
 require 'fpdf/korean'
