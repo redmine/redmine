6dab391932ef31af465342b75ae8b3578aa86f65
Jean-Philippe Lang <jp_lang@yahoo.fr>
2011-11-24 20:39:24 +0000

    Use user's time zone when logging time while editing ticket (#9619).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@7918 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -616,7 +616,7 @@ class Issue < ActiveRecord::Base
         @time_entry.project = project
         @time_entry.issue = self
         @time_entry.user = User.current
-        @time_entry.spent_on = Date.today
+        @time_entry.spent_on = User.current.today
         @time_entry.attributes = params[:time_entry]
         self.time_entries << @time_entry
       end
