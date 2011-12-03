23fd43374d714e765bf0796ad22ecf3ab815fbc9
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-12-03 14:33:49 +0000

    Rails3: model: replace deprecated validate method at member model
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@8047 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/member.rb
+++ app/models/member.rb
@@ -24,6 +24,7 @@ class Member < ActiveRecord::Base
 
   validates_presence_of :principal, :project
   validates_uniqueness_of :user_id, :scope => :project_id
+  validate :validate_role
 
   after_destroy :unwatch_from_permission_change
 
@@ -81,7 +82,7 @@ class Member < ActiveRecord::Base
 
   protected
 
-  def validate
+  def validate_role
     errors.add_on_empty :role if member_roles.empty? && roles.empty?
   end
 
