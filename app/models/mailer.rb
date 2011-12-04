afbdc51e01813d82a12cbb59d456369ae4c9ae6f
Jean-Philippe Lang <jp_lang@yahoo.fr>
2011-12-04 23:44:59 +0000

    Removed ARCondition.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@8089 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -15,8 +15,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-require 'ar_condition'
-
 class Mailer < ActionMailer::Base
   layout 'mailer'
   helper :application
