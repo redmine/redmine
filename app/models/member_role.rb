1de2c1c2af3672ffa6834e77e8c735152f760db9
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-12-04 05:21:20 +0000

    Rails3: model: replace deprecated validate method at member_role model
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@8068 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/member_role.rb
+++ app/models/member_role.rb
@@ -25,8 +25,9 @@ class MemberRole < ActiveRecord::Base
   after_destroy :remove_role_from_group_users
 
   validates_presence_of :role
+  validate :validate_role_member
 
-  def validate
+  def validate_role_member
     errors.add :role_id, :invalid if role && !role.member?
   end
 
