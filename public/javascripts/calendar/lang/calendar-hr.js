d4246334e0f6b712894f39fda7c6c02039035427
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-12-07 01:21:06 +0000

    Merged r8105 from trunk
    convert calendar-hr.js (Croatian) from Windows-1250 to UTF-8.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/branches/1.2-stable@8107 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- public/javascripts/calendar/lang/calendar-hr.js
+++ public/javascripts/calendar/lang/calendar-hr.js
@@ -51,7 +51,7 @@ Calendar._FD = 0;
 Calendar._MN = new Array
 ("Sijecanj",
  "Veljaca",
- "Oûujak",
+ "O≈æujak",
  "Travanj",
  "Svibanj",
  "Lipanj",
@@ -66,7 +66,7 @@ Calendar._MN = new Array
 Calendar._SMN = new Array
 ("Sij",
  "Velj",
- "Oûu",
+ "O≈æu",
  "Tra",
  "Svi",
  "Lip",
@@ -100,7 +100,7 @@ Calendar._TT["ABOUT_TIME"] = "\n\n" +
 
 Calendar._TT["PREV_YEAR"] = "Prethodna godina (hold for menu)";
 Calendar._TT["PREV_MONTH"] = "Prethodni mjesec (hold for menu)";
-Calendar._TT["GO_TODAY"] = "Na danaönji dan";   
+Calendar._TT["GO_TODAY"] = "Na dana≈°nji dan";   
 Calendar._TT["NEXT_MONTH"] = "Naredni mjesec (hold for menu)";
 Calendar._TT["NEXT_YEAR"] = "Naredna godina (hold for menu)";
 Calendar._TT["SEL_DATE"] = "Odaberite datum";
@@ -109,7 +109,7 @@ Calendar._TT["PART_TODAY"] = " (Danas)";
 
 // the following is to inform that "%s" is to be the first day of week
 // %s will be replaced with the day name.
-Calendar._TT["DAY_FIRST"] = "Prikaûi %s prvo";
+Calendar._TT["DAY_FIRST"] = "Prika≈æi %s prvo";
 
 // This may be locale-dependent.  It specifies the week-end days, as an array
 // of comma-separated numbers.  The numbers are from 0 to 6: 0 means Sunday, 1
