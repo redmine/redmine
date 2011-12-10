eb15c9eaad429410da447f17f6f7187f72f41632
Jean-Philippe Lang <jp_lang@yahoo.fr>
2011-12-10 17:06:46 +0000

    Role.all broken in migration due to default sort (columns do not exist yet). Use raw SQL instead.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@8182 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- db/migrate/020_add_role_position.rb
+++ db/migrate/020_add_role_position.rb
@@ -1,7 +1,7 @@
 class AddRolePosition < ActiveRecord::Migration
   def self.up
     add_column :roles, :position, :integer, :default => 1
-    Role.find(:all).each_with_index {|role, i| role.update_attribute(:position, i+1)}
+    Role.update_all("position = (SELECT COUNT(*) FROM #{Role.table_name} r WHERE r.id < id) + 1")
   end
 
   def self.down
