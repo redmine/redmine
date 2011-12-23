c18d24434a97a491380d972d9b0086e9b457b593
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-12-23 23:06:52 +0000

    test: route: add account test
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@8326 e93f8b46-1217-0410-a6f0-8f06a7374b81

---
+++ test/integration/routing/account_test.rb
@@ -0,0 +1,55 @@
+# Redmine - project management software
+# Copyright (C) 2006-2011  Jean-Philippe Lang
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+require File.expand_path('../../../test_helper', __FILE__)
+
+class RoutingAccountTest < ActionController::IntegrationTest
+  def test_account
+    assert_routing(
+        { :method => 'get', :path => "/login" },
+        { :controller => 'account', :action => 'login' }
+      )
+    assert_routing(
+        { :method => 'post', :path => "/login" },
+        { :controller => 'account', :action => 'login' }
+      )
+    assert_routing(
+        { :method => 'get', :path => "/logout" },
+        { :controller => 'account', :action => 'logout' }
+      )
+    assert_routing(
+        { :method => 'get', :path => "/account/register" },
+        { :controller => 'account', :action => 'register' }
+      )
+    assert_routing(
+        { :method => 'post', :path => "/account/register" },
+        { :controller => 'account', :action => 'register' }
+      )
+    assert_routing(
+        { :method => 'get', :path => "/account/lost_password" },
+        { :controller => 'account', :action => 'lost_password' }
+      )
+    assert_routing(
+        { :method => 'post', :path => "/account/lost_password" },
+        { :controller => 'account', :action => 'lost_password' }
+      )
+    assert_routing(
+        { :method => 'get', :path => "/account/activate" },
+        { :controller => 'account', :action => 'activate' }
+      )
+  end
+end
