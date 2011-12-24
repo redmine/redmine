bb7854923d00580eb3d649fe05f72b8f67ee5f11
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-12-24 01:04:26 +0000

    test: route: add 'my' test
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@8332 e93f8b46-1217-0410-a6f0-8f06a7374b81

---
+++ test/integration/routing/my_test.rb
@@ -0,0 +1,71 @@
+# Redmine - project management software
+# Copyright (C) 2006-2011  Jean-Philippe Lang
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+require File.expand_path('../../../test_helper', __FILE__)
+
+class RoutingMyTest < ActionController::IntegrationTest
+  def test_my
+    assert_routing(
+        { :method => 'get', :path => "/my/account" },
+        { :controller => 'my', :action => 'account' }
+      )
+    assert_routing(
+        { :method => 'post', :path => "/my/account" },
+        { :controller => 'my', :action => 'account' }
+      )
+    assert_routing(
+        { :method => 'get', :path => "/my/page" },
+        { :controller => 'my', :action => 'page' }
+      )
+    assert_routing(
+        { :method => 'get', :path => "/my" },
+        { :controller => 'my', :action => 'index' }
+      )
+    assert_routing(
+        { :method => 'post', :path => "/my/reset_rss_key" },
+        { :controller => 'my', :action => 'reset_rss_key' }
+      )
+    assert_routing(
+        { :method => 'post', :path => "/my/reset_api_key" },
+        { :controller => 'my', :action => 'reset_api_key' }
+      )
+    assert_routing(
+        { :method => 'get', :path => "/my/password" },
+        { :controller => 'my', :action => 'password' }
+      )
+    assert_routing(
+        { :method => 'post', :path => "/my/password" },
+        { :controller => 'my', :action => 'password' }
+      )
+    assert_routing(
+        { :method => 'get', :path => "/my/page_layout" },
+        { :controller => 'my', :action => 'page_layout' }
+      )
+    assert_routing(
+        { :method => 'post', :path => "/my/add_block" },
+        { :controller => 'my', :action => 'add_block' }
+      )
+    assert_routing(
+        { :method => 'post', :path => "/my/remove_block" },
+        { :controller => 'my', :action => 'remove_block' }
+      )
+    assert_routing(
+        { :method => 'post', :path => "/my/order_blocks" },
+        { :controller => 'my', :action => 'order_blocks' }
+      )
+  end
+end
