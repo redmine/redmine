4f9f68046cd433c49e9ade796115c6f8f27e0bb0
Jean-Philippe Lang <jp_lang@yahoo.fr>
2011-12-26 12:42:36 +0000

    Makes assertion Rails3 compatible.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@8386 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/project_test.rb
+++ test/unit/project_test.rb
@@ -126,7 +126,11 @@ class ProjectTest < ActiveSupport::TestCase
       p = Project.new
       p.identifier = identifier
       p.valid?
-      assert_equal valid, p.errors['identifier'].nil?
+      if valid
+        assert p.errors['identifier'].blank?, "identifier #{identifier} was not valid"
+      else
+        assert p.errors['identifier'].present?, "identifier #{identifier} was valid"
+      end
     end
   end
 
