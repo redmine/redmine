e3742a9ed50ddc818b237811c4ea31e775584599
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-12-27 03:00:27 +0000

    test: route: add sys test
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@8396 e93f8b46-1217-0410-a6f0-8f06a7374b81

---
+++ test/integration/routing/sys_test.rb
@@ -0,0 +1,35 @@
+# Redmine - project management software
+# Copyright (C) 2006-2011  Jean-Philippe Lang
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+require File.expand_path('../../../test_helper', __FILE__)
+
+class RoutingSysTest < ActionController::IntegrationTest
+  def test_sys
+    assert_routing(
+        { :method => 'get', :path => "/sys/projects" },
+        { :controller => 'sys', :action => 'projects' }
+      )
+    assert_routing(
+        { :method => 'post', :path => "/sys/projects/testid/repository" },
+        { :controller => 'sys', :action => 'create_project_repository', :id => 'testid' }
+      )
+    assert_routing(
+        { :method => 'get', :path => "/sys/fetch_changesets" },
+        { :controller => 'sys', :action => 'fetch_changesets' }
+      )
+  end
+end
