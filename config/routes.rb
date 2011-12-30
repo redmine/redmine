510647e864758766db7f2e2d9ec1bbb9f4f67a35
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2011-12-30 02:16:22 +0000

    route: code layout clean up previews route
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@8428 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,9 @@ ActionController::Routing::Routes.draw do |map|
   # Misc issue routes. TODO: move into resources
   map.auto_complete_issues '/issues/auto_complete', :controller => 'auto_completes',
                            :action => 'issues', :conditions => { :method => :get }
-  map.preview_issue '/issues/preview/:id', :controller => 'previews', :action => 'issue' # TODO: would look nicer as /issues/:id/preview
+  # TODO: would look nicer as /issues/:id/preview
+  map.preview_issue '/issues/preview/:id', :controller => 'previews',
+                    :action => 'issue'
   map.issues_context_menu '/issues/context_menu', :controller => 'context_menus', :action => 'issues'
   map.issue_changes '/issues/changes', :controller => 'journals', :action => 'index'
   map.quoted_issue '/issues/:id/quoted', :controller => 'journals', :action => 'new',
