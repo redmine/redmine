1b61fbf739fed19747f4ad4c784586323d2cb3e7
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2012-01-02 09:07:57 +0000

    Rails3.1: test: include ERB::Util in unit/helpers/search_helper_test.rb
    undefined method `h' error raises on Rails 3.1.3 and Ruby 1.8.7.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@8467 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/helpers/search_helper_test.rb
+++ test/unit/helpers/search_helper_test.rb
@@ -21,6 +21,7 @@ require File.expand_path('../../../test_helper', __FILE__)
 
 class SearchHelperTest < ActionView::TestCase
   include SearchHelper
+  include ERB::Util
 
   def test_highlight_single_token
     assert_equal 'This is a <span class="highlight token-0">token</span>.',
