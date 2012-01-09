77b58fc3511d48ada103c22238cf693aec40761a
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2012-01-09 15:07:48 +0000

    Merged r8571, r8572, r8573, r8574, r8578, r8582, r8588, r8589 from trunk
    Arabic translations (#9943).
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/branches/1.3-stable@8590 e93f8b46-1217-0410-a6f0-8f06a7374b81

---
+++ public/javascripts/jstoolbar/lang/jstoolbar-ar.js
@@ -0,0 +1,16 @@
+jsToolBar.strings = {}; 
+jsToolBar.strings ['Strong'] = 'قوي';
+jsToolBar.strings ['Italic'] = 'مائل';
+jsToolBar.strings ['Underline'] = 'تسطير';
+jsToolBar.strings ['Deleted'] = 'محذوف';
+jsToolBar.strings ['Code'] = 'رمز ضمني';
+jsToolBar.strings ['Heading 1'] = 'عنوان 1';
+jsToolBar.strings ['Heading 2'] = 'عنوان 2';
+jsToolBar.strings ['Heading 3'] = 'عنوان 3';
+jsToolBar.strings ['Unordered list'] = 'قائمة غير مرتبة';
+jsToolBar.strings ['Ordered list'] = 'قائمة مرتبة';
+jsToolBar.strings ['Quote'] = 'اقتباس';
+jsToolBar.strings ['Unquote'] = 'إزالة الاقتباس';
+jsToolBar.strings ['Preformatted text'] = 'نص مسبق التنسيق';
+jsToolBar.strings ['Wiki link'] = 'رابط الى صفحة ويكي';
+jsToolBar.strings ['Image'] = 'صورة';
