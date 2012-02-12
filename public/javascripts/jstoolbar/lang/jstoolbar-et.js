e14a9f5503a909482d07e38a27c1718aa24cf528
Jean-Philippe Lang <jp_lang@yahoo.fr>
2012-02-12 10:56:45 +0000

    Adds Estonian translation by Kaitseministeerium (#10208).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@8859 e93f8b46-1217-0410-a6f0-8f06a7374b81

---
+++ public/javascripts/jstoolbar/lang/jstoolbar-et.js
@@ -0,0 +1,33 @@
+/*
+ Copyright (C) 2012 Kaitseministeerium
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+jsToolBar.strings = {};
+jsToolBar.strings['Strong'] = 'Paks';
+jsToolBar.strings['Italic'] = 'Kaldkiri';
+jsToolBar.strings['Underline'] = 'Allakriipsutatud';
+jsToolBar.strings['Deleted'] = 'Läbikriipsutatud';
+jsToolBar.strings['Code'] = 'Inline Code';
+jsToolBar.strings['Heading 1'] = 'Pealkiri 1';
+jsToolBar.strings['Heading 2'] = 'Pealkiri 2';
+jsToolBar.strings['Heading 3'] = 'Pealkiri 3';
+jsToolBar.strings['Unordered list'] = 'Täpitud nimekiri';
+jsToolBar.strings['Ordered list'] = 'Nummerdatud nimekiri';
+jsToolBar.strings['Quote'] = 'Tsitaadi algus';
+jsToolBar.strings['Unquote'] = 'Tsitaadi lõpp';
+jsToolBar.strings['Preformatted text'] = 'Eelvormindatud tekst';
+jsToolBar.strings['Wiki link'] = 'Link Wiki lehele';
+jsToolBar.strings['Image'] = 'Pilt';
