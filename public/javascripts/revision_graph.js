c5317a14ac0696cac5d9ae8d3b3ddd2b61aee01c
Etienne Massip <etienne.massip@gmail.com>
2012-02-13 20:20:18 +0000

    Redraw revision graph on window resize (#10206).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@8866 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- public/javascripts/revision_graph.js
+++ public/javascripts/revision_graph.js
@@ -1,5 +1,6 @@
+var revisionGraph = null;
 
-function revisionGraph(holder, commits_hash, graph_space) {
+function drawRevisionGraph(holder, commits_hash, graph_space) {
 
     var XSTEP = 20,
         CIRCLE_INROW_OFFSET = 10;
@@ -11,21 +12,27 @@ function revisionGraph(holder, commits_hash, graph_space) {
 
     var commit_table_rows = $$('table.changesets tr.changeset');
 
+    // create graph
+    if(revisionGraph != null)
+        revisionGraph.clear();
+    else
+        revisionGraph = Raphael(holder);
+
+    var top = revisionGraph.set();
+
     // init dimensions
     var graph_offset = $(holder).getLayout().get('top'),
         graph_width = (graph_space + 1) * XSTEP,
         graph_height = commit_table_rows[max_rdmid].getLayout().get('top') + commit_table_rows[max_rdmid].getLayout().get('height') - graph_offset;
 
+    revisionGraph.setSize(graph_width, graph_height);
 
     // init colors
     var colors = [];
-    for (var k = 0; k < graph_space + 1; k++) {
+    Raphael.getColor.reset();
+    for (var k = 0; k <= graph_space; k++) {
         colors.push(Raphael.getColor());
     }
-
-    // create graph
-    var graph = Raphael(holder, graph_width, graph_height),
-        top = graph.set();
 
     var parent_commit;
     var x, y, parent_x, parent_y;
@@ -36,14 +43,14 @@ function revisionGraph(holder, commits_hash, graph_space) {
         y = commit_table_rows[max_rdmid - commit.rdmid].getLayout().get('top') - graph_offset + CIRCLE_INROW_OFFSET;
         x = XSTEP / 2 + XSTEP * commit.space;
 
-        graph.circle(x, y, 3).attr({fill: colors[commit.space], stroke: 'none'});
+        revisionGraph.circle(x, y, 3).attr({fill: colors[commit.space], stroke: 'none'});
 
         // title
         if (commit.refs != null && commit.refs != '') {
             longrefs  = commit.refs;
             shortrefs = longrefs.length > 15 ? longrefs.substr(0, 13) + '...' : longrefs;
 
-            label = graph.text(x + 5, y + 5, shortrefs)
+            label = revisionGraph.text(x + 5, y + 5, shortrefs)
                 .attr({
                     font: '12px Fontin-Sans, Arial',
                     fill: '#666',
@@ -65,26 +72,26 @@ function revisionGraph(holder, commits_hash, graph_space) {
 
                 if (parent_commit.space == commit.space) {
                     // vertical path
-                    path = graph.path([
+                    path = revisionGraph.path([
                         'M', x, y,
                         'V', parent_y]);
                 } else {
                     // path to a commit in a different branch (Bezier curve)
-                    path = graph.path([
+                    path = revisionGraph.path([
                         'M', x, y,
                         'C', x, y, x, y + (parent_y - y) / 2, x + (parent_x - x) / 2, y + (parent_y - y) / 2,
                         'C', x + (parent_x - x) / 2, y + (parent_y - y) / 2, parent_x, parent_y-(parent_y-y)/2, parent_x, parent_y]);
                 }
             } else {
-                // vertical path ending at the bottom of the graph
-                path = graph.path([
+                // vertical path ending at the bottom of the revisionGraph
+                path = revisionGraph.path([
                     'M', x, y,
                     'V', graph_height]);
             }
             path.attr({stroke: colors[commit.space], "stroke-width": 1.5});
         });
 
-        top.push(graph.circle(x, y, 10)
+        top.push(revisionGraph.circle(x, y, 10)
             .attr({
                 fill: '#000',
                 opacity: 0,
