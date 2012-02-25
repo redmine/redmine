9964e20a0326adbc2857fd1bb0c6f825bd360c20
Jean-Philippe Lang <jp_lang@yahoo.fr>
2012-02-25 10:41:22 +0000

    Adds missing format parameter.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@8992 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/repositories_controller_test.rb
+++ test/functional/repositories_controller_test.rb
@@ -163,7 +163,7 @@ class RepositoriesControllerTest < ActionController::TestCase
   def test_add_related_issue
     @request.session[:user_id] = 2
     assert_difference 'Changeset.find(103).issues.size' do
-      post :add_related_issue, :id => 1, :rev => 4, :issue_id => 2
+      post :add_related_issue, :id => 1, :rev => 4, :issue_id => 2, :format => 'js'
       assert_response :success
     end
     assert_select_rjs :replace_html, 'related-issues'
@@ -173,7 +173,7 @@ class RepositoriesControllerTest < ActionController::TestCase
   def test_add_related_issue_with_invalid_issue_id
     @request.session[:user_id] = 2
     assert_no_difference 'Changeset.find(103).issues.size' do
-      post :add_related_issue, :id => 1, :rev => 4, :issue_id => 9999
+      post :add_related_issue, :id => 1, :rev => 4, :issue_id => 9999, :format => 'js'
       assert_response :success
     end
     assert_include 'alert("Issue is invalid")', @response.body
@@ -185,7 +185,7 @@ class RepositoriesControllerTest < ActionController::TestCase
 
     @request.session[:user_id] = 2
     assert_difference 'Changeset.find(103).issues.size', -1 do
-      delete :remove_related_issue, :id => 1, :rev => 4, :issue_id => 2
+      delete :remove_related_issue, :id => 1, :rev => 4, :issue_id => 2, :format => 'js'
       assert_response :success
     end
     assert_select_rjs :remove, 'related-issue-2'
