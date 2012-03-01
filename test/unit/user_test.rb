ac46497f1abd9e2e69a0b3c4848d8825847d1934
Jean-Philippe Lang <jp_lang@yahoo.fr>
2012-03-01 19:47:03 +0000

    Fixes LDAP test.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@9045 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -483,7 +483,7 @@ class UserTest < ActiveSupport::TestCase
       context "on the fly registration" do
         setup do
           @auth_source = AuthSourceLdap.find(1)
-          @auth.update_attribute :onthefly_register, true
+          @auth_source.update_attribute :onthefly_register, true
         end
 
         context "with a successful authentication" do
