ffc6c99ae83f227ae60f81cb5ec06d210ad18efa
Jean-Philippe Lang <jp_lang@yahoo.fr>
2012-03-01 15:14:52 +0000

    Merged r9026 from trunk.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/branches/1.3-stable@9040 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/issues_controller_test.rb
+++ test/functional/issues_controller_test.rb
@@ -933,7 +933,7 @@ class IssuesControllerTest < ActionController::TestCase
   def test_post_new_with_group_assignment
     group = Group.find(11)
     project = Project.find(1)
-    project.members << Member.new(:principal => group, :roles => [Role.first])
+    project.members << Member.new(:principal => group, :roles => [Role.givable.first])
 
     with_settings :issue_group_assignment => '1' do
       @request.session[:user_id] = 2
@@ -1803,7 +1803,7 @@ class IssuesControllerTest < ActionController::TestCase
   def test_bulk_update_with_group_assignee
     group = Group.find(11)
     project = Project.find(1)
-    project.members << Member.new(:principal => group, :roles => [Role.first])
+    project.members << Member.new(:principal => group, :roles => [Role.givable.first])
 
     @request.session[:user_id] = 2
     # update issues assignee
