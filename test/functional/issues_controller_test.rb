53f9979696f0455408b300677c002b9f2d6ddab5
Jean-Philippe Lang <jp_lang@yahoo.fr>
2012-03-02 07:38:43 +0000

    Use valid filters in #test_index_with_sort_filters.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@9050 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/issues_controller_test.rb
+++ test/functional/issues_controller_test.rb
@@ -149,7 +149,6 @@ class IssuesControllerTest < ActionController::TestCase
   end
 
   def test_index_with_short_filters
-
     to_test = {
       'status_id' => {
         'o' => { :op => 'o', :values => [''] },
@@ -183,9 +182,9 @@ class IssuesControllerTest < ActionController::TestCase
         't-2' => { :op => 't-', :values => ['2'] }},
       'created_on' => {
         '>=2011-10-12' => { :op => '>=', :values => ['2011-10-12'] },
-        '<t+2' => { :op => '=', :values => ['<t+2'] },
-        '>t+2' => { :op => '=', :values => ['>t+2'] },
-        't+2' => { :op => 't', :values => ['+2'] }},
+        '<t-2' => { :op => '<t-', :values => ['2'] },
+        '>t-2' => { :op => '>t-', :values => ['2'] },
+        't-2' => { :op => 't-', :values => ['2'] }},
       'cf_1' => {
         'c' => { :op => '=', :values => ['c'] },
         '!c' => { :op => '!', :values => ['c'] },
@@ -217,7 +216,6 @@ class IssuesControllerTest < ActionController::TestCase
         assert_equal(default_filter.merge({field => {:operator => expected[:op], :values => expected[:values]}}), query.filters)
       end
     end
-
   end
 
   def test_index_with_project_and_empty_filters
