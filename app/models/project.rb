60b0a6c4b853041f7fbdd64ea69cdfb4216e13fe
Jean-Philippe Lang <jp_lang@yahoo.fr>
2012-03-04 11:53:15 +0000

    Removed sort order on issues association.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@9075 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/project.rb
+++ app/models/project.rb
@@ -37,7 +37,7 @@ class Project < ActiveRecord::Base
 
   has_many :enabled_modules, :dependent => :delete_all
   has_and_belongs_to_many :trackers, :order => "#{Tracker.table_name}.position"
-  has_many :issues, :dependent => :destroy, :order => "#{Issue.table_name}.created_on DESC", :include => [:status, :tracker]
+  has_many :issues, :dependent => :destroy, :include => [:status, :tracker]
   has_many :issue_changes, :through => :issues, :source => :journals
   has_many :versions, :dependent => :destroy, :order => "#{Version.table_name}.effective_date DESC, #{Version.table_name}.name DESC"
   has_many :time_entries, :dependent => :delete_all
