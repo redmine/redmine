db110ba35e73d64e1df6cb5d25feecd1be9ff17a
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2012-04-01 12:41:25 +0000

    remove trailing white-spaces from extra/sample_plugin/app/models/meeting.rb
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@9294 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- extra/sample_plugin/app/models/meeting.rb
+++ extra/sample_plugin/app/models/meeting.rb
@@ -4,8 +4,8 @@ class Meeting < ActiveRecord::Base
   acts_as_event :title => Proc.new {|o| "#{o.scheduled_on} Meeting"},
                 :datetime => :scheduled_on,
                 :author => nil,
-                :url => Proc.new {|o| {:controller => 'meetings', :action => 'show', :id => o.id}}                
-  
+                :url => Proc.new {|o| {:controller => 'meetings', :action => 'show', :id => o.id}}
+
   acts_as_activity_provider :timestamp => 'scheduled_on',
                             :find_options => { :include => :project }
 end
