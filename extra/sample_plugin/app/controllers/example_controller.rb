a81927361e0b84a27fbfdfd43d9b7519bdeb5e9c
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2012-04-01 12:41:10 +0000

    remove trailing white-spaces from extra/sample_plugin/app/controllers/example_controller.rb
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@9293 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- extra/sample_plugin/app/controllers/example_controller.rb
+++ extra/sample_plugin/app/controllers/example_controller.rb
@@ -1,20 +1,20 @@
 # Sample plugin controller
 class ExampleController < ApplicationController
   unloadable
-  
-  layout 'base'  
+
+  layout 'base'
   before_filter :find_project, :authorize
   menu_item :sample_plugin
-    
+
   def say_hello
     @value = Setting.plugin_sample_plugin['sample_setting']
   end
 
   def say_goodbye
   end
-  
+
 private
-  def find_project   
+  def find_project
     @project=Project.find(params[:id])
   end
 end
