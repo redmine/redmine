f9d123c08fd1afa8a2a381d3c70692990b55f459
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2012-04-02 13:49:28 +0000

    remove trailing white-spaces from vendor/plugins/acts_as_attachable/lib/acts_as_attachable.rb
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@9302 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- vendor/plugins/acts_as_attachable/lib/acts_as_attachable.rb
+++ vendor/plugins/acts_as_attachable/lib/acts_as_attachable.rb
@@ -1,16 +1,16 @@
 # Redmine - project management software
-# Copyright (C) 2006-2011  Jean-Philippe Lang
+# Copyright (C) 2006-2012  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -28,7 +28,7 @@ module Redmine
           self.attachable_options = {}
           attachable_options[:view_permission] = options.delete(:view_permission) || "view_#{self.name.pluralize.underscore}".to_sym
           attachable_options[:delete_permission] = options.delete(:delete_permission) || "edit_#{self.name.pluralize.underscore}".to_sym
-          
+
           has_many :attachments, options.merge(:as => :container,
                                                :order => "#{Attachment.table_name}.created_on",
                                                :dependent => :destroy)
@@ -41,12 +41,12 @@ module Redmine
         def self.included(base)
           base.extend ClassMethods
         end
-        
+
         def attachments_visible?(user=User.current)
           (respond_to?(:visible?) ? visible?(user) : true) &&
             user.allowed_to?(self.class.attachable_options[:view_permission], self.project)
         end
-        
+
         def attachments_deletable?(user=User.current)
           (respond_to?(:visible?) ? visible?(user) : true) &&
             user.allowed_to?(self.class.attachable_options[:delete_permission], self.project)
