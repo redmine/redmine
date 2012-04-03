f0f7158d87c9ea7cc5046133091f52fe77cbb532
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2012-04-03 14:04:38 +0000

    fix typo "RedMine" at app/models/wiki_content.rb
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@9316 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/wiki_content.rb
+++ app/models/wiki_content.rb
@@ -1,5 +1,5 @@
-# RedMine - project management software
-# Copyright (C) 2006-2011  Jean-Philippe Lang
+# Redmine - project management software
+# Copyright (C) 2006-2012  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
