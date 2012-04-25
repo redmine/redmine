d15ab86459a70d5aa8e19f3f8922f70ea6b8b44e
Jean-Philippe Lang <jp_lang@yahoo.fr>
2012-04-25 18:17:00 +0000

    Manually generates the javascript that prototype_helper fails to do with ruby1.9.2.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@9529 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/watchers_controller.rb
+++ app/controllers/watchers_controller.rb
@@ -70,9 +70,7 @@ class WatchersController < ApplicationController
         format.js do
           render :update do |page|
             users.each do |user|
-              page.select("#issue_watcher_user_ids_#{user.id}").each do |item|
-                page.remove item
-              end
+              page << %|$$("#issue_watcher_user_ids_#{user.id}").each(function(el){el.remove();});|
             end
             page.insert_html :bottom, 'watchers_inputs', :text => watchers_checkboxes(nil, users, true)
           end
@@ -122,9 +120,7 @@ private
       format.js do
         render(:update) do |page|
           c = watcher_css(@watched)
-          page.select(".#{c}").each do |item|
-            page.replace_html item, watcher_link(@watched, user)
-          end
+          page << %|$$(".#{c}").each(function(el){el.innerHTML="#{escape_javascript watcher_link(@watched, user)}"});|
         end
       end
     end
