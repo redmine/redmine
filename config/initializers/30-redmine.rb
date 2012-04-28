bceaf8be94091ca331b820fdc68e4a3b10a831bd
Jean-Philippe Lang <jp_lang@yahoo.fr>
2012-04-28 12:00:45 +0000

    Let the secret token be set in configuration.yml.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@9567 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- config/initializers/30-redmine.rb
+++ config/initializers/30-redmine.rb
@@ -4,6 +4,12 @@ I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
 
 require 'redmine'
 
+# Load the secret token from the Redmine configuration file
+secret = Redmine::Configuration['secret_token']
+if secret.present?
+  RedmineApp::Application.config.secret_token = secret
+end
+
 Redmine::Plugin.load
 unless Redmine::Configuration['mirror_plugins_assets_on_startup'] == false
   Redmine::Plugin.mirror_assets
