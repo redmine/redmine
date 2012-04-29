253b02bd224e9c1fed6538272697f3ce393c7e56
Jean-Philippe Lang <jp_lang@yahoo.fr>
2012-04-29 08:25:50 +0000

    Adds an id to the flash message divs (#9034).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@9578 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -220,7 +220,7 @@ module ApplicationHelper
   def render_flash_messages
     s = ''
     flash.each do |k,v|
-      s << (content_tag('div', v.html_safe, :class => "flash #{k}"))
+      s << content_tag('div', v.html_safe, :class => "flash #{k}", :id => "flash_#{k}")
     end
     s.html_safe
   end
