d8c70d7a11904fe8f9067d9743848d55bd2f6af2
Jean-Philippe Lang <jp_lang@yahoo.fr>
2012-04-29 08:48:50 +0000

    Fixed openid redirect.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@9579 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -153,7 +153,7 @@ class AccountController < ApplicationController
   end
 
   def open_id_authenticate(openid_url)
-    authenticate_with_open_id(openid_url, :required => [:nickname, :fullname, :email], :return_to => signin_url) do |result, identity_url, registration|
+    authenticate_with_open_id(openid_url, :required => [:nickname, :fullname, :email], :return_to => signin_url, :method => :post) do |result, identity_url, registration|
       if result.successful?
         user = User.find_or_initialize_by_identity_url(identity_url)
         if user.new_record?
