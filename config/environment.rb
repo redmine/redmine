6fca0289055fae8f066eeb493a590b35e6f26cc0
Jean-Philippe Lang <jp_lang@yahoo.fr>
2012-05-01 10:42:25 +0000

    Make sure we don't boot with plugins in vendor/plugins.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@9600 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,14 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Make sure there's no plugin in vendor/plugin before starting
+vendor_plugins_dir = File.join(Rails.root, "vendor", "plugins")
+if Dir.glob(File.join(vendor_plugins_dir, "*")).any?
+  $stderr.puts "Plugins in vendor/plugins (#{vendor_plugins_dir}) are no longer allowed. " +
+    "Please, put your Redmine plugins in the `plugins` directory at the root of your " +
+    "Redmine directory (#{File.join(Rails.root, "plugins")})"
+  exit 1
+end
+
 # Initialize the rails application
 RedmineApp::Application.initialize!
