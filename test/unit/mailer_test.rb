017d974dd8d5c092b5bd5d496096bb7126a185f1
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2012-05-01 12:45:39 +0000

    remove trailing white-spaces from test/unit/mailer_test.rb
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@9602 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/mailer_test.rb
+++ test/unit/mailer_test.rb
@@ -1,5 +1,5 @@
 # Redmine - project management software
-# Copyright (C) 2006-2011  Jean-Philippe Lang
+# Copyright (C) 2006-2012  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -19,7 +19,7 @@ require File.expand_path('../../test_helper', __FILE__)
 
 class MailerTest < ActiveSupport::TestCase
   include Redmine::I18n
-  include ActionDispatch::Assertions::SelectorAssertions 
+  include ActionDispatch::Assertions::SelectorAssertions
   fixtures :projects, :enabled_modules, :issues, :users, :members,
            :member_roles, :roles, :documents, :attachments, :news,
            :tokens, :journals, :journal_details, :changesets, :trackers,
