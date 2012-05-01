5b680625c198d8c29031423ab2ac18370b27de07
Jean-Philippe Lang <jp_lang@yahoo.fr>
2012-05-01 11:41:15 +0000

    Adds a simple script/about.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@9601 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/info.rb
+++ lib/redmine/info.rb
@@ -5,6 +5,25 @@ module Redmine
       def url; 'http://www.redmine.org/' end
       def help_url; 'http://www.redmine.org/guide' end
       def versioned_name; "#{app_name} #{Redmine::VERSION}" end
+
+      def environment
+        s = "Environment:\n"
+        s << [
+          ["Redmine version", Redmine::VERSION],
+          ["Ruby version", "#{RUBY_VERSION} (#{RUBY_PLATFORM})"],
+          ["Rails version", Rails::VERSION::STRING],
+          ["Environment", Rails.env],
+          ["Database adapter", ActiveRecord::Base.connection.adapter_name]
+        ].map {|info| "  %-40s %s" % info}.join("\n")
+        s << "\nRedmine plugins:\n"
+
+        plugins = Redmine::Plugin.all
+        if plugins.any?
+          s << plugins.map {|plugin| "  %-40s %s" % [plugin.id.to_s, plugin.version.to_s]}.join("\n")
+        else
+          s << "  no plugin installed"
+        end
+      end
     end
   end
 end
