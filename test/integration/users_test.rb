17d19febc64733962562a8cfc61317f4c8c2a688
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2012-05-05 05:29:28 +0000

    fix typo of integration users test
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@9626 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/integration/users_test.rb
+++ test/integration/users_test.rb
@@ -17,7 +17,7 @@
 
 require File.expand_path('../../test_helper', __FILE__)
 
-class USersTest < ActionController::IntegrationTest
+class UsersTest < ActionController::IntegrationTest
   fixtures :users
 
   def test_destroy_should_not_accept_get_requests
