678469cf092b2ffbf001605ac851dc8c9fce7d95
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2012-05-09 13:58:59 +0000

    additional comment at config/environments/test.rb
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@9660 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -14,9 +14,14 @@ RedmineApp::Application.configure do
   config.action_controller.perform_caching = false
 
   config.action_mailer.perform_deliveries = true
+
+  # Tell Action Mailer not to deliver emails to the real world.
+  # The :test delivery method accumulates sent emails in the
+  # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
-  # Skip protect_from_forgery in requests http://m.onkey.org/2007/9/28/csrf-protection-for-your-existing-rails-application
+  # Skip protect_from_forgery in requests
+  # http://m.onkey.org/2007/9/28/csrf-protection-for-your-existing-rails-application
   config.action_controller.allow_forgery_protection = false
 
   config.active_support.deprecation = :log
