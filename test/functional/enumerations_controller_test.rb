69e55fb1ef26c0744993e3f83e80ea987d9db5c9
Jean-Philippe Lang <jp_lang@yahoo.fr>
2012-05-13 08:45:15 +0000

    Fixed that enumerations option tags are escaped.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@9681 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/enumerations_controller_test.rb
+++ test/functional/enumerations_controller_test.rb
@@ -105,6 +105,9 @@ class EnumerationsControllerTest < ActionController::TestCase
     assert_response :success
     assert_template 'destroy'
     assert_not_nil Enumeration.find_by_id(4)
+    assert_select 'select[name=reassign_to_id]' do
+      assert_select 'option[value=6]', :text => 'High'
+    end
   end
 
   def test_destroy_enumeration_in_use_with_reassignment
