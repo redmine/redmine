74645eb017f2f8a0f9e7ce669c50c3827488b801
Jean-Philippe Lang <jp_lang@yahoo.fr>
2012-06-10 13:16:56 +0000

    Configurable session lifetime and timeout (#6597).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@9797 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,7 +35,7 @@ class ApplicationController < ActionController::Base
     cookies.delete(:autologin)
   end
 
-  before_filter :user_setup, :check_if_login_required, :set_localization
+  before_filter :session_expiration, :user_setup, :check_if_login_required, :set_localization
 
   rescue_from ActionController::InvalidAuthenticityToken, :with => :invalid_authenticity_token
   rescue_from ::Unauthorized, :with => :deny_access
@@ -43,6 +43,38 @@ class ApplicationController < ActionController::Base
   include Redmine::Search::Controller
   include Redmine::MenuManager::MenuController
   helper Redmine::MenuManager::MenuHelper
+
+  def session_expiration
+    if session[:user_id]
+      if session_expired? && !try_to_autologin
+        reset_session
+        flash[:error] = l(:error_session_expired)
+        redirect_to signin_url
+      else
+        session[:atime] = Time.now.utc.to_i
+      end
+    end
+  end
+
+  def session_expired?
+    if Setting.session_lifetime?
+      unless session[:ctime] && (Time.now.utc.to_i - session[:ctime].to_i <= Setting.session_lifetime.to_i * 60)
+        return true
+      end
+    end
+    if Setting.session_timeout?
+      unless session[:atime] && (Time.now.utc.to_i - session[:atime].to_i <= Setting.session_timeout.to_i * 60)
+        return true
+      end
+    end
+    false
+  end
+
+  def start_user_session(user)
+    session[:user_id] = user.id
+    session[:ctime] = Time.now.utc.to_i
+    session[:atime] = Time.now.utc.to_i
+  end
 
   def user_setup
     # Check the settings cache for each request
@@ -57,10 +89,7 @@ class ApplicationController < ActionController::Base
     if session[:user_id]
       # existing session
       (User.active.find(session[:user_id]) rescue nil)
-    elsif cookies[:autologin] && Setting.autologin?
-      # auto-login feature starts a new session
-      user = User.try_to_autologin(cookies[:autologin])
-      session[:user_id] = user.id if user
+    elsif user = try_to_autologin
       user
     elsif params[:format] == 'atom' && params[:key] && request.get? && accept_rss_auth?
       # RSS key authentication does not start a session
@@ -78,12 +107,24 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def try_to_autologin
+    if cookies[:autologin] && Setting.autologin?
+      # auto-login feature starts a new session
+      user = User.try_to_autologin(cookies[:autologin])
+      if user
+        reset_session
+        start_user_session(user)
+      end
+      user
+    end
+  end
+
   # Sets the logged in user
   def logged_user=(user)
     reset_session
     if user && user.is_a?(User)
       User.current = user
-      session[:user_id] = user.id
+      start_user_session(user)
     else
       User.current = User.anonymous
     end
---
+++ test/functional/sessions_test.rb
@@ -0,0 +1,113 @@
+# Redmine - project management software
+# Copyright (C) 2006-2012  Jean-Philippe Lang
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+require File.expand_path('../../test_helper', __FILE__)
+
+class SessionStartTest < ActionController::TestCase
+  tests AccountController
+
+  def test_login_should_set_session_timestamps
+    post :login, :username => 'jsmith', :password => 'jsmith'
+    assert_response 302
+    assert_equal 2, session[:user_id]
+    assert_not_nil session[:ctime]
+    assert_not_nil session[:atime]
+  end
+end
+
+class SessionsTest < ActionController::TestCase
+  tests WelcomeController
+
+  def test_atime_from_user_session_should_be_updated
+    created = 2.hours.ago.utc.to_i
+    get :index, {}, {:user_id => 2, :ctime => created, :atime => created}
+    assert_response :success
+    assert_equal created, session[:ctime]
+    assert_not_equal created, session[:atime]
+    assert session[:atime] > created
+  end
+
+  def test_user_session_should_not_be_reset_if_lifetime_and_timeout_disabled
+    with_settings :session_lifetime => '0', :session_timeout => '0' do
+      get :index, {}, {:user_id => 2}
+      assert_response :success
+    end
+  end
+
+  def test_user_session_without_ctime_should_be_reset_if_lifetime_enabled
+    with_settings :session_lifetime => '720' do
+      get :index, {}, {:user_id => 2}
+      assert_redirected_to '/login'
+    end
+  end
+
+  def test_user_session_with_expired_ctime_should_be_reset_if_lifetime_enabled
+    with_settings :session_timeout => '720' do
+      get :index, {}, {:user_id => 2, :atime => 2.days.ago.utc.to_i}
+      assert_redirected_to '/login'
+    end
+  end
+
+  def test_user_session_with_valid_ctime_should_not_be_reset_if_lifetime_enabled
+    with_settings :session_timeout => '720' do
+      get :index, {}, {:user_id => 2, :atime => 3.hours.ago.utc.to_i}
+      assert_response :success
+    end
+  end
+
+  def test_user_session_without_atime_should_be_reset_if_timeout_enabled
+    with_settings :session_timeout => '60' do
+      get :index, {}, {:user_id => 2}
+      assert_redirected_to '/login'
+    end
+  end
+
+  def test_user_session_with_expired_atime_should_be_reset_if_timeout_enabled
+    with_settings :session_timeout => '60' do
+      get :index, {}, {:user_id => 2, :atime => 4.hours.ago.utc.to_i}
+      assert_redirected_to '/login'
+    end
+  end
+
+  def test_user_session_with_valid_atime_should_not_be_reset_if_timeout_enabled
+    with_settings :session_timeout => '60' do
+      get :index, {}, {:user_id => 2, :atime => 10.minutes.ago.utc.to_i}
+      assert_response :success
+    end
+  end
+
+  def test_expired_user_session_should_be_restarted_if_autologin
+    with_settings :session_lifetime => '720', :session_timeout => '60', :autologin => 7 do
+      token = Token.create!(:user_id => 2, :action => 'autologin', :created_on => 1.day.ago)
+      @request.cookies['autologin'] = token.value
+      created = 2.hours.ago.utc.to_i
+
+      get :index, {}, {:user_id => 2, :ctime => created, :atime => created}
+      assert_equal 2, session[:user_id]
+      assert_response :success
+      assert_not_equal created, session[:ctime]
+      assert session[:ctime] >= created
+    end
+  end
+
+  def test_anonymous_session_should_not_be_reset
+    with_settings :session_lifetime => '720', :session_timeout => '60' do
+      get :index
+      assert_response :success
+    end
+  end
+end
