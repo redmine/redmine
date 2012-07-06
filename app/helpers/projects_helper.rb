f944a7dd3c6f911dd5bae20b9b13fc55694f522c
Padraig Gleeson <p.gleeson@gmail.com>
2012-07-06 11:43:39 +0100

    Manually adding changes from master... Updated base page with logo Added images for about page, incl flags Adding theme, as this may be modified Adding about view/controller Updated project view to only show those with category=Project Changing favicon.ico to OSB

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -60,24 +60,38 @@ module ProjectsHelper
       ancestors = []
       original_project = @project
       projects.each do |project|
-        # set the project environment to please macros.
-        @project = project
-        if (ancestors.empty? || project.is_descendant_of?(ancestors.last))
-          s << "<ul class='projects #{ ancestors.empty? ? 'root' : nil}'>\n"
-        else
-          ancestors.pop
-          s << "</li>"
-          while (ancestors.any? && !project.is_descendant_of?(ancestors.last))
-            ancestors.pop
-            s << "</ul></li>\n"
-          end
+       
+        show_this = 0
+        
+        project.visible_custom_field_values.each do |custom_value|
+	        if (custom_value.custom_field.name == 'Category')
+	            if (custom_value.value == 'Project')
+	                show_this = 1
+	            end
+	         end
+	    end
+        
+        if (show_this == 1)
+            # set the project environment to please macros.
+            @project = project
+            if (ancestors.empty? || project.is_descendant_of?(ancestors.last))
+              s << "<ul class='projects #{ ancestors.empty? ? 'root' : nil}'>\n"
+            else
+              ancestors.pop
+              s << "</li>"
+              while (ancestors.any? && !project.is_descendant_of?(ancestors.last)) 
+                ancestors.pop
+                s << "</ul></li>\n"
+              end
+            end
+            classes = (ancestors.empty? ? 'root' : 'child')
+            s << "<li class='#{classes}'><div class='#{classes}'>" +
+                   link_to_project(project, {}, :class => "project #{User.current.member_of?(project) ? 'my-project' : nil}")
+            s << "<div class='wiki description'>#{textilizable(project.short_description, :project => project)}</div>" unless project.description.blank?
+            s << "</div>\n"
+            ancestors << project
         end
-        classes = (ancestors.empty? ? 'root' : 'child')
-        s << "<li class='#{classes}'><div class='#{classes}'>" +
-               link_to_project(project, {}, :class => "project #{User.current.member_of?(project) ? 'my-project' : nil}")
-        s << "<div class='wiki description'>#{textilizable(project.short_description, :project => project)}</div>" unless project.description.blank?
-        s << "</div>\n"
-        ancestors << project
+
       end
       s << ("</li></ul>\n" * ancestors.size)
       @project = original_project
