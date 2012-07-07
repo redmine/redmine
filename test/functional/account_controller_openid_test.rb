553066e804dd5796ac4f8d1d775cfeb5670d239f
Jean-Philippe Lang <jp_lang@yahoo.fr>
2012-07-07 09:26:26 +0000

    Fixed test case name.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@9930 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/account_controller_openid_test.rb
+++ test/functional/account_controller_openid_test.rb
@@ -17,7 +17,7 @@
 
 require File.expand_path('../../test_helper', __FILE__)
 
-class AccountControllerTest < ActionController::TestCase
+class AccountControllerOpenidTest < ActionController::TestCase
   tests AccountController
   fixtures :users, :roles
 
