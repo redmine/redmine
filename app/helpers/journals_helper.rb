d4f7b4af6df8a925df2f09a71ce8a5d4c3758253
Jean-Philippe Lang <jp_lang@yahoo.fr>
2012-07-18 19:09:52 +0000

    Removes calls to link_to_remote.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@10038 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/journals_helper.rb
+++ app/helpers/journals_helper.rb
@@ -23,9 +23,11 @@ module JournalsHelper
     editable = User.current.logged? && (User.current.allowed_to?(:edit_issue_notes, issue.project) || (journal.user == User.current && User.current.allowed_to?(:edit_own_issue_notes, issue.project)))
     links = []
     if !journal.notes.blank?
-      links << link_to_remote(image_tag('comment.png'),
-                              { :url => {:controller => 'journals', :action => 'new', :id => issue, :journal_id => journal} },
-                              :title => l(:button_quote)) if options[:reply_links]
+      links << link_to(image_tag('comment.png'),
+                       {:controller => 'journals', :action => 'new', :id => issue, :journal_id => journal},
+                       :remote => true,
+                       :method => 'post',
+                       :title => l(:button_quote)) if options[:reply_links]
       links << link_to_in_place_notes_editor(image_tag('edit.png'), "journal-#{journal.id}-notes",
                                              { :controller => 'journals', :action => 'edit', :id => journal },
                                                 :title => l(:button_edit)) if editable
