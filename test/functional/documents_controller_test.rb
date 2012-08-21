4fcec69a40bf55e0c754df2dda34870864e6d4c9
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2012-08-21 04:53:10 +0000

    Merged r10216 from trunk
    add missing fixtures to test/functional/documents_controller_test.rb
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/branches/1.4-stable@10218 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/documents_controller_test.rb
+++ test/functional/documents_controller_test.rb
@@ -18,7 +18,9 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class DocumentsControllerTest < ActionController::TestCase
-  fixtures :projects, :users, :roles, :members, :member_roles, :enabled_modules, :documents, :enumerations
+  fixtures :projects, :users, :roles, :members, :member_roles,
+           :enabled_modules, :documents, :enumerations,
+           :groups_users, :attachments
 
   def setup
     User.current = nil
