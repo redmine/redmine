a1d8cab6c6528b2714a7db5b180ada62541ab497
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2012-08-22 16:31:26 +0000

    remove empty setup method from unit enumeration test
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@10232 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/enumeration_test.rb
+++ test/unit/enumeration_test.rb
@@ -1,5 +1,5 @@
 # Redmine - project management software
-# Copyright (C) 2006-2008  Jean-Philippe Lang
+# Copyright (C) 2006-2012  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -19,9 +19,6 @@ require File.expand_path('../../test_helper', __FILE__)
 
 class EnumerationTest < ActiveSupport::TestCase
   fixtures :enumerations, :issues, :custom_fields, :custom_values
-
-  def setup
-  end
 
   def test_objects_count
     # low priority
