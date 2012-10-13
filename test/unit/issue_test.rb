756adebb960283b0cf8207472d3114fd52a505f8
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2012-10-13 10:15:34 +0000

    code layout clean up context "#assignable_users" of unit issue test
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@10635 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/issue_test.rb
+++ test/unit/issue_test.rb
@@ -1286,7 +1286,8 @@ class IssueTest < ActiveSupport::TestCase
       assert_equal 2, assignable_user_ids.length
 
       assignable_user_ids.each do |user_id|
-        assert_equal 1, assignable_user_ids.select {|i| i == user_id}.length, "User #{user_id} appears more or less than once"
+        assert_equal 1, assignable_user_ids.select {|i| i == user_id}.length,
+                     "User #{user_id} appears more or less than once"
       end
     end
 
