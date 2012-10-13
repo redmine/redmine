31ee1d22eb89c6b4debac8307e84022a7dd23bc5
Jean-Philippe Lang <jp_lang@yahoo.fr>
2012-10-13 08:03:19 +0000

    Test moved to tests with transactional fixtures disabled.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@10626 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/issues_controller_transaction_test.rb
+++ test/functional/issues_controller_transaction_test.rb
@@ -235,6 +235,24 @@ class IssuesControllerTransactionTest < ActionController::TestCase
     assert_nil issue.fixed_version_id
   end
 
+  def test_put_update_with_spent_time_and_failure_should_not_add_spent_time
+    @request.session[:user_id] = 2
+
+    assert_no_difference('TimeEntry.count') do
+      put :update,
+           :id => 1,
+           :issue => { :subject => '' },
+           :time_entry => { :hours => '2.5', :comments => 'should not be added', :activity_id => TimeEntryActivity.first.id }
+      assert_response :success
+    end
+
+    assert_select 'input[name=?][value=?]', 'time_entry[hours]', '2.5'
+    assert_select 'input[name=?][value=?]', 'time_entry[comments]', 'should not be added'
+    assert_select 'select[name=?]', 'time_entry[activity_id]' do
+      assert_select 'option[value=?][selected=selected]', TimeEntryActivity.first.id
+    end
+  end
+
   def test_index_should_rescue_invalid_sql_query
     Query.any_instance.stubs(:statement).returns("INVALID STATEMENT")
 
