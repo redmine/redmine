02c770968f57699f109e90f3f5e2ac7f08d4aed8
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2012-10-13 11:20:06 +0000

    code layout clean up test_should_not_be_able_to_assign_a_new_issue_to_a_closed_version of unit issue test
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@10653 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/issue_test.rb
+++ test/unit/issue_test.rb
@@ -883,7 +883,9 @@ class IssueTest < ActiveSupport::TestCase
   end
 
   def test_should_not_be_able_to_assign_a_new_issue_to_a_closed_version
-    issue = Issue.new(:project_id => 1, :tracker_id => 1, :author_id => 1, :status_id => 1, :fixed_version_id => 1, :subject => 'New issue')
+    issue = Issue.new(:project_id => 1, :tracker_id => 1, :author_id => 1,
+                      :status_id => 1, :fixed_version_id => 1,
+                      :subject => 'New issue')
     assert !issue.save
     assert_not_nil issue.errors[:fixed_version_id]
   end
