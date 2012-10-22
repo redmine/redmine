1d888f25f6f09e7db8365311a52fcd7e7051e4ec
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2012-10-22 03:34:02 +0000

    Merged r10688 from trunk to 2.0-stable
    Backout r10253.
    
    Backout replacing shoulda context of test/unit/helpers/issues_helper_test.rb.
    Test methods in shoulda context do not run on Ruby 1.8.7-p370.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/branches/2.0-stable@10694 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/helpers/issues_helper_test.rb
+++ test/unit/helpers/issues_helper_test.rb
@@ -108,27 +108,29 @@ class IssuesHelperTest < ActionView::TestCase
       end
     end
 
-    def test_with_a_start_date_attribute_should_format_the_current_date
-      @detail = JournalDetail.new(
+    context "with a start_date attribute" do
+      should "format the current date" do
+        @detail = JournalDetail.new(
                    :property  => 'attr',
                    :old_value => '2010-01-01',
                    :value     => '2010-01-31',
                    :prop_key  => 'start_date'
                 )
-      with_settings :date_format => '%m/%d/%Y' do
-        assert_match "01/31/2010", show_detail(@detail, true)
+        with_settings :date_format => '%m/%d/%Y' do
+          assert_match "01/31/2010", show_detail(@detail, true)
+        end
       end
-    end
 
-    def test_with_a_start_date_attribute_should_format_the_old_date
-      @detail = JournalDetail.new(
+      should "format the old date" do
+        @detail = JournalDetail.new(
                    :property  => 'attr',
                    :old_value => '2010-01-01',
                    :value     => '2010-01-31',
                    :prop_key  => 'start_date'
                 )
-      with_settings :date_format => '%m/%d/%Y' do
-        assert_match "01/01/2010", show_detail(@detail, true)
+        with_settings :date_format => '%m/%d/%Y' do
+          assert_match "01/01/2010", show_detail(@detail, true)
+        end
       end
     end
 
