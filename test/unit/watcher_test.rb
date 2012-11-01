6e190a76fb1c9d277c87379c9e40248b961e7886
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2012-11-01 10:22:37 +0000

    Fixed test/unit/watcher_test.rb breaking when run alone (#12285)
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@10779 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/watcher_test.rb
+++ test/unit/watcher_test.rb
@@ -19,7 +19,7 @@ require File.expand_path('../../test_helper', __FILE__)
 
 class WatcherTest < ActiveSupport::TestCase
   fixtures :projects, :users, :members, :member_roles, :roles, :enabled_modules,
-           :issues,
+           :issues, :issue_statuses, :enumerations, :trackers, :projects_trackers,
            :boards, :messages,
            :wikis, :wiki_pages,
            :watchers
