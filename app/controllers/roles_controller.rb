6c2eec59d8fa2a657fcbc55f76dd65de5980c09b
Jean-Philippe Lang <jp_lang@yahoo.fr>
2012-11-30 08:58:26 +0000

    Merged r10893 into 2.1-stable (#12472).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/branches/2.1-stable@10895 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -22,6 +22,7 @@ class RolesController < ApplicationController
   before_filter :require_admin_or_api_request, :only => :index
   before_filter :find_role, :only => [:edit, :update, :destroy]
   accept_api_auth :index
+  accept_api_auth :index, :show
 
   def index
     respond_to do |format|
