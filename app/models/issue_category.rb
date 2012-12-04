ee7685670d2938d4fa2b496d550885ba5f3d571d
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2012-12-04 04:22:59 +0000

    remove trailing white-space from app/models/issue_category.rb
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@10941 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/issue_category.rb
+++ app/models/issue_category.rb
@@ -24,7 +24,7 @@ class IssueCategory < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => [:project_id]
   validates_length_of :name, :maximum => 30
-  
+
   safe_attributes 'name', 'assigned_to_id'
 
   scope :named, lambda {|arg| where("LOWER(#{table_name}.name) = LOWER(?)", arg.to_s.strip)}
