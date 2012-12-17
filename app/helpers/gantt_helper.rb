dd26ac3038a47a1fc442fba50f18e359c982d128
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2012-12-17 07:14:26 +0000

    code cleanup gantt_helper.rb
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@11021 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/gantt_helper.rb
+++ app/helpers/gantt_helper.rb
@@ -24,19 +24,19 @@ module GanttHelper
     when :in
       if gantt.zoom < 4
         link_to_content_update l(:text_zoom_in),
-          params.merge(gantt.params.merge(:zoom => (gantt.zoom+1))),
+          params.merge(gantt.params.merge(:zoom => (gantt.zoom + 1))),
           :class => 'icon icon-zoom-in'
       else
-        content_tag('span', l(:text_zoom_in), :class => 'icon icon-zoom-in').html_safe
+        content_tag(:span, l(:text_zoom_in), :class => 'icon icon-zoom-in').html_safe
       end
 
     when :out
       if gantt.zoom > 1
         link_to_content_update l(:text_zoom_out),
-          params.merge(gantt.params.merge(:zoom => (gantt.zoom-1))),
+          params.merge(gantt.params.merge(:zoom => (gantt.zoom - 1))),
           :class => 'icon icon-zoom-out'
       else
-        content_tag('span', l(:text_zoom_out), :class => 'icon icon-zoom-out').html_safe
+        content_tag(:span, l(:text_zoom_out), :class => 'icon icon-zoom-out').html_safe
       end
     end
   end
