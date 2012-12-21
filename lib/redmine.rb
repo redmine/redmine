7f74044854eef6e4cdd0ac80c708ef9b8e01b6e3
Jean-Philippe Lang <jp_lang@yahoo.fr>
2012-12-21 21:17:21 +0000

    "New issue" menu item should not link to copy form (#12652).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@11064 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine.rb
+++ lib/redmine.rb
@@ -238,7 +238,7 @@ Redmine::MenuManager.map :project_menu do |menu|
   menu.push :roadmap, { :controller => 'versions', :action => 'index' }, :param => :project_id,
               :if => Proc.new { |p| p.shared_versions.any? }
   menu.push :issues, { :controller => 'issues', :action => 'index' }, :param => :project_id, :caption => :label_issue_plural
-  menu.push :new_issue, { :controller => 'issues', :action => 'new' }, :param => :project_id, :caption => :label_issue_new,
+  menu.push :new_issue, { :controller => 'issues', :action => 'new', :copy_from => nil }, :param => :project_id, :caption => :label_issue_new,
               :html => { :accesskey => Redmine::AccessKeys.key_for(:new_issue) }
   menu.push :gantt, { :controller => 'gantts', :action => 'show' }, :param => :project_id, :caption => :label_gantt
   menu.push :calendar, { :controller => 'calendars', :action => 'show' }, :param => :project_id, :caption => :label_calendar
