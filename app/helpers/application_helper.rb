7606353ff99134c4f160ce9ed1aaefce50bc437c
Jean-Philippe Lang <jp_lang@yahoo.fr>
2012-12-26 11:47:51 +0000

    Upgraded JQuery to 1.8.3 and JQuery UI to 1.9.2.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@11081 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1183,7 +1183,7 @@ module ApplicationHelper
 
   # Returns the javascript tags that are included in the html layout head
   def javascript_heads
-    tags = javascript_include_tag('jquery-1.7.2-ui-1.8.21-ujs-2.0.3', 'application')
+    tags = javascript_include_tag('jquery-1.8.3-ui-1.9.2-ujs-2.0.3', 'application')
     unless User.current.pref.warn_on_leaving_unsaved == '0'
       tags << "\n".html_safe + javascript_tag("$(window).load(function(){ warnLeavingUnsaved('#{escape_javascript l(:text_warn_on_leaving_unsaved)}'); });")
     end
