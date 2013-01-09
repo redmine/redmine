f0aad8c2a8d78e4ad763fb751d43c814587aa3c6
Jean-Philippe Lang <jp_lang@yahoo.fr>
2013-01-09 14:01:27 +0000

    Backported r10842 from trunk.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/branches/2.1-stable@11150 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -129,15 +129,6 @@ class WikiController < ApplicationController
     # don't keep previous comment
     @content.comments = nil
 
-    if !@page.new_record? && params[:content].present? && @content.text == params[:content][:text]
-      attachments = Attachment.attach_files(@page, params[:attachments])
-      render_attachment_warning_if_needed(@page)
-      # don't save content if text wasn't changed
-      @page.save
-      redirect_to :action => 'show', :project_id => @project, :id => @page.title
-      return
-    end
-
     @content.comments = params[:content][:comments]
     @text = params[:content][:text]
     if params[:section].present? && Redmine::WikiFormatting.supports_section_edit?
@@ -149,8 +140,8 @@ class WikiController < ApplicationController
       @content.text = @text
     end
     @content.author = User.current
-    @page.content = @content
-    if @page.save
+
+    if @page.save_with_content
       attachments = Attachment.attach_files(@page, params[:attachments])
       render_attachment_warning_if_needed(@page)
       call_hook(:controller_wiki_edit_after_save, { :params => params, :page => @page})
