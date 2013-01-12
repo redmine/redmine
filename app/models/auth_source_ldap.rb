e224d50408283f18c2703afbd73446efd73bf5b2
Jean-Philippe Lang <jp_lang@yahoo.fr>
2013-01-12 20:36:22 +0000

    Removed useless require 'iconv' (#12787).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@11176 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/auth_source_ldap.rb
+++ app/models/auth_source_ldap.rb
@@ -15,7 +15,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-require 'iconv'
 require 'net/ldap'
 require 'net/ldap/dn'
 require 'timeout'
