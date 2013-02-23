aca59db4cc5a68098b2e60251d20583ac73c8164
Jean-Philippe Lang <jp_lang@yahoo.fr>
2013-02-23 11:10:10 +0000

    Rename issues CSV and PDF exports.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@11449 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -85,8 +85,8 @@ class IssuesController < ApplicationController
           Issue.load_visible_relations(@issues) if include_in_api_response?('relations')
         }
         format.atom { render_feed(@issues, :title => "#{@project || Setting.app_title}: #{l(:label_issue_plural)}") }
-        format.csv  { send_data(query_to_csv(@issues, @query, params), :type => 'text/csv; header=present', :filename => 'export.csv') }
-        format.pdf  { send_data(issues_to_pdf(@issues, @project, @query), :type => 'application/pdf', :filename => 'export.pdf') }
+        format.csv  { send_data(query_to_csv(@issues, @query, params), :type => 'text/csv; header=present', :filename => 'issues.csv') }
+        format.pdf  { send_data(issues_to_pdf(@issues, @project, @query), :type => 'application/pdf', :filename => 'issues.pdf') }
       end
     else
       respond_to do |format|
