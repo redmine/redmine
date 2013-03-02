d5ca5146888f01d710091dc5ddb160cf653ffcbd
Jean-Philippe Lang <jp_lang@yahoo.fr>
2013-03-02 20:38:25 +0000

    Adds autologin param in openid return_to url (#3371).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@11521 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -176,7 +176,9 @@ class AccountController < ApplicationController
   end
 
   def open_id_authenticate(openid_url)
-    authenticate_with_open_id(openid_url, :required => [:nickname, :fullname, :email], :return_to => signin_url, :method => :post) do |result, identity_url, registration|
+    back_url = signin_url(:autologin => params[:autologin])
+
+    authenticate_with_open_id(openid_url, :required => [:nickname, :fullname, :email], :return_to => back_url, :method => :post) do |result, identity_url, registration|
       if result.successful?
         user = User.find_or_initialize_by_identity_url(identity_url)
         if user.new_record?
