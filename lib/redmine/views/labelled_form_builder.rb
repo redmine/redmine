944a6cea9999bab54408e16b0a489c0303f8a072
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2013-03-21 15:12:37 +0000

    Fix #12

--- lib/redmine/views/labelled_form_builder.rb
+++ lib/redmine/views/labelled_form_builder.rb
@@ -35,7 +35,7 @@ class Redmine::Views::LabelledFormBuilder < ActionView::Helpers::FormBuilder
   end
 
   def time_zone_select(field, priority_zones = nil, options = {}, html_options = {})
-        label_for_field(field, options) + super(field, priority_zones, options, html_options.except(:label)).html_safe
+        label_for_field(field, options) + ("<div class='controls'>" + super(field, priority_zones, options, html_options.except(:label))+ "</div>").html_safe
   end
 
   # Returns a label tag for the given field
