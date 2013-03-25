213e8faceeb63e19a7c7dd1bf01fa6092b7cdb96
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2013-03-25 11:46:41 +0000

    add test of datepicker in Simplified Chinese and Traditional Chinese (#13579)
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@11685 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/integration/layout_test.rb
+++ test/integration/layout_test.rb
@@ -85,6 +85,16 @@ class LayoutTest < ActionController::IntegrationTest
       get '/issues'
       assert_not_include "/javascripts/i18n/jquery.ui.datepicker", response.body
     end
+
+    with_settings :default_language => 'zh' do
+      get '/issues'
+      assert_include "/javascripts/i18n/jquery.ui.datepicker-zh-CN.js", response.body
+    end
+
+    with_settings :default_language => 'zh-TW' do
+      get '/issues'
+      assert_include "/javascripts/i18n/jquery.ui.datepicker-zh-TW.js", response.body
+    end
   end
 
   def test_search_field_outside_project_should_link_to_global_search
