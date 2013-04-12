691add1affbf8481cbe9d791ba69cda192cbfe4e
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2013-04-12 01:18:52 +0000

    remove redundant 'IssuesHelper#' from issues_helper_test context
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@11714 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/helpers/issues_helper_test.rb
+++ test/unit/helpers/issues_helper_test.rb
@@ -67,25 +67,25 @@ class IssuesHelperTest < ActionView::TestCase
                  issues_destroy_confirmation_message(Issue.find([1, 2]))
   end
 
-  test 'IssuesHelper#show_detail with no_html should show a changing attribute' do
+  test 'show_detail with no_html should show a changing attribute' do
     detail = JournalDetail.new(:property => 'attr', :old_value => '40',
                                :value => '100', :prop_key => 'done_ratio')
     assert_equal "% Done changed from 40 to 100", show_detail(detail, true)
   end
 
-  test 'IssuesHelper#show_detail with no_html should show a new attribute' do
+  test 'show_detail with no_html should show a new attribute' do
     detail = JournalDetail.new(:property => 'attr', :old_value => nil,
                                :value => '100', :prop_key => 'done_ratio')
     assert_equal "% Done set to 100", show_detail(detail, true)
   end
 
-  test 'IssuesHelper#show_detail with no_html should show a deleted attribute' do
+  test 'show_detail with no_html should show a deleted attribute' do
     detail = JournalDetail.new(:property => 'attr', :old_value => '50',
                                :value => nil, :prop_key => 'done_ratio')
     assert_equal "% Done deleted (50)", show_detail(detail, true)
   end
 
-  test 'IssuesHelper#show_detail with html should show a changing attribute with HTML highlights' do
+  test 'show_detail with html should show a changing attribute with HTML highlights' do
     detail = JournalDetail.new(:property => 'attr', :old_value => '40',
                                :value => '100', :prop_key => 'done_ratio')
     html = show_detail(detail, false)
@@ -94,7 +94,7 @@ class IssuesHelperTest < ActionView::TestCase
     assert_include '<i>100</i>', html
   end
 
-  test 'IssuesHelper#show_detail with html should show a new attribute with HTML highlights' do
+  test 'show_detail with html should show a new attribute with HTML highlights' do
     detail = JournalDetail.new(:property => 'attr', :old_value => nil,
                                :value => '100', :prop_key => 'done_ratio')
     html = show_detail(detail, false)
@@ -102,7 +102,7 @@ class IssuesHelperTest < ActionView::TestCase
     assert_include '<i>100</i>', html
   end
 
-  test 'IssuesHelper#show_detail with html should show a deleted attribute with HTML highlights' do
+  test 'show_detail with html should show a deleted attribute with HTML highlights' do
     detail = JournalDetail.new(:property => 'attr', :old_value => '50',
                                :value => nil, :prop_key => 'done_ratio')
     html = show_detail(detail, false)
@@ -110,7 +110,7 @@ class IssuesHelperTest < ActionView::TestCase
     assert_include '<del><i>50</i></del>', html
   end
 
-  test 'IssuesHelper#show_detail with a start_date attribute should format the dates' do
+  test 'show_detail with a start_date attribute should format the dates' do
     detail = JournalDetail.new(
                :property  => 'attr',
                :old_value => '2010-01-01',
@@ -123,7 +123,7 @@ class IssuesHelperTest < ActionView::TestCase
     end
   end
 
-  test 'IssuesHelper#show_detail with a due_date attribute should format the dates' do
+  test 'show_detail with a due_date attribute should format the dates' do
     detail = JournalDetail.new(
               :property  => 'attr',
               :old_value => '2010-01-01',
@@ -136,75 +136,75 @@ class IssuesHelperTest < ActionView::TestCase
     end
   end
 
-  test 'IssuesHelper#show_detail should show old and new values with a project attribute' do
+  test 'show_detail should show old and new values with a project attribute' do
     detail = JournalDetail.new(:property => 'attr', :prop_key => 'project_id',
                                :old_value => 1, :value => 2)
     assert_match 'eCookbook', show_detail(detail, true)
     assert_match 'OnlineStore', show_detail(detail, true)
   end
 
-  test 'IssuesHelper#show_detail should show old and new values with a issue status attribute' do
+  test 'show_detail should show old and new values with a issue status attribute' do
     detail = JournalDetail.new(:property => 'attr', :prop_key => 'status_id',
                                :old_value => 1, :value => 2)
     assert_match 'New', show_detail(detail, true)
     assert_match 'Assigned', show_detail(detail, true)
   end
 
-  test 'IssuesHelper#show_detail should show old and new values with a tracker attribute' do
+  test 'show_detail should show old and new values with a tracker attribute' do
     detail = JournalDetail.new(:property => 'attr', :prop_key => 'tracker_id',
                                :old_value => 1, :value => 2)
     assert_match 'Bug', show_detail(detail, true)
     assert_match 'Feature request', show_detail(detail, true)
   end
 
-  test 'IssuesHelper#show_detail should show old and new values with a assigned to attribute' do
+  test 'show_detail should show old and new values with a assigned to attribute' do
     detail = JournalDetail.new(:property => 'attr', :prop_key => 'assigned_to_id',
                                :old_value => 1, :value => 2)
     assert_match 'Redmine Admin', show_detail(detail, true)
     assert_match 'John Smith', show_detail(detail, true)
   end
 
-  test 'IssuesHelper#show_detail should show old and new values with a priority attribute' do
+  test 'show_detail should show old and new values with a priority attribute' do
     detail = JournalDetail.new(:property => 'attr', :prop_key => 'priority_id',
                                :old_value => 4, :value => 5)
     assert_match 'Low', show_detail(detail, true)
     assert_match 'Normal', show_detail(detail, true)
   end
 
-  test 'IssuesHelper#show_detail should show old and new values with a category attribute' do
+  test 'show_detail should show old and new values with a category attribute' do
     detail = JournalDetail.new(:property => 'attr', :prop_key => 'category_id',
                                :old_value => 1, :value => 2)
     assert_match 'Printing', show_detail(detail, true)
     assert_match 'Recipes', show_detail(detail, true)
   end
 
-  test 'IssuesHelper#show_detail should show old and new values with a fixed version attribute' do
+  test 'show_detail should show old and new values with a fixed version attribute' do
     detail = JournalDetail.new(:property => 'attr', :prop_key => 'fixed_version_id',
                                :old_value => 1, :value => 2)
     assert_match '0.1', show_detail(detail, true)
     assert_match '1.0', show_detail(detail, true)
   end
 
-  test 'IssuesHelper#show_detail should show old and new values with a estimated hours attribute' do
+  test 'show_detail should show old and new values with a estimated hours attribute' do
     detail = JournalDetail.new(:property => 'attr', :prop_key => 'estimated_hours',
                                :old_value => '5', :value => '6.3')
     assert_match '5.00', show_detail(detail, true)
     assert_match '6.30', show_detail(detail, true)
   end
 
-  test 'IssuesHelper#show_detail should show old and new values with a custom field' do
+  test 'show_detail should show old and new values with a custom field' do
     detail = JournalDetail.new(:property => 'cf', :prop_key => '1',
                                :old_value => 'MySQL', :value => 'PostgreSQL')
     assert_equal 'Database changed from MySQL to PostgreSQL', show_detail(detail, true)
   end
 
-  test 'IssuesHelper#show_detail should show added file' do
+  test 'show_detail should show added file' do
     detail = JournalDetail.new(:property => 'attachment', :prop_key => '1',
                                :old_value => nil, :value => 'error281.txt')
     assert_match 'error281.txt', show_detail(detail, true)
   end
 
-  test 'IssuesHelper#show_detail should show removed file' do
+  test 'show_detail should show removed file' do
     detail = JournalDetail.new(:property => 'attachment', :prop_key => '1',
                                :old_value => 'error281.txt', :value => nil)
     assert_match 'error281.txt', show_detail(detail, true)
