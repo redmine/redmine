c5ac2d3966c2ac04a2580fca67a40acf24b51239
Jean-Philippe Lang <jp_lang@yahoo.fr>
2013-05-01 17:10:15 +0000

    Fixed that deleting a system activity with children in use raises an error (#13783).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@11763 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/object_helpers.rb
+++ test/object_helpers.rb
@@ -107,10 +107,11 @@ module ObjectHelpers
   def TimeEntry.generate!(attributes={})
     entry = TimeEntry.new(attributes)
     entry.user ||= User.find(2)
-    entry.issue ||= Issue.find(1)
+    entry.issue ||= Issue.find(1) unless entry.project
     entry.project ||= entry.issue.project
     entry.activity ||= TimeEntryActivity.first
     entry.spent_on ||= Date.today
+    entry.hours ||= 1.0
     entry.save!
     entry
   end
