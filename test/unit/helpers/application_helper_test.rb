360bda7e682e04a138c9d2891e847bf278d7a694
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2013-05-18 22:36:58 +0000

    include Redmine::I18n in ApplicationHelperTest
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@11877 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -20,6 +20,7 @@
 require File.expand_path('../../../test_helper', __FILE__)
 
 class ApplicationHelperTest < ActionView::TestCase
+  include Redmine::I18n
   include ERB::Util
   include Rails.application.routes.url_helpers
 
