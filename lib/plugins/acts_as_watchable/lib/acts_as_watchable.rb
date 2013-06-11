7371077fa27cfc7442a74be5e1c53b40fa6724d8
Jean-Philippe Lang <jp_lang@yahoo.fr>
2013-06-11 18:29:29 +0000

    Code cleanup.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@11960 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/plugins/acts_as_watchable/lib/acts_as_watchable.rb
+++ lib/plugins/acts_as_watchable/lib/acts_as_watchable.rb
@@ -14,8 +14,8 @@ module Redmine
             has_many :watcher_users, :through => :watchers, :source => :user, :validate => false
 
             scope :watched_by, lambda { |user_id|
-              { :include => :watchers,
-                :conditions => ["#{Watcher.table_name}.user_id = ?", user_id] }
+              joins(:watchers).
+              where("#{Watcher.table_name}.user_id = ?", user_id)
             }
             attr_protected :watcher_ids, :watcher_user_ids
           end
