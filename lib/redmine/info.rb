e07bc81c5e099a68bc8476d8a813551631b59776
Jean-Philippe Lang <jp_lang@yahoo.fr>
2013-07-11 18:43:48 +0000

    Add more info about the ruby version (#14419).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/branches/2.3-stable@12002 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/info.rb
+++ lib/redmine/info.rb
@@ -10,16 +10,16 @@ module Redmine
         s = "Environment:\n"
         s << [
           ["Redmine version", Redmine::VERSION],
-          ["Ruby version", "#{RUBY_VERSION} (#{RUBY_PLATFORM})"],
+          ["Ruby version", "#{RUBY_VERSION}-p#{RUBY_PATCHLEVEL} (#{RUBY_RELEASE_DATE}) [#{RUBY_PLATFORM}]"],
           ["Rails version", Rails::VERSION::STRING],
           ["Environment", Rails.env],
           ["Database adapter", ActiveRecord::Base.connection.adapter_name]
-        ].map {|info| "  %-40s %s" % info}.join("\n")
+        ].map {|info| "  %-30s %s" % info}.join("\n")
         s << "\nRedmine plugins:\n"
 
         plugins = Redmine::Plugin.all
         if plugins.any?
-          s << plugins.map {|plugin| "  %-40s %s" % [plugin.id.to_s, plugin.version.to_s]}.join("\n")
+          s << plugins.map {|plugin| "  %-30s %s" % [plugin.id.to_s, plugin.version.to_s]}.join("\n")
         else
           s << "  no plugin installed"
         end
