bc4785ca60d013892524e6e0c8fc4ad2dbacb13f
Jean-Philippe Lang <jp_lang@yahoo.fr>
2013-07-11 18:45:43 +0000

    Add more info about the ruby version (#14419).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@12003 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/info.rb
+++ lib/redmine/info.rb
@@ -10,24 +10,24 @@ module Redmine
         s = "Environment:\n"
         s << [
           ["Redmine version", Redmine::VERSION],
-          ["Ruby version", "#{RUBY_VERSION} (#{RUBY_PLATFORM})"],
+          ["Ruby version", "#{RUBY_VERSION}-p#{RUBY_PATCHLEVEL} (#{RUBY_RELEASE_DATE}) [#{RUBY_PLATFORM}]"],
           ["Rails version", Rails::VERSION::STRING],
           ["Environment", Rails.env],
           ["Database adapter", ActiveRecord::Base.connection.adapter_name]
-        ].map {|info| "  %-40s %s" % info}.join("\n") + "\n"
+        ].map {|info| "  %-30s %s" % info}.join("\n") + "\n"
 
         s << "SCM:\n"
         Redmine::Scm::Base.all.each do |scm|
           scm_class = "Repository::#{scm}".constantize
           if scm_class.scm_available
-            s << "  %-40s %s\n" % [scm, scm_class.scm_version_string]
+            s << "  %-30s %s\n" % [scm, scm_class.scm_version_string]
           end
         end
 
         s << "Redmine plugins:\n"
         plugins = Redmine::Plugin.all
         if plugins.any?
-          s << plugins.map {|plugin| "  %-40s %s" % [plugin.id.to_s, plugin.version.to_s]}.join("\n")
+          s << plugins.map {|plugin| "  %-30s %s" % [plugin.id.to_s, plugin.version.to_s]}.join("\n")
         else
           s << "  no plugin installed"
         end
