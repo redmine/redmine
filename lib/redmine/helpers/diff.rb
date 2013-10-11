461342dd2ec9226e8475fbf45025e320cf836cb3
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2013-10-11 17:37:38 +0000

    add missing "require 'diff'" to Redmine::Helpers::Diff (#15105)
    Contributed by Felix Bünemann.
    
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@12206 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/helpers/diff.rb
+++ lib/redmine/helpers/diff.rb
@@ -15,6 +15,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+require 'diff'
+
 module Redmine
   module Helpers
     class Diff
