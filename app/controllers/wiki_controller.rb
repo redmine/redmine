b8aa4da28a1e61d3d14e296cddd3584d505da3c3
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2013-10-11 17:37:51 +0000

    remove unneeded "require 'diff'" from WikiController (#15105)
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@12207 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/wiki_controller.rb
+++ app/controllers/wiki_controller.rb
@@ -15,8 +15,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-require 'diff'
-
 # The WikiController follows the Rails REST controller pattern but with
 # a few differences
 #
