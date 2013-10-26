e162f87964bd2f9721c415b2feb5a228e85644b6
Jean-Philippe Lang <jp_lang@yahoo.fr>
2013-10-26 08:08:45 +0000

    Replaces <option value=""></option> which is not HTML5 valid (#15191).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/redmine/trunk@12237 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -46,7 +46,7 @@ module ProjectsHelper
     end
 
     options = ''
-    options << "<option value=''></option>" if project.allowed_parents.include?(nil)
+    options << "<option value=''>&nbsp;</option>" if project.allowed_parents.include?(nil)
     options << project_tree_options_for_select(project.allowed_parents.compact, :selected => selected)
     content_tag('select', options.html_safe, :name => 'project[parent_id]', :id => 'project_parent_id')
   end
