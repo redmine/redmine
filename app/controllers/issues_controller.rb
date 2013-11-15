071bb653b81c74a15fd3f1a8af44980a80308bc2
Jean-Philippe Lang <jp_lang@yahoo.fr>
2013-11-15 20:32:26 +0000

    Merged r12277 (#15344).
    git-svn-id: http://svn.redmine.org/redmine/branches/2.3-stable@12279 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -410,7 +410,7 @@ class IssuesController < ApplicationController
     @issue.safe_attributes = params[:issue]
 
     @priorities = IssuePriority.active
-    @allowed_statuses = @issue.new_statuses_allowed_to(User.current, true)
+    @allowed_statuses = @issue.new_statuses_allowed_to(User.current, @issue.new_record?)
     @available_watchers = (@issue.project.users.sort + @issue.watcher_users).uniq
   end
 
