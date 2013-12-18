df92208cf014265bf81e02f6b76df21ddfc87a0d
MnrUchida <mukenan108@gmail.com>
2013-12-18 20:50:20 +0900

    テストコードを追加

--- plugins/github_relation/spec/models/github_issue_spec.rb
+++ plugins/github_relation/spec/models/github_issue_spec.rb
@@ -8,23 +8,27 @@ describe 'github_issue' do
   context "create_issues" do
 
     before do
-      user = Hashie::Mash.new
-      user.login = "user_login"
-      assignee = Hashie::Mash.new
-      assignee.login = "assignee_login"
+      github_users = %w{user assignee}.map do |user_name|
+                        user = Hashie::Mash.new
+                        user.login = "#{user_name}_login"
+                        user
+                      end
+      GithubUser.create_users(github_users)
 
-      @github_issues = 10.times.map do |index|
+      github_issues = 10.times.map do |index|
                         issue = Hashie::Mash.new
                         issue.number = index
                         issue.title = "title#{index}"
                         issue.body = "body#{index}"
                         issue.created_at = Time.parse("2010/01/01")
                         issue.updated_at = Time.parse("2011/01/01")
-                        issue.user = user
+                        issue.user = github_users[0]
                         issue
                       end
-      @github_issues[5].assignee = assignee
-      GithubIssue.create_issues(project, @github_issues)
+      github_issues[5].assignee = github_users[1]
+      github_issues[6].user.login = "hoge"
+
+      GithubIssue.create_issues(project, github_issues)
     end
 
     subject{GithubIssue.scoped.order(:issue_number)}
@@ -36,9 +40,24 @@ describe 'github_issue' do
         github_issue.issue.description.should == "body#{index}"
         github_issue.issue.created_on.should == Time.parse("2010/01/01")
         github_issue.issue.updated_on.should == Time.parse("2011/01/01")
-        github_issue.issue.author.should == User.first
-        github_issue.issue.assigned_to.should == nil
       end
+    end
+    it "GithubIssues User" do
+      user = GithubUser.where(login: "user_login").first.user
+
+      (0..4).each do |index|
+        subject[index].issue.author.should == user
+        subject[index].issue.assigned_to.should == nil
+      end
+      (7..9).each do |index|
+        subject[index].issue.author.should == user
+        subject[index].issue.assigned_to.should == nil
+      end
+
+      subject[5].issue.author.should == user
+      subject[5].issue.assigned_to.should == GithubUser.where(login: "assignee_login").first.user
+      subject[6].issue.author.should == User.first
+      subject[6].issue.assigned_to.should == nil
     end
   end
 end
--- plugins/github_relation/spec/models/github_user_spec.rb
+++ plugins/github_relation/spec/models/github_user_spec.rb
@@ -3,8 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe 'github_user' do
   let!(:project){Project.create(name: 'github_test', identifier: 'github_test')}
 
-  context "create_users" do
-
+  describe "create_users" do
     before do
       github_users =
           10.times.map do |index|
@@ -15,11 +14,47 @@ describe 'github_user' do
       GithubUser.create_users(github_users)
     end
 
-    subject{GithubUser.scoped.order(:login)}
-    its(:count){should == 10}
-    it "GithubUsers" do
-      subject.each_with_index do |github_user, index|
-        github_user.login.should == "login#{index}"
+    context "create" do
+      subject{GithubUser.scoped.order(:login)}
+      its(:count){should == 10}
+      it "GithubUsers" do
+        subject.each_with_index do |github_user, index|
+          github_user.login.should == "login#{index}"
+        end
+      end
+    end
+
+    context "duplicate" do
+      before do
+        user = Hashie::Mash.new
+        user.login = "login6"
+        GithubUser.create_users([user])
+      end
+
+      subject{GithubUser.scoped.order(:login)}
+      its(:count){should == 10}
+      it "GithubUsers" do
+        subject.each_with_index do |github_user, index|
+          github_user.login.should == "login#{index}"
+        end
+      end
+    end
+
+    context "add" do
+      before do
+        user = Hashie::Mash.new
+        user.login = "login10"
+        GithubUser.create_users([user])
+      end
+
+      subject{GithubUser.scoped.order(:login)}
+      its(:count){should == 11}
+      it "GithubUsers" do
+        login_idx = [0,1,10,2,3,4,5,6,7,8,9]
+
+        subject.each_with_index do |github_user, index|
+          github_user.login.should == "login#{login_idx[index]}"
+        end
       end
     end
   end
