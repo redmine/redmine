d731c036fb72bfc770c4bbdb548a587eddfcb68d
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2014-01-08 05:55:52 +0000

    Rails4: replace deprecated find_all_by_* at GroupsController
    git-svn-id: http://svn.redmine.org/redmine/trunk@12518 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -90,7 +90,7 @@ class GroupsController < ApplicationController
   end
 
   def add_users
-    @users = User.find_all_by_id(params[:user_id] || params[:user_ids])
+    @users = User.where(:id => (params[:user_id] || params[:user_ids])).all
     @group.users << @users if request.post?
     respond_to do |format|
       format.html { redirect_to edit_group_path(@group, :tab => 'users') }
