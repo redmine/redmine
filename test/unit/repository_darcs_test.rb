776bf76f46a36e76bf1fc27bf13a1f4aa8c04f76
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2014-01-09 09:54:54 +0000

    remove unneeded Relation#all from RepositoryDarcsTest
    git-svn-id: http://svn.redmine.org/redmine/trunk@12575 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/repository_darcs_test.rb
+++ test/unit/repository_darcs_test.rb
@@ -79,7 +79,7 @@ class RepositoryDarcsTest < ActiveSupport::TestCase
       assert_equal NUM_REV, @repository.changesets.count
 
       # Remove changesets with revision > 3
-      @repository.changesets.all.each {|c| c.destroy if c.revision.to_i > 3}
+      @repository.changesets.each {|c| c.destroy if c.revision.to_i > 3}
       @project.reload
       @repository.reload
       assert_equal 3, @repository.changesets.count
