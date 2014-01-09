d7f986b6a8f2f82836ee6285aa8d1230274c9d80
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2014-01-09 04:23:36 +0000

    Rails4: replace deprecated Relation#update_all at ProjectNestedSetTest
    git-svn-id: http://svn.redmine.org/redmine/trunk@12552 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/project_nested_set_test.rb
+++ test/unit/project_nested_set_test.rb
@@ -55,7 +55,7 @@ class ProjectNestedSetTest < ActiveSupport::TestCase
   end
 
   def test_rebuild_tree_should_build_valid_tree_even_with_valid_lft_rgt_values
-    Project.update_all "name = 'YY'", {:id => @a.id }
+    Project.where({:id => @a.id }).update_all("name = 'YY'")
     # lft and rgt values are still valid (Project.rebuild! would not update anything)
     # but projects are not ordered properly (YY is in the first place)
 
