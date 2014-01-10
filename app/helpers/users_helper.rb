fa544d99345fe23b51a427324266996154a02024
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2014-01-10 12:48:48 +0000

    Rails4: replace deprecated Relation#count with finder options at UsersHelper#users_status_options_for_select
    git-svn-id: http://svn.redmine.org/redmine/trunk@12612 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -19,7 +19,7 @@
 
 module UsersHelper
   def users_status_options_for_select(selected)
-    user_count_by_status = User.count(:group => 'status').to_hash
+    user_count_by_status = User.group('status').count.to_hash
     options_for_select([[l(:label_all), ''],
                         ["#{l(:status_active)} (#{user_count_by_status[1].to_i})", '1'],
                         ["#{l(:status_registered)} (#{user_count_by_status[2].to_i})", '2'],
