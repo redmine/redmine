ca4ea40888babf40f796c33126c4b7e0e77ab520
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2014-01-11 07:05:48 +0000

    fix find_all_by_id(n1, n2) parameter at IssuesCustomFieldsVisibilityTest#test_index_with_partial_custom_field_visibility
    find_all_by_id(n1, n2) returns only n1 result.
    
    git-svn-id: http://svn.redmine.org/redmine/trunk@12621 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/issues_custom_fields_visibility_test.rb
+++ test/functional/issues_custom_fields_visibility_test.rb
@@ -199,7 +199,7 @@ class IssuesCustomFieldsVisibilityTest < ActionController::TestCase
     p1 = Project.generate!
     p2 = Project.generate!
     user = User.generate!
-    User.add_to_project(user, p1, Role.find_all_by_id(1,3))
+    User.add_to_project(user, p1, Role.find_all_by_id([1, 3]))
     User.add_to_project(user, p2, Role.find_all_by_id(3))
     Issue.generate!(:project => p1, :tracker_id => 1, :custom_field_values => {@field2.id => 'ValueA'})
     Issue.generate!(:project => p2, :tracker_id => 1, :custom_field_values => {@field2.id => 'ValueB'})
