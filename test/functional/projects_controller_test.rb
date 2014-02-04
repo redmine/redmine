67e3af6c97cf80881a85ca10382842bdabf19634
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2014-02-04 23:50:14 +0000

    code format clean up ProjectsControllerTest
    git-svn-id: http://svn.redmine.org/redmine/trunk@12796 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/projects_controller_test.rb
+++ test/functional/projects_controller_test.rb
@@ -18,8 +18,10 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class ProjectsControllerTest < ActionController::TestCase
-  fixtures :projects, :versions, :users, :roles, :members, :member_roles, :issues, :journals, :journal_details,
-           :trackers, :projects_trackers, :issue_statuses, :enabled_modules, :enumerations, :boards, :messages,
+  fixtures :projects, :versions, :users, :roles, :members,
+           :member_roles, :issues, :journals, :journal_details,
+           :trackers, :projects_trackers, :issue_statuses,
+           :enabled_modules, :enumerations, :boards, :messages,
            :attachments, :custom_fields, :custom_values, :time_entries
 
   def setup
