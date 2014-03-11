b772319977d71114118de91db0096cda14e7a7d2
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2014-03-11 09:59:56 +0000

    fix source indent of lib/redmine/views/labelled_form_builder.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@12958 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/views/labelled_form_builder.rb
+++ lib/redmine/views/labelled_form_builder.rb
@@ -44,12 +44,12 @@ class Redmine::Views::LabelledFormBuilder < ActionView::Helpers::FormBuilder
 
   # Returns a label tag for the given field
   def label_for_field(field, options = {})
-      return ''.html_safe if options.delete(:no_label)
-      text = options[:label].is_a?(Symbol) ? l(options[:label]) : options[:label]
-      text ||= l(("field_" + field.to_s.gsub(/\_id$/, "")).to_sym)
-      text += @template.content_tag("span", " *", :class => "required") if options.delete(:required)
-      @template.content_tag("label", text.html_safe,
-                                     :class => (@object && @object.errors[field].present? ? "error" : nil),
-                                     :for => (@object_name.to_s + "_" + field.to_s))
+    return ''.html_safe if options.delete(:no_label)
+    text = options[:label].is_a?(Symbol) ? l(options[:label]) : options[:label]
+    text ||= l(("field_" + field.to_s.gsub(/\_id$/, "")).to_sym)
+    text += @template.content_tag("span", " *", :class => "required") if options.delete(:required)
+    @template.content_tag("label", text.html_safe,
+                                   :class => (@object && @object.errors[field].present? ? "error" : nil),
+                                   :for => (@object_name.to_s + "_" + field.to_s))
   end
 end
