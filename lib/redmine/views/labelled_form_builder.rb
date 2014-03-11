c67043e01c6b25a0f7ae7e193532b911315aae10
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2014-03-11 09:59:40 +0000

    fix source indent of lib/redmine/views/labelled_form_builder.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@12957 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/views/labelled_form_builder.rb
+++ lib/redmine/views/labelled_form_builder.rb
@@ -39,7 +39,7 @@ class Redmine::Views::LabelledFormBuilder < ActionView::Helpers::FormBuilder
   end
 
   def time_zone_select(field, priority_zones = nil, options = {}, html_options = {})
-        label_for_field(field, options) + super(field, priority_zones, options, html_options.except(:label)).html_safe
+    label_for_field(field, options) + super(field, priority_zones, options, html_options.except(:label)).html_safe
   end
 
   # Returns a label tag for the given field
