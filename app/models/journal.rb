4bf18a697c04e0f84eaa036f81d2ecd5691b5520
Jean-Philippe Lang <jp_lang@yahoo.fr>
2014-03-17 08:20:27 +0000

    Adds an option to send email on "Assignee updated" in application settings (#16362).
    git-svn-id: http://svn.redmine.org/redmine/trunk@12975 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -190,7 +190,7 @@ class Journal < ActiveRecord::Base
     if notify? && (Setting.notified_events.include?('issue_updated') ||
         (Setting.notified_events.include?('issue_note_added') && notes.present?) ||
         (Setting.notified_events.include?('issue_status_updated') && new_status.present?) ||
-        (Setting.notified_events.include?('issue_assigned_to_updated') && new_value_for('assigned_to_id').present?) ||
+        (Setting.notified_events.include?('issue_assigned_to_updated') && detail_for_attribute('assigned_to_id').present?) ||
         (Setting.notified_events.include?('issue_priority_updated') && new_value_for('priority_id').present?)
       )
       Mailer.deliver_issue_edit(self)
