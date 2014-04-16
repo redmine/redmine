e830dbb5eda62f1cb11c9899ecec3ca424cc893a
adrianq <adrian.perez@ucl.ac.uk>
2014-04-16 19:17:41 +0100

    Working on gui for osbmv

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -281,6 +281,8 @@ class ApplicationController < ActionController::Base
 
   # Find project of id params[:project_id]
   def find_project_by_project_id
+    print "taka"
+    print params[:project_id]
     @project = Project.find(params[:project_id])
   rescue ActiveRecord::RecordNotFound
     render_404
