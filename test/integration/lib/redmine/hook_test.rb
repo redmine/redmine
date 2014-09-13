416ebc222db4604e1f4740189ec4d18c3c454f51
Jean-Philippe Lang <jp_lang@yahoo.fr>
2014-09-13 11:09:01 +0000

    Adds a test for default context of controller hooks (#16930).
    git-svn-id: http://svn.redmine.org/redmine/trunk@13394 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/integration/lib/redmine/hook_test.rb
+++ test/integration/lib/redmine/hook_test.rb
@@ -45,6 +45,15 @@ class HookTest < ActionController::IntegrationTest
 VIEW
   end
 
+  # Hooks that stores the call context
+  class ContextTestHook < Redmine::Hook::ViewListener
+    cattr_accessor :context
+
+    def controller_account_success_authentication_after(context)
+      self.class.context = context
+    end
+  end
+
   def setup
     Redmine::Hook.clear_listeners
   end
@@ -85,5 +94,15 @@ VIEW
       assert_select 'script[src=/plugin_assets/test_plugin/javascripts/test_plugin.js]'
       assert_select 'link[href=/plugin_assets/test_plugin/stylesheets/test_plugin.css]'
     end
+  end
+
+  def test_controller_hook_context_should_include_request
+    Redmine::Hook.add_listener(ContextTestHook)
+    post '/login', :username => 'admin', :password => 'admin'
+    assert_not_nil ContextTestHook.context
+    context = ContextTestHook.context
+    assert_kind_of ActionDispatch::Request, context[:request]
+    assert_kind_of Hash, context[:request].params
+    assert_kind_of AccountController, context[:hook_caller]
   end
 end
