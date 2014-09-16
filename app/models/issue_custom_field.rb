5a96012ad0ead59e2af651226766a4899c358b84
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2014-09-16 02:54:12 +0000

    remove trailing white-space from app/models/issue_custom_field.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@13410 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/issue_custom_field.rb
+++ app/models/issue_custom_field.rb
@@ -32,7 +32,7 @@ class IssueCustomField < CustomField
     sql = super
     id_column ||= id
     tracker_condition = "#{Issue.table_name}.tracker_id IN (SELECT tracker_id FROM #{table_name_prefix}custom_fields_trackers#{table_name_suffix} WHERE custom_field_id = #{id_column})"
-    project_condition = "EXISTS (SELECT 1 FROM #{CustomField.table_name} ifa WHERE ifa.is_for_all = #{connection.quoted_true} AND ifa.id = #{id_column})" + 
+    project_condition = "EXISTS (SELECT 1 FROM #{CustomField.table_name} ifa WHERE ifa.is_for_all = #{connection.quoted_true} AND ifa.id = #{id_column})" +
       " OR #{Issue.table_name}.project_id IN (SELECT project_id FROM #{table_name_prefix}custom_fields_projects#{table_name_suffix} WHERE custom_field_id = #{id_column})"
 
     "((#{sql}) AND (#{tracker_condition}) AND (#{project_condition}))"
