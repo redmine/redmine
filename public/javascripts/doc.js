1443c1d3936105880e28dac6ed35c6441bcb5605
Adrian Quintana <adrian.perez@ucl.ac.uk>
2014-10-13 12:10:29 +0100

    #124 Every page is loaded independtly

---
+++ public/javascripts/doc.js
@@ -0,0 +1,24 @@
+jQuery(function()
+{
+	// Show/hide sections in doc
+	function enableDocSection(sectionId){
+		$("#project_overview_sections section").hide();
+		$(sectionId).show();
+		window.scrollTo(0,0);
+	}
+	
+	//	Show/hide sections when section click in side menu
+	$("#docContainer #project_overview_list a").click(
+		function(event){
+			enableDocSection($(this).attr("href").substring());
+			event.preventDefault();
+		}
+	);
+
+	//Check if url points to a specific section 
+	var a = location.href.split("#");
+	if ( a.length > 1){
+		enableDocSection("#"+a[1]);
+	}
+
+});
