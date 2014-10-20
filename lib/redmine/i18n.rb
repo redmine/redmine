778625de9a00d2c0bc2f660a7e9bf7b9ac8f8bfd
Jean-Philippe Lang <jp_lang@yahoo.fr>
2014-10-20 21:10:32 +0000

    Merged r13450 (#18110).
    git-svn-id: http://svn.redmine.org/redmine/branches/2.6-stable@13462 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/i18n.rb
+++ lib/redmine/i18n.rb
@@ -90,11 +90,19 @@ module Redmine
     # Returns an array of languages names and code sorted by names, example:
     # [["Deutsch", "de"], ["English", "en"] ...]
     #
-    # The result is cached to prevent from loading all translations files.
-    def languages_options
-      ActionController::Base.cache_store.fetch "i18n/languages_options" do
-        valid_languages.map {|lang| [ll(lang.to_s, :general_lang_name), lang.to_s]}.sort {|x,y| x.first <=> y.first }
-      end      
+    # The result is cached to prevent from loading all translations files
+    # unless :cache => false option is given
+    def languages_options(options={})
+      if options[:cache] == false
+        valid_languages.
+          select {|locale| ::I18n.exists?(:general_lang_name, locale)}.
+          map {|lang| [ll(lang.to_s, :general_lang_name), lang.to_s]}.
+          sort {|x,y| x.first <=> y.first }
+      else
+        ActionController::Base.cache_store.fetch "i18n/languages_options" do
+          languages_options :cache => false
+        end
+      end
     end
 
     def find_language(lang)
