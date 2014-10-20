3a0a55c8ba37dda072d9d3e788713ad430ed7bb4
Jean-Philippe Lang <jp_lang@yahoo.fr>
2014-10-20 21:11:08 +0000

    Merged r13451 (#18119).
    git-svn-id: http://svn.redmine.org/redmine/branches/2.6-stable@13463 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/lib/redmine/wiki_formatting/macros_test.rb
+++ test/unit/lib/redmine/wiki_formatting/macros_test.rb
@@ -302,17 +302,26 @@ RAW
   end
 
   def test_macro_thumbnail
-    link = link_to('<img alt="testfile.PNG" src="http://test.host/attachments/thumbnail/17" />'.html_safe,
-                   "http://test.host/attachments/17",
+    link = link_to('<img alt="testfile.PNG" src="/attachments/thumbnail/17" />'.html_safe,
+                   "/attachments/17",
                    :class => "thumbnail",
                    :title => "testfile.PNG")
     assert_equal "<p>#{link}</p>",
                  textilizable("{{thumbnail(testfile.png)}}", :object => Issue.find(14))
   end
 
-  def test_macro_thumbnail_with_size
-    link = link_to('<img alt="testfile.PNG" src="http://test.host/attachments/thumbnail/17/200" />'.html_safe,
+  def test_macro_thumbnail_with_full_path
+    link = link_to('<img alt="testfile.PNG" src="http://test.host/attachments/thumbnail/17" />'.html_safe,
                    "http://test.host/attachments/17",
+                   :class => "thumbnail",
+                   :title => "testfile.PNG")
+    assert_equal "<p>#{link}</p>",
+                 textilizable("{{thumbnail(testfile.png)}}", :object => Issue.find(14), :only_path => false)
+  end
+
+  def test_macro_thumbnail_with_size
+    link = link_to('<img alt="testfile.PNG" src="/attachments/thumbnail/17/200" />'.html_safe,
+                   "/attachments/17",
                    :class => "thumbnail",
                    :title => "testfile.PNG")
     assert_equal "<p>#{link}</p>",
@@ -320,8 +329,8 @@ RAW
   end
 
   def test_macro_thumbnail_with_title
-    link = link_to('<img alt="testfile.PNG" src="http://test.host/attachments/thumbnail/17" />'.html_safe,
-                   "http://test.host/attachments/17",
+    link = link_to('<img alt="testfile.PNG" src="/attachments/thumbnail/17" />'.html_safe,
+                   "/attachments/17",
                    :class => "thumbnail",
                    :title => "Cool image")
     assert_equal "<p>#{link}</p>",
