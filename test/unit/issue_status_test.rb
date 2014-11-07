426780c6b18da6a5a83b921a1885d7283116456b
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2014-11-07 00:38:21 +0000

    add missing fixtures to test/unit/issue_status_test.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@13569 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/issue_status_test.rb
+++ test/unit/issue_status_test.rb
@@ -18,7 +18,15 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class IssueStatusTest < ActiveSupport::TestCase
-  fixtures :issue_statuses, :issues, :roles, :trackers
+  fixtures :projects, :users, :members, :member_roles, :roles,
+           :groups_users,
+           :trackers, :projects_trackers,
+           :enabled_modules,
+           :versions,
+           :issue_statuses, :issue_categories, :issue_relations, :workflows,
+           :enumerations,
+           :issues, :journals, :journal_details,
+           :custom_fields, :custom_fields_projects, :custom_fields_trackers, :custom_values
 
   def test_create
     status = IssueStatus.new :name => "Assigned"
