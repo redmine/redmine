53ff2bce3ebc655ad93f27ce5368a6187a1e09cc
Adrian Quintana <adrian.perez@ucl.ac.uk>
2014-11-18 13:26:39 +0000

    Update synapses script

--- public/geppetto/scripts/osbSynapseScript.js
+++ public/geppetto/scripts/osbSynapseScript.js
@@ -5,4 +5,4 @@ $ENTER_ID.electrical.getModelTree();
 
 // Adding Scatter3d 1
 G.addWidget(3);
-TreeVisualiserDAT1.setData($ENTER_ID, {expandNodes: true});
+TreeVisualiserDAT1.setData($ENTER_ID.electrical.ModelTree.Summary, {expandNodes: true});
