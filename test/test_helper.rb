90e16a35c5d85d8f3aed31ffe040bddfab2c9109
Jean-Philippe Lang <jp_lang@yahoo.fr>
2014-12-14 16:31:11 +0000

    Missing test helper (#18537).
    git-svn-id: http://svn.redmine.org/redmine/trunk@13761 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -163,6 +163,10 @@ class ActiveSupport::TestCase
     hs
   end
 
+  def sqlite?
+    ActiveRecord::Base.connection.adapter_name =~ /sqlite/i
+  end
+
   def assert_save(object)
     saved = object.save
     message = "#{object.class} could not be saved"
