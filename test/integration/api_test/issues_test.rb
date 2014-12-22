3fe8197d73234ea68dd50b96002ad78b4ee58463
Jean-Philippe Lang <jp_lang@yahoo.fr>
2014-12-22 13:46:31 +0000

    Use raw request content in tests to make sure that XML/JSON payload is properly parsed.
    git-svn-id: http://svn.redmine.org/redmine/trunk@13792 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/integration/api_test/issues_test.rb
+++ test/integration/api_test/issues_test.rb
@@ -328,10 +328,19 @@ class Redmine::ApiTest::IssuesTest < Redmine::ApiTest::Base
   end
 
   test "POST /issues.xml should create an issue with the attributes" do
+
+payload = <<-XML
+<?xml version="1.0" encoding="UTF-8" ?>
+<issue>
+  <project_id>1</project_id>
+  <tracker_id>2</tracker_id>
+  <status_id>3</status_id>
+  <subject>API test</subject>
+</issue>
+XML
+
     assert_difference('Issue.count') do
-      post '/issues.xml',
-           {:issue => {:project_id => 1, :subject => 'API test',
-            :tracker_id => 2, :status_id => 3}}, credentials('jsmith')
+      post '/issues.xml', payload, {"CONTENT_TYPE" => 'application/xml'}.merge(credentials('jsmith'))
     end
     issue = Issue.order('id DESC').first
     assert_equal 1, issue.project_id
@@ -365,11 +374,20 @@ class Redmine::ApiTest::IssuesTest < Redmine::ApiTest::Base
   end
 
   test "POST /issues.json should create an issue with the attributes" do
+
+payload = <<-JSON
+{
+  "issue": {
+    "project_id": "1",
+    "tracker_id": "2",
+    "status_id": "3",
+    "subject": "API test"
+  }
+}
+JSON
+
     assert_difference('Issue.count') do
-      post '/issues.json',
-           {:issue => {:project_id => 1, :subject => 'API test',
-                       :tracker_id => 2, :status_id => 3}},
-           credentials('jsmith')
+      post '/issues.json', payload, {"CONTENT_TYPE" => 'application/json'}.merge(credentials('jsmith'))
     end
 
     issue = Issue.order('id DESC').first
