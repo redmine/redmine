471df16f9c02c6a9b2c0ae3b15624d45e3f79a97
Jean-Philippe Lang <jp_lang@yahoo.fr>
2015-01-17 14:55:17 +0000

    Missing fixtures.
    git-svn-id: http://svn.redmine.org/redmine/trunk@13890 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/news_test.rb
+++ test/unit/news_test.rb
@@ -18,7 +18,7 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class NewsTest < ActiveSupport::TestCase
-  fixtures :projects, :users, :roles, :members, :member_roles, :enabled_modules, :news
+  fixtures :projects, :users, :email_addresses, :roles, :members, :member_roles, :enabled_modules, :news
 
   def valid_news
     { :title => 'Test news', :description => 'Lorem ipsum etc', :author => User.first }
