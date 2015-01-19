caa02e65375ded3d4650a72678567423f23b25ce
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2015-01-19 04:41:49 +0000

    add missing fixture
    git-svn-id: http://svn.redmine.org/redmine/trunk@13916 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/journal_observer_test.rb
+++ test/unit/journal_observer_test.rb
@@ -20,7 +20,7 @@ require File.expand_path('../../test_helper', __FILE__)
 class JournalObserverTest < ActiveSupport::TestCase
   fixtures :issues, :issue_statuses, :journals, :journal_details, :projects,
            :projects_trackers, :trackers, :enabled_modules, :enumerations,
-           :users, :roles
+           :users, :email_addresses, :roles
 
   def setup
     ActionMailer::Base.deliveries.clear
