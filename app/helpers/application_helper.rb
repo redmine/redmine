91967554b93f2477ca13e9389dd6379b50922dd6
Jean-Philippe Lang <jp_lang@yahoo.fr>
2015-01-24 10:58:21 +0000

    Use named route.
    git-svn-id: http://svn.redmine.org/redmine/trunk@13941 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -425,7 +425,7 @@ module ApplicationHelper
   def time_tag(time)
     text = distance_of_time_in_words(Time.now, time)
     if @project
-      link_to(text, {:controller => 'activities', :action => 'index', :id => @project, :from => User.current.time_to_date(time)}, :title => format_time(time))
+      link_to(text, project_activity_path(@project, :from => User.current.time_to_date(time)), :title => format_time(time))
     else
       content_tag('abbr', text, :title => format_time(time))
     end
