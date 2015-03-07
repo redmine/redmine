f821eaf7221ca05e86efb49d25ac7e3a236e63f7
Jean-Philippe Lang <jp_lang@yahoo.fr>
2015-03-07 08:12:08 +0000

    Adds the date format for ambiguous dates (#19243).
    git-svn-id: http://svn.redmine.org/redmine/trunk@14048 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -125,4 +125,15 @@ module SettingsHelper
 
     options.map {|label, value| [l(label), value.to_s]}
   end
+
+	# Returns the options for the date_format setting
+  def date_format_setting_options(locale)
+    Setting::DATE_FORMATS.map do |f|
+      today = ::I18n.l(Date.today, :locale => locale, :format => f)
+      format = f.gsub('%', '').gsub(/[dmY]/) do
+        {'d' => 'dd', 'm' => 'mm', 'Y' => 'yyyy'}[$&]
+      end
+      ["#{today} (#{format})", f]
+    end
+  end
 end
