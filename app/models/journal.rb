cb9d405db3ba4b2ec772a0c67dd8f45e192f01b0
Jean-Philippe Lang <jp_lang@yahoo.fr>
2015-04-12 07:55:30 +0000

    Fixed that journals are shown multiple times in activity (#19168).
    git-svn-id: http://svn.redmine.org/redmine/trunk@14163 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -38,7 +38,7 @@ class Journal < ActiveRecord::Base
                             :scope => preload({:issue => :project}, :user).
                                       joins("LEFT OUTER JOIN #{JournalDetail.table_name} ON #{JournalDetail.table_name}.journal_id = #{Journal.table_name}.id").
                                       where("#{Journal.table_name}.journalized_type = 'Issue' AND" +
-                                            " (#{JournalDetail.table_name}.prop_key = 'status_id' OR #{Journal.table_name}.notes <> '')")
+                                            " (#{JournalDetail.table_name}.prop_key = 'status_id' OR #{Journal.table_name}.notes <> '')").uniq
 
   before_create :split_private_notes
   after_create :send_notification
