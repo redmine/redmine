ef8328ab3bee90ed8e54308e2a6e665d9691c765
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2015-04-20 10:50:46 +0000

    remove trailing white spaces from public/javascripts/jstoolbar/markdown.js
    git-svn-id: http://svn.redmine.org/redmine/trunk@14188 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- public/javascripts/jstoolbar/markdown.js
+++ public/javascripts/jstoolbar/markdown.js
@@ -7,12 +7,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * DotClear is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with DotClear; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -66,7 +66,7 @@ jsToolBar.prototype.elements.h1 = {
 	type: 'button',
 	title: 'Heading 1',
 	fn: {
-		wiki: function() { 
+		wiki: function() {
 		  this.encloseLineSelection('# ', '',function(str) {
 		    str = str.replace(/^#+\s+/, '')
 		    return str;
@@ -78,7 +78,7 @@ jsToolBar.prototype.elements.h2 = {
 	type: 'button',
 	title: 'Heading 2',
 	fn: {
-		wiki: function() { 
+		wiki: function() {
 		  this.encloseLineSelection('## ', '',function(str) {
 		    str = str.replace(/^#+\s+/, '')
 		    return str;
@@ -90,7 +90,7 @@ jsToolBar.prototype.elements.h3 = {
 	type: 'button',
 	title: 'Heading 3',
 	fn: {
-		wiki: function() { 
+		wiki: function() {
 		  this.encloseLineSelection('### ', '',function(str) {
 		    str = str.replace(/^#+\s+/, '')
 		    return str;
