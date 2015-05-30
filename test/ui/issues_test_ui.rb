affeabcc526449057b29ca636f456234a87798b6
Jean-Philippe Lang <jp_lang@yahoo.fr>
2015-05-30 08:37:22 +0000

    Missing fixtures.
    git-svn-id: http://svn.redmine.org/redmine/trunk@14286 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/ui/issues_test_ui.rb
+++ test/ui/issues_test_ui.rb
@@ -18,7 +18,7 @@
 require File.expand_path('../base', __FILE__)
 
 class Redmine::UiTest::IssuesTest < Redmine::UiTest::Base
-  fixtures :projects, :users, :roles, :members, :member_roles,
+  fixtures :projects, :users, :email_addresses, :roles, :members, :member_roles,
            :trackers, :projects_trackers, :enabled_modules, :issue_statuses, :issues,
            :enumerations, :custom_fields, :custom_values, :custom_fields_trackers,
            :watchers
