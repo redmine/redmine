redmine/redmine

commit 89edc4dd39419e9c044dac397f33cceb5acbb0dc

Sort members on all their roles.
git-svn-id: http://svn.redmine.org/redmine/trunk@14366 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/models/member.rb
@@ -64,7 +64,7 @@ class Member < ActiveRecord::Base
   end
 
   def <=>(member)
-    a, b = roles.sort.first, member.roles.sort.first
+    a, b = roles.sort, member.roles.sort
     if a == b
       if principal
         principal <=> member.principal

commit 0dd95cdbc53e5805df2969887a40d9b6a4acfa33

Fixed test failure introduced by r14366.
git-svn-id: http://svn.redmine.org/redmine/trunk@14368 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/models/member.rb
@@ -71,8 +71,8 @@ class Member < ActiveRecord::Base
       else
         1
       end
-    elsif a
-      a <=> b
+    elsif a.any?
+      b.any? ? a <=> b : -1
     else
       1
     end