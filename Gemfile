0dc3c2fe1c864b3236e23cad7592d359d40a8075
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2015-08-31 07:38:29 +0000

    add roadie-rails gem for e-mail inline styles (#8335, #20641)
    Contributed by Go MAEDA.
    
    git-svn-id: http://svn.redmine.org/redmine/trunk@14540 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem "protected_attributes"
 gem "actionpack-action_caching"
 gem "actionpack-xml_parser"
 gem "loofah", "~> 2.0"
+gem "roadie-rails"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :x64_mingw, :mswin, :jruby]
--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -15,6 +15,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+require 'roadie'
+
 class Mailer < ActionMailer::Base
   layout 'mailer'
   helper :application
@@ -22,6 +24,7 @@ class Mailer < ActionMailer::Base
   helper :custom_fields
 
   include Redmine::I18n
+  include Roadie::Rails::Automatic
 
   def self.default_url_options
     options = {:protocol => Setting.protocol}
