868d949f47c2767f2d8943edf530dfbc9d00fb73
Jean-Philippe Lang <jp_lang@yahoo.fr>
2015-11-11 07:34:15 +0000

    Reverts r14812 (#6969).
    git-svn-id: http://svn.redmine.org/redmine/trunk@14863 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redcloth3.rb
+++ lib/redcloth3.rb
@@ -1202,7 +1202,7 @@ class RedCloth3 < String
     ALLOWED_TAGS = %w(redpre pre code notextile)
     
     def escape_html_tags(text)
-      text.gsub!(%r{<(\/?([!\w]+)[^<>\n]*)?(>?)}) {|m| $2 && ALLOWED_TAGS.include?($2) ? "<#{$1}#{$3}" : "&lt;#{$1}#{'&gt;' unless $3.blank?}" }
+      text.gsub!(%r{<(\/?([!\w]+)[^<>\n]*)(>?)}) {|m| ALLOWED_TAGS.include?($2) ? "<#{$1}#{$3}" : "&lt;#{$1}#{'&gt;' unless $3.blank?}" }
     end
 end
 
