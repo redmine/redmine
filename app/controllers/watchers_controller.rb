9b52ae5c5a6492c6a1a3c7eaf5d62f5e8dccd7fa
Jean-Philippe Lang <jp_lang@yahoo.fr>
2015-12-05 10:21:07 +0000

    Fixed that user with permission can't remove a locked watcher (#21382).
    git-svn-id: http://svn.redmine.org/redmine/trunk@14946 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/watchers_controller.rb
+++ app/controllers/watchers_controller.rb
@@ -62,12 +62,14 @@ class WatchersController < ApplicationController
   end
 
   def destroy
-    @watched.set_watcher(User.visible.find(params[:user_id]), false)
+    @watched.set_watcher(User.find(params[:user_id]), false)
     respond_to do |format|
       format.html { redirect_to :back }
       format.js
       format.api { render_api_ok }
     end
+  rescue ActiveRecord::RecordNotFound
+    render_404
   end
 
   def autocomplete_for_user
