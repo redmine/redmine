719d4b527d043379afc14b9149e197a7d0ba93e1
Jean-Philippe Lang <jp_lang@yahoo.fr>
2016-01-09 11:18:55 +0000

    Tooltip on progress bar (#21497).
    Patch by Go MAEDA.
    
    git-svn-id: http://svn.redmine.org/redmine/trunk@15027 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1108,12 +1108,14 @@ module ApplicationHelper
     pcts = pcts.collect(&:round)
     pcts[1] = pcts[1] - pcts[0]
     pcts << (100 - pcts[1] - pcts[0])
+    titles = options[:titles].to_a
+    titles[0] = "#{pcts[0]}%" if titles[0].blank?
     legend = options[:legend] || ''
     content_tag('table',
       content_tag('tr',
-        (pcts[0] > 0 ? content_tag('td', '', :style => "width: #{pcts[0]}%;", :class => 'closed') : ''.html_safe) +
-        (pcts[1] > 0 ? content_tag('td', '', :style => "width: #{pcts[1]}%;", :class => 'done') : ''.html_safe) +
-        (pcts[2] > 0 ? content_tag('td', '', :style => "width: #{pcts[2]}%;", :class => 'todo') : ''.html_safe)
+        (pcts[0] > 0 ? content_tag('td', '', :style => "width: #{pcts[0]}%;", :class => 'closed', :title => titles[0]) : ''.html_safe) +
+        (pcts[1] > 0 ? content_tag('td', '', :style => "width: #{pcts[1]}%;", :class => 'done', :title => titles[1]) : ''.html_safe) +
+        (pcts[2] > 0 ? content_tag('td', '', :style => "width: #{pcts[2]}%;", :class => 'todo', :title => titles[2]) : ''.html_safe)
       ), :class => "progress progress-#{pcts[0]}").html_safe +
       content_tag('p', legend, :class => 'percent').html_safe
   end
