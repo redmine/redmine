39b47a1a80762f5906416ef9599f8bc9e95dabeb
Jean-Philippe Lang <jp_lang@yahoo.fr>
2016-01-30 19:07:58 +0000

    Test broken by r15136 (#21593).
    git-svn-id: http://svn.redmine.org/redmine/trunk@15138 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -1242,14 +1242,14 @@ RAW
     result = textilizable(raw, :edit_section_links => {:controller => 'wiki', :action => 'edit', :project_id => '1', :id => 'Test'}).gsub("\n", "")
 
     # heading that contains inline code
-    assert_match Regexp.new('<div class="contextual" title="Edit this section" id="section-4">' +
+    assert_match Regexp.new('<div class="contextual heading-2" title="Edit this section" id="section-4">' +
       '<a class="icon-only icon-edit" href="/projects/1/wiki/Test/edit\?section=4"></a></div>' +
       '<a name="Subtitle-with-inline-code"></a>' +
       '<h2 >Subtitle with <code>inline code</code><a href="#Subtitle-with-inline-code" class="wiki-anchor">&para;</a></h2>'),
       result
 
     # last heading
-    assert_match Regexp.new('<div class="contextual" title="Edit this section" id="section-5">' +
+    assert_match Regexp.new('<div class="contextual heading-2" title="Edit this section" id="section-5">' +
       '<a class="icon-only icon-edit" href="/projects/1/wiki/Test/edit\?section=5"></a></div>' +
       '<a name="Subtitle-after-pre-tag"></a>' +
       '<h2 >Subtitle after pre tag<a href="#Subtitle-after-pre-tag" class="wiki-anchor">&para;</a></h2>'),
