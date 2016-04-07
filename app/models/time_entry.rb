95cc53a3ed1dcb68544716a2562b0b1819ad948c
Ilya Khalturinskiy <khalturinsky@gmail.com>
2016-04-08 00:20:13 +0700

    Минимум 10 символов для комментария к трудозатратам

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -39,7 +39,8 @@ class TimeEntry < ActiveRecord::Base
 
   validates_presence_of :user_id, :activity_id, :project_id, :hours, :spent_on, :comments
   validates_numericality_of :hours, :allow_nil => true, :message => :invalid
-  validates_length_of :comments, :maximum => 1024,:comments, :maximum => 10, :allow_nil => false
+  validates_length_of :comments, :maximum => 1024, :allow_nil => false
+  validates_length_of :comments, :minimum => 10, :allow_nil => false
   validates :spent_on, :date => true
   before_validation :set_project_if_nil
   validate :validate_time_entry
