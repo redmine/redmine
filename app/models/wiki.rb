redmine/redmine

commit 7b3f2b51c0ea735a4723fae24eff9bfbca254d8a

Add length validations for string fields (#24283).
Patch by Felix Schäfer.

git-svn-id: http://svn.redmine.org/redmine/trunk@15989 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/models/wiki.rb
@@ -25,6 +25,7 @@ class Wiki < ActiveRecord::Base
 
   validates_presence_of :start_page
   validates_format_of :start_page, :with => /\A[^,\.\/\?\;\|\:]*\z/
+  validates_length_of :title, maximum: 255
   attr_protected :id
 
   before_destroy :delete_redirects

app/models/custom_field.rb
@@ -32,6 +32,7 @@ class CustomField < ActiveRecord::Base
   validates_presence_of :name, :field_format
   validates_uniqueness_of :name, :scope => :type
   validates_length_of :name, :maximum => 30
+  validates_length_of :regexp, maximum: 30
   validates_inclusion_of :field_format, :in => Proc.new { Redmine::FieldFormat.available_formats }
   validate :validate_custom_field
   attr_protected :id

app/models/repository.rb
@@ -37,7 +37,9 @@ class Repository < ActiveRecord::Base
   # has_many :changesets, :dependent => :destroy is too slow for big repositories
   before_destroy :clear_changesets
 
+  validates_length_of :login, maximum: 60, allow_nil: true
   validates_length_of :password, :maximum => 255, :allow_nil => true
+  validates_length_of :root_url, :url, maximum: 255
   validates_length_of :identifier, :maximum => IDENTIFIER_MAX_LENGTH, :allow_blank => true
   validates_uniqueness_of :identifier, :scope => :project_id
   validates_exclusion_of :identifier, :in => %w(browse show entry raw changes annotate diff statistics graph revisions revision)

app/models/user.rb
@@ -111,6 +111,7 @@ class User < Principal
   validates_format_of :login, :with => /\A[a-z0-9_\-@\.]*\z/i
   validates_length_of :login, :maximum => LOGIN_LENGTH_LIMIT
   validates_length_of :firstname, :lastname, :maximum => 30
+  validates_length_of :identity_url, maximum: 255
   validates_inclusion_of :mail_notification, :in => MAIL_NOTIFICATION_OPTIONS.collect(&:first), :allow_blank => true
   validate :validate_password_length
   validate do

app/models/version.rb
@@ -35,7 +35,7 @@ class Version < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => [:project_id]
   validates_length_of :name, :maximum => 60
-  validates_length_of :description, :maximum => 255
+  validates_length_of :description, :wiki_page_title, :maximum => 255
   validates :effective_date, :date => true
   validates_inclusion_of :status, :in => VERSION_STATUSES
   validates_inclusion_of :sharing, :in => VERSION_SHARINGS

app/models/wiki_page.rb
@@ -45,6 +45,7 @@ class WikiPage < ActiveRecord::Base
   validates_presence_of :title
   validates_format_of :title, :with => /\A[^,\.\/\?\;\|\s]*\z/
   validates_uniqueness_of :title, :scope => :wiki_id, :case_sensitive => false
+  validates_length_of :title, maximum: 255
   validates_associated :content
   attr_protected :id
 

commit 42e65685521d32c5cef1a2de426efb87a8d9f672

Wrong validation introduced in r15989 (#24283).
git-svn-id: http://svn.redmine.org/redmine/trunk@15991 e93f8b46-1217-0410-a6f0-8f06a7374b81

app/models/wiki.rb
@@ -25,7 +25,7 @@ class Wiki < ActiveRecord::Base
 
   validates_presence_of :start_page
   validates_format_of :start_page, :with => /\A[^,\.\/\?\;\|\:]*\z/
-  validates_length_of :title, maximum: 255
+  validates_length_of :start_page, maximum: 255
   attr_protected :id
 
   before_destroy :delete_redirects