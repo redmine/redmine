ff272ad308af2d154f880e7df31e685199ac3922
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2016-11-21 14:38:45 +0000

    Scripts refactoring, work in progress.

--- public/geppetto/scripts/osbGenericScript.js
+++ public/geppetto/scripts/osbGenericScript.js
@@ -1,11 +1,1 @@
-// GEPPETTO SCRIPT FOR VISUALISING ANY KIND OF MODEL IN OSB
-var mdPopupWidth = 350;
-var mdPopupHeight = 400;
-var elementMargin = 20;
-
-var realHeightScreen = heightScreen - marginTop - marginBottom;
-var realWidthScreen = widthScreen - marginRight - marginLeft - defaultWidgetWidth - elementMargin;
-
-showModelDescription((typeof($ENTER_ID) === 'undefined')?GEPPETTO.ModelFactory.geppettoModel.neuroml.$ENTER_ID:$ENTER_ID.getType());
-
-G.setCameraPosition(-60,-250,370);
+initOSBGeppetto("generic", $ENTER_ID);
--- public/geppetto/scripts/osbNetworkScript.js
+++ public/geppetto/scripts/osbNetworkScript.js
@@ -1,10 +1,1 @@
-// Check if there is one single cell and select it so that TreeVisualisers work from the beginning 
-var population = GEPPETTO.ModelFactory.getAllTypesOfType(GEPPETTO.ModelFactory.geppettoModel.neuroml.population);
-// If there are two cells -> SuperType and cell
-if (population.length == 2){
-	for (var i = 0; i<population.length; i++){
-		if (typeof population[i].getSize === "function" && population[i].getSize() == 1){
-			population[i].select();
-		}
-	}
-}
+initOSBGeppetto("network", $ENTER_ID);
