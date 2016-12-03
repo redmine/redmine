49fcec80b7eb42debb749b7eef27b315c137d19f
Jean-Philippe Lang <jp_lang@yahoo.fr>
2016-12-03 09:59:03 +0000

    Reverts r16051 and r16050 for now (#15361).
    git-svn-id: http://svn.redmine.org/redmine/trunk@16052 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -685,6 +685,10 @@ function beforeShowDatePicker(input, inst) {
           type: 'put',
           dataType: 'script',
           data: data,
+          success: function(data){
+            sortable.children(":even").removeClass("even").addClass("odd");
+            sortable.children(":odd").removeClass("odd").addClass("even");
+          },
           error: function(jqXHR, textStatus, errorThrown){
             alert(jqXHR.status);
             sortable.sortable("cancel");
