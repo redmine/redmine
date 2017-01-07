fcdb810ebfa9abb3a22fb6296ee5dd2bb34fe0f9
Jean-Philippe Lang <jp_lang@yahoo.fr>
2017-01-07 08:40:49 +0000

    Merged r16092.
    git-svn-id: http://svn.redmine.org/redmine/branches/3.2-stable@16146 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/thumbnail.rb
+++ lib/redmine/thumbnail.rb
@@ -23,13 +23,14 @@ module Redmine
     extend Redmine::Utils::Shell
 
     CONVERT_BIN = (Redmine::Configuration['imagemagick_convert_command'] || 'convert').freeze
+    ALLOWED_TYPES = %w(image/bmp image/gif image/jpeg image/png)
 
     # Generates a thumbnail for the source image to target
     def self.generate(source, target, size)
       return nil unless convert_available?
       unless File.exists?(target)
-        # Make sure we only invoke Imagemagick if this is actually an image
-        unless File.open(source) {|f| MimeMagic.by_magic(f).try(:image?)}
+        # Make sure we only invoke Imagemagick if the file type is allowed
+        unless File.open(source) {|f| ALLOWED_TYPES.include? MimeMagic.by_magic(f).try(:type) }
           return nil
         end
         directory = File.dirname(target)
