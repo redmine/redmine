b60596532e322718d465a2073dae0537321d0509
Jean-Philippe Lang <jp_lang@yahoo.fr>
2017-01-14 09:56:04 +0000

    Don't always load data that is for html response only.
    git-svn-id: http://svn.redmine.org/redmine/trunk@16182 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -102,13 +102,12 @@ class IssuesController < ApplicationController
       @changesets.reverse!
     end
 
-    @allowed_statuses = @issue.new_statuses_allowed_to(User.current)
-    @priorities = IssuePriority.active
-    @time_entry = TimeEntry.new(:issue => @issue, :project => @issue.project)
-    @relation = IssueRelation.new
-
     respond_to do |format|
       format.html {
+        @allowed_statuses = @issue.new_statuses_allowed_to(User.current)
+        @priorities = IssuePriority.active
+        @time_entry = TimeEntry.new(:issue => @issue, :project => @issue.project)
+        @relation = IssueRelation.new
         retrieve_previous_and_next_issue_ids
         render :template => 'issues/show'
       }
