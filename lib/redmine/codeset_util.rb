281bc5c454411e72f6ce934de7cbf1ca5192d35e
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2017-01-28 05:43:33 +0000

    unify duplicate codes (#24616)
    git-svn-id: http://svn.redmine.org/redmine/trunk@16272 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/codeset_util.rb
+++ lib/redmine/codeset_util.rb
@@ -25,11 +25,7 @@ module Redmine
         str = str.encode("UTF-8", :invalid => :replace,
               :undef => :replace, :replace => '?')
       else
-        str.force_encoding("UTF-8")
-        if ! str.valid_encoding?
-          str = str.encode("US-ASCII", :invalid => :replace,
-                :undef => :replace, :replace => '?').encode("UTF-8")
-        end
+        str = replace_invalid_utf8(str)
       end
       str
     end
