730f1c30711b5f9e016e8f5adb0522fefc3a7132
Jean-Philippe Lang <jp_lang@yahoo.fr>
2017-01-29 10:25:29 +0000

    Merged r16227 (#24869).
    git-svn-id: http://svn.redmine.org/redmine/branches/3.3-stable@16290 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/wiki_formatting/macros.rb
+++ lib/redmine/wiki_formatting/macros.rb
@@ -207,8 +207,8 @@ module Redmine
         page = Wiki.find_page(args.first.to_s, :project => @project)
         raise 'Page not found' if page.nil? || !User.current.allowed_to?(:view_wiki_pages, page.wiki.project)
         @included_wiki_pages ||= []
-        raise 'Circular inclusion detected' if @included_wiki_pages.include?(page.title)
-        @included_wiki_pages << page.title
+        raise 'Circular inclusion detected' if @included_wiki_pages.include?(page.id)
+        @included_wiki_pages << page.id
         out = textilizable(page.content, :text, :attachments => page.attachments, :headings => false)
         @included_wiki_pages.pop
         out
