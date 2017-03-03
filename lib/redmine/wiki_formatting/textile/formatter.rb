ddef0ad7c164c8eee4a9832fe8fbce1fb538745a
Jean-Philippe Lang <jp_lang@yahoo.fr>
2017-03-03 17:16:53 +0000

    Moves redcloth3.rb to lib/redmine/wiki_formatting/textile.
    git-svn-id: http://svn.redmine.org/redmine/trunk@16358 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/wiki_formatting/textile/formatter.rb
+++ lib/redmine/wiki_formatting/textile/formatter.rb
@@ -15,7 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-require 'redcloth3'
+require File.expand_path('../redcloth3', __FILE__)
 require 'digest/md5'
 
 module Redmine
