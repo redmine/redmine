a1bdec9301ed9df84c222fd4c16feb5c3d17363d
Matt Earnshaw <matt@earnshaw.org.uk>
2017-05-05 15:33:17 +0100

    various markup / style fixes

--- lib/redmine.rb
+++ lib/redmine.rb
@@ -234,26 +234,26 @@ Redmine::MenuManager.map :admin_menu do |menu|
 end
 
 Redmine::MenuManager.map :project_menu do |menu|
-  menu.push :new_object, nil, :caption => ' + ',
-              :if => Proc.new { |p| Setting.new_item_menu_tab == '2' },
-              :html => { :id => 'new-object', :onclick => 'toggleNewObjectDropdown(); return false;' }
-  menu.push :new_issue_sub, { :controller => 'issues', :action => 'new', :copy_from => nil }, :param => :project_id, :caption => :label_issue_new,
-              :html => { :accesskey => Redmine::AccessKeys.key_for(:new_issue) },
-              :if => Proc.new { |p| Issue.allowed_target_trackers(p).any? },
-              :permission => :add_issues,
-              :parent => :new_object
-  menu.push :new_issue_category, {:controller => 'issue_categories', :action => 'new'}, :param => :project_id, :caption => :label_issue_category_new,
-              :parent => :new_object
-  menu.push :new_version, {:controller => 'versions', :action => 'new'}, :param => :project_id, :caption => :label_version_new,
-              :parent => :new_object
-  menu.push :new_news, {:controller => 'news', :action => 'new'}, :param => :project_id, :caption => :label_news_new,
-              :parent => :new_object
-  menu.push :new_document, {:controller => 'documents', :action => 'new'}, :param => :project_id, :caption => :label_document_new,
-              :parent => :new_object
-  menu.push :new_wiki_page, {:controller => 'wiki', :action => 'new'}, :param => :project_id, :caption => :label_wiki_page_new,
-              :parent => :new_object
-  menu.push :new_file, {:controller => 'files', :action => 'new'}, :param => :project_id, :caption => :label_attachment_new,
-              :parent => :new_object
+  #menu.push :new_object, nil, :caption => ' + ',
+              #:if => Proc.new { |p| Setting.new_item_menu_tab == '2' },
+              #:html => { :id => 'new-object', :onclick => 'toggleNewObjectDropdown(); return false;' }
+  #menu.push :new_issue_sub, { :controller => 'issues', :action => 'new', :copy_from => nil }, :param => :project_id, :caption => :label_issue_new,
+              #:html => { :accesskey => Redmine::AccessKeys.key_for(:new_issue) },
+              #:if => Proc.new { |p| Issue.allowed_target_trackers(p).any? },
+              #:permission => :add_issues,
+              #:parent => :new_object
+  #menu.push :new_issue_category, {:controller => 'issue_categories', :action => 'new'}, :param => :project_id, :caption => :label_issue_category_new,
+              #:parent => :new_object
+  #menu.push :new_version, {:controller => 'versions', :action => 'new'}, :param => :project_id, :caption => :label_version_new,
+              #:parent => :new_object
+  #menu.push :new_news, {:controller => 'news', :action => 'new'}, :param => :project_id, :caption => :label_news_new,
+              #:parent => :new_object
+  #menu.push :new_document, {:controller => 'documents', :action => 'new'}, :param => :project_id, :caption => :label_document_new,
+              #:parent => :new_object
+  #menu.push :new_wiki_page, {:controller => 'wiki', :action => 'new'}, :param => :project_id, :caption => :label_wiki_page_new,
+              #:parent => :new_object
+  #menu.push :new_file, {:controller => 'files', :action => 'new'}, :param => :project_id, :caption => :label_attachment_new,
+              #:parent => :new_object
   menu.push :overview, { :controller => 'projects', :action => 'show' }
   menu.push :activity, { :controller => 'activities', :action => 'index' }
   menu.push :models, { :controller => 'projects', :action => 'models' }
