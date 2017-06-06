c02c9ac1f7f7a22845017c5a35444f4b36d63cdb
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2017-06-06 02:12:24 +0000

    remove trailing white space from test/unit/project_nested_set_test.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@16610 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/project_nested_set_test.rb
+++ test/unit/project_nested_set_test.rb
@@ -166,7 +166,7 @@ class ProjectNestedSetTest < ActiveSupport::TestCase
         assert project.rgt < project.parent.rgt, "rgt=#{project.rgt} was not < parent.rgt=#{project.parent.rgt} for project #{project.name}"
       end
       # no overlapping lft/rgt values
-      overlapping = projects.detect {|other| 
+      overlapping = projects.detect {|other|
         other != project && (
           (other.lft > project.lft && other.lft < project.rgt && other.rgt > project.rgt) ||
           (other.rgt > project.lft && other.rgt < project.rgt && other.lft < project.lft)
