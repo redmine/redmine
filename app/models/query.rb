edbc9611de355e7933793b1eb155474d88d46fbd
Jean-Philippe Lang <jp_lang@yahoo.fr>
2017-07-24 17:14:53 +0000

    Fixed that sort_criteria is required (#26513).
    git-svn-id: http://svn.redmine.org/redmine/trunk@16877 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/query.rb
+++ app/models/query.rb
@@ -15,6 +15,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+require 'redmine/sort_criteria'
+
 class QueryColumn
   attr_accessor :name, :sortable, :groupable, :totalable, :default_order
   include Redmine::I18n
