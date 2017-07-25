9555732eb3af681379202e94269c254979fc04ae
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2017-07-25 13:42:07 +0000

    add "config.enable_dependency_loading = true" at config/environments/production.rb (#26513, #26524, #26526)
    git-svn-id: http://svn.redmine.org/redmine/trunk@16881 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,8 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  config.enable_dependency_loading = true
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
