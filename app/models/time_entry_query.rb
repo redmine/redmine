f0c83c92df378d675f56de6f88e6d2b1400cdad6
Jean-Philippe Lang <jp_lang@yahoo.fr>
2017-10-15 11:24:42 +0000

    Merged r16990 (#26780).
    git-svn-id: http://svn.redmine.org/redmine/branches/3.3-stable@16992 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/time_entry_query.rb
+++ app/models/time_entry_query.rb
@@ -22,7 +22,7 @@ class TimeEntryQuery < Query
   self.available_columns = [
     QueryColumn.new(:project, :sortable => "#{Project.table_name}.name", :groupable => true),
     QueryColumn.new(:spent_on, :sortable => ["#{TimeEntry.table_name}.spent_on", "#{TimeEntry.table_name}.created_on"], :default_order => 'desc', :groupable => true),
-    QueryColumn.new(:tweek, :sortable => ["#{TimeEntry.table_name}.spent_on", "#{TimeEntry.table_name}.created_on"], :caption => l(:label_week)),
+    QueryColumn.new(:tweek, :sortable => ["#{TimeEntry.table_name}.spent_on", "#{TimeEntry.table_name}.created_on"], :caption => :label_week),
     QueryColumn.new(:user, :sortable => lambda {User.fields_for_order_statement}, :groupable => true),
     QueryColumn.new(:activity, :sortable => "#{TimeEntryActivity.table_name}.position", :groupable => true),
     QueryColumn.new(:issue, :sortable => "#{Issue.table_name}.id"),
