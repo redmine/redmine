0d8baa01227983511ea57694f761a3bac301d3a9
Jean-Philippe Lang <jp_lang@yahoo.fr>
2017-10-15 11:23:35 +0000

    Translation for label_week in time report is not working (#26780).
    Patch by Alexander Meindl.
    
    git-svn-id: http://svn.redmine.org/redmine/trunk@16990 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/time_entry_query.rb
+++ app/models/time_entry_query.rb
@@ -24,7 +24,7 @@ class TimeEntryQuery < Query
     QueryColumn.new(:project, :sortable => "#{Project.table_name}.name", :groupable => true),
     QueryColumn.new(:spent_on, :sortable => ["#{TimeEntry.table_name}.spent_on", "#{TimeEntry.table_name}.created_on"], :default_order => 'desc', :groupable => true),
     QueryColumn.new(:created_on, :sortable => "#{TimeEntry.table_name}.created_on", :default_order => 'desc'),
-    QueryColumn.new(:tweek, :sortable => ["#{TimeEntry.table_name}.spent_on", "#{TimeEntry.table_name}.created_on"], :caption => l(:label_week)),
+    QueryColumn.new(:tweek, :sortable => ["#{TimeEntry.table_name}.spent_on", "#{TimeEntry.table_name}.created_on"], :caption => :label_week),
     QueryColumn.new(:user, :sortable => lambda {User.fields_for_order_statement}, :groupable => true),
     QueryColumn.new(:activity, :sortable => "#{TimeEntryActivity.table_name}.position", :groupable => true),
     QueryColumn.new(:issue, :sortable => "#{Issue.table_name}.id"),
