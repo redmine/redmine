d0ec0508ad4f91d66abb03ab301c5e72ae3f9247
Go MAEDA <maeda@farend.jp>
2017-12-13 14:07:15 +0000

    Merged r17113 to 3.4-stable (#22532).
    git-svn-id: http://svn.redmine.org/redmine/branches/3.4-stable@17114 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/account_controller_test.rb
+++ test/functional/account_controller_test.rb
@@ -220,6 +220,15 @@ class AccountControllerTest < Redmine::ControllerTest
     assert_response 302
   end
 
+  def test_login_should_strip_whitespaces_from_user_name
+    post :login, :params => {
+        :username => ' jsmith ',
+        :password => 'jsmith'
+      }
+    assert_response 302
+    assert_equal 2, @request.session[:user_id]
+  end
+
   def test_get_logout_should_not_logout
     @request.session[:user_id] = 2
     get :logout
