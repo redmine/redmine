cffc5d70264e0fb2813b63798dfbc523d0631ff2
Jean-Philippe Lang <jp_lang@yahoo.fr>
2018-01-08 17:52:19 +0000

    Merged r17158 to 3.4-stable (#26705).
    git-svn-id: http://svn.redmine.org/redmine/branches/3.4-stable@17159 e93f8b46-1217-0410-a6f0-8f06a7374b81

---
+++ test/functional/attachments_visibility_test.rb
@@ -0,0 +1,58 @@
+# encoding: utf-8
+#
+# Redmine - project management software
+# Copyright (C) 2006-2017  Jean-Philippe Lang
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+require File.expand_path('../../test_helper', __FILE__)
+
+class AttachmentsVisibilityTest < Redmine::ControllerTest
+  tests AttachmentsController
+  fixtures :users, :email_addresses, :projects, :roles, :members, :member_roles,
+           :enabled_modules, :projects_trackers, :issue_statuses, :enumerations,
+           :issues, :trackers, :versions
+
+  def setup
+    set_tmp_attachments_directory
+
+    @field = IssueCustomField.generate!(:field_format => 'attachment', :visible => true)
+    @attachment = new_record(Attachment) do
+      issue = Issue.generate
+      issue.custom_field_values = {@field.id => {:file => mock_file}}
+      issue.save!
+    end
+  end
+
+  def test_attachment_should_be_visible
+    @request.session[:user_id] = 2 # manager
+    get :show, :params => {:id => @attachment.id}
+    assert_response :success
+
+    @field.update!(:visible => false, :role_ids => [1])
+    get :show, :params => {:id => @attachment.id}
+    assert_response :success
+  end
+
+  def test_attachment_should_be_visible_with_permission
+    @request.session[:user_id] = 3 # developer
+    get :show, :params => {:id => @attachment.id}
+    assert_response :success
+
+    @field.update!(:visible => false, :role_ids => [1])
+    get :show, :params => {:id => @attachment.id}
+    assert_response 403
+  end
+end
