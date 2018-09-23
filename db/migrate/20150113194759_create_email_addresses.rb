7d7e44ba9cb3c6a4b6a8549a8cf253b789fc85ae
Jean-Philippe Lang <jp_lang@yahoo.fr>
2018-09-23 14:43:09 +0000

    Failed to run “rake db:migrate" command against clean database on MySQL5.7 (#27283).
    Patch by Marius BALTEANU.
    
    git-svn-id: http://svn.redmine.org/redmine/trunk@17515 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- db/migrate/20150113194759_create_email_addresses.rb
+++ db/migrate/20150113194759_create_email_addresses.rb
@@ -5,8 +5,8 @@ class CreateEmailAddresses < ActiveRecord::Migration[4.2]
       t.column :address, :string, :null => false
       t.column :is_default, :boolean, :null => false, :default => false
       t.column :notify, :boolean, :null => false, :default => true
-      t.column :created_on, :timestamp, :null => false
-      t.column :updated_on, :timestamp, :null => false
+      t.column :created_on, :datetime, :null => false
+      t.column :updated_on, :datetime, :null => false
     end
   end
 end
