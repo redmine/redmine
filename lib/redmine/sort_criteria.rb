14d441a144aabc35cb75605755e924eefb6ed377
Go MAEDA <maeda@farend.jp>
2018-11-26 05:22:04 +0000

    Remove blank keys from sort criteria (#30009).
    Patch by Marius BALTEANU.
    
    
    git-svn-id: http://svn.redmine.org/redmine/trunk@17638 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/sort_criteria.rb
+++ lib/redmine/sort_criteria.rb
@@ -87,6 +87,7 @@ module Redmine
     private
 
     def normalize!
+      self.reject! {|s| s.first.blank? }
       self.collect! {|s| s = Array(s); [s.first, (s.last == false || s.last.to_s == 'desc') ? 'desc' : 'asc']}
       self.replace self.first(3)
     end
