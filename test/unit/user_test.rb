1cadb28a6e1de597b24b6e9d12ea5b1095b45b1e
Jean-Philippe Lang <jp_lang@yahoo.fr>
2018-11-29 19:24:30 +0000

    Current user should be nil to prevent ActiveJob::DeserializationError errors in tests.
    git-svn-id: http://svn.redmine.org/redmine/trunk@17665 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -34,6 +34,7 @@ class UserTest < ActiveSupport::TestCase
     @admin = User.find(1)
     @jsmith = User.find(2)
     @dlopper = User.find(3)
+    User.current = nil
   end
 
   def test_admin_scope_without_args_should_return_admin_users
