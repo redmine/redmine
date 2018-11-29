05e701b877c4033936ebb2f6db1a2e93fb8408fb
Go MAEDA <maeda@farend.jp>
2018-11-29 06:58:04 +0000

    Add missing fixtures for test_create_with_one_attachment (#30054).
    Patch by Marius BALTEANU.
    
    
    git-svn-id: http://svn.redmine.org/redmine/trunk@17649 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/documents_controller_test.rb
+++ test/functional/documents_controller_test.rb
@@ -20,7 +20,7 @@ require File.expand_path('../../test_helper', __FILE__)
 class DocumentsControllerTest < Redmine::ControllerTest
   fixtures :projects, :users, :email_addresses, :roles, :members, :member_roles,
            :enabled_modules, :documents, :enumerations,
-           :groups_users, :attachments
+           :groups_users, :attachments, :user_preferences
 
   def setup
     User.current = nil
