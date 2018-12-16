82685d9a28b45d28c3415c8684e7bfce25de4efe
Jean-Philippe Lang <jp_lang@yahoo.fr>
2018-12-16 16:43:17 +0000

    Test failure.
    git-svn-id: http://svn.redmine.org/redmine/trunk@17758 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/time_entry_test.rb
+++ test/unit/time_entry_test.rb
@@ -29,6 +29,10 @@ class TimeEntryTest < ActiveSupport::TestCase
            :groups_users,
            :enabled_modules
 
+  def setup
+    User.current = nil
+  end
+
   def test_visibility_with_permission_to_view_all_time_entries
     user = User.generate!
     role = Role.generate!(:permissions => [:view_time_entries], :time_entries_visibility => 'all')
