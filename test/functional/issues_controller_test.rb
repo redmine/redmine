0a01dc7366d2a1cff8d557516e7b2780d0d4ba7e
Go MAEDA <maeda@farend.jp>
2019-03-31 11:07:56 +0000

    Merged r18028 from trunk to 4.0-stable (#31113).
    git-svn-id: http://svn.redmine.org/redmine/branches/4.0-stable@18029 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/issues_controller_test.rb
+++ test/functional/issues_controller_test.rb
@@ -591,7 +591,7 @@ class IssuesControllerTest < Redmine::ControllerTest
       }
     assert_response :success
 
-    assert_equal 'text/csv; header=present', @response.content_type
+    assert_equal 'text/csv', @response.content_type
     assert response.body.starts_with?("#,")
     lines = response.body.chomp.split("\n")
     # default columns + id and project
@@ -605,7 +605,7 @@ class IssuesControllerTest < Redmine::ControllerTest
       }
     assert_response :success
 
-    assert_equal 'text/csv; header=present', @response.content_type
+    assert_equal 'text/csv', @response.content_type
   end
 
   def test_index_csv_without_any_filters
@@ -633,7 +633,7 @@ class IssuesControllerTest < Redmine::ControllerTest
       assert_response :success
     end
 
-    assert_equal 'text/csv; header=present', response.content_type
+    assert_equal 'text/csv', response.content_type
     headers = response.body.chomp.split("\n").first.split(',')
     assert_include 'Description', headers
     assert_include 'test_index_csv_with_description', response.body
@@ -649,7 +649,7 @@ class IssuesControllerTest < Redmine::ControllerTest
         :c => %w(subject spent_hours)
       }
     assert_response :success
-    assert_equal 'text/csv; header=present', @response.content_type
+    assert_equal 'text/csv', @response.content_type
     lines = @response.body.chomp.split("\n")
     assert_include "#{issue.id},#{issue.subject},7.33", lines
   end
@@ -661,7 +661,7 @@ class IssuesControllerTest < Redmine::ControllerTest
       }
     assert_response :success
 
-    assert_equal 'text/csv; header=present', @response.content_type
+    assert_equal 'text/csv', @response.content_type
     assert_match /\A#,/, response.body
     lines = response.body.chomp.split("\n")
     assert_equal IssueQuery.new.available_inline_columns.size, lines[0].split(',').size
@@ -733,7 +733,7 @@ class IssuesControllerTest < Redmine::ControllerTest
           :subject => str_utf8,
           :format => 'csv'
         }
-      assert_equal 'text/csv; header=present', @response.content_type
+      assert_equal 'text/csv', @response.content_type
       lines = @response.body.chomp.split("\n")
       header = lines[0]
       status = "\xaa\xac\xbaA".force_encoding('Big5')
@@ -755,7 +755,7 @@ class IssuesControllerTest < Redmine::ControllerTest
           :format => 'csv',
           :set_filter => 1
         }
-      assert_equal 'text/csv; header=present', @response.content_type
+      assert_equal 'text/csv', @response.content_type
       lines = @response.body.chomp.split("\n")
       header = lines[0]
       issue_line = lines.find {|l| l =~ /^#{issue.id},/}
@@ -779,7 +779,7 @@ class IssuesControllerTest < Redmine::ControllerTest
           :format => 'csv',
           :set_filter => 1
         }
-      assert_equal 'text/csv; header=present', @response.content_type
+      assert_equal 'text/csv', @response.content_type
       lines = @response.body.chomp.split("\n")
       assert_include "#{issue.id},1234.50,#{str1}", lines
     end
@@ -797,7 +797,7 @@ class IssuesControllerTest < Redmine::ControllerTest
           :format => 'csv',
           :set_filter => 1
         }
-      assert_equal 'text/csv; header=present', @response.content_type
+      assert_equal 'text/csv', @response.content_type
       lines = @response.body.chomp.split("\n")
       assert_include "#{issue.id};1234,50;#{str1}", lines
     end
@@ -1263,7 +1263,7 @@ class IssuesControllerTest < Redmine::ControllerTest
         :format => 'csv'
       }
     assert_response :success
-    assert_equal 'text/csv; header=present', response.content_type
+    assert_equal 'text/csv', response.content_type
     lines = response.body.chomp.split("\n")
     assert_include '1,"Related to #7, Related to #8, Blocks #11"', lines
     assert_include '2,Blocked by #12', lines
