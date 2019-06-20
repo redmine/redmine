aca28e0716122fda10288e756854a4892ce12639
Go MAEDA <maeda@farend.jp>
2019-06-20 14:21:20 +0000

    Code cleanup: RuboCop: Layout/TrailingWhitespace
    git-svn-id: http://svn.redmine.org/redmine/trunk@18300 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- db/migrate/20190620135549_change_roles_name_limit.rb
+++ db/migrate/20190620135549_change_roles_name_limit.rb
@@ -2,7 +2,7 @@ class ChangeRolesNameLimit < ActiveRecord::Migration[5.2]
   def self.up
     change_column :roles, :name, :string, :limit => 255, :default => ''
   end
-  
+
   def self.down
     change_column :roles, :name, :string, :limit => 30, :default => ''
   end
