5c9447fd631010982a7dd3320d93e668fcf4aa66
Go MAEDA <maeda@farend.jp>
2019-06-30 05:57:52 +0000

    Support "active" attribute in Enumerations REST API (#31559).
    Patch by Go MAEDA.
    
    
    git-svn-id: http://svn.redmine.org/redmine/trunk@18325 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/integration/api_test/enumerations_test.rb
+++ test/integration/api_test/enumerations_test.rb
@@ -27,9 +27,15 @@ class Redmine::ApiTest::EnumerationsTest < Redmine::ApiTest::Base
     assert_response :success
     assert_equal 'application/xml', response.content_type
     assert_select 'issue_priorities[type=array]' do
-      assert_select 'issue_priority' do
+      assert_select 'issue_priority:nth-of-type(3)' do
         assert_select 'id', :text => '6'
         assert_select 'name', :text => 'High'
+        assert_select 'active', :text => 'true'
+      end
+      assert_select 'issue_priority:nth-of-type(6)' do
+        assert_select 'id', :text => '15'
+        assert_select 'name', :text => 'Inactive Priority'
+        assert_select 'active', :text => 'false'
       end
     end
   end
