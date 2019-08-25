0553e8375ced0da79df1432d1679a029b290a094
Go MAEDA <maeda@farend.jp>
2019-08-25 05:04:48 +0000

    Code cleanup: RuboCop: Fix Lint/UnneededRequireStatement in app/models/wiki_page.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@18392 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/wiki_page.rb
+++ app/models/wiki_page.rb
@@ -18,7 +18,6 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 require 'diff'
-require 'enumerator'
 
 class WikiPage < ActiveRecord::Base
   include Redmine::SafeAttributes
