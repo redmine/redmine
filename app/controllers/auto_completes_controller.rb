3ba5e04f5ed4c0c88444dd076287b5256ffbfbf6
Go MAEDA <maeda@farend.jp>
2019-09-11 06:39:00 +0000

    Issue auto complete should return last 10 issues (#31994).
    Patch by Marius BALTEANU.
    
    
    git-svn-id: http://svn.redmine.org/redmine/trunk@18449 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/auto_completes_controller.rb
+++ app/controllers/auto_completes_controller.rb
@@ -25,20 +25,18 @@ class AutoCompletesController < ApplicationController
     q = (params[:q] || params[:term]).to_s.strip
     status = params[:status].to_s
     issue_id = params[:issue_id].to_s
+
+    scope = Issue.cross_project_scope(@project, params[:scope]).visible
+    scope = scope.open(status == 'o') if status.present?
+    scope = scope.where.not(:id => issue_id.to_i) if issue_id.present?
     if q.present?
-      scope = Issue.cross_project_scope(@project, params[:scope]).visible
-      if status.present?
-        scope = scope.open(status == 'o')
-      end
-      if issue_id.present?
-        scope = scope.where.not(:id => issue_id.to_i)
-      end
       if q.match(/\A#?(\d+)\z/)
         issues << scope.find_by_id($1.to_i)
       end
-
       issues += scope.like(q).order(:id => :desc).limit(10).to_a
       issues.compact!
+    else
+      issues += scope.order(:id => :desc).limit(10).to_a
     end
 
     render :json => format_issues_json(issues)
