bb5706e2f935728bba312c94773d59e357da6b6e
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2019-09-24 05:21:20 +0000

    code cleanup: rubocop: fix Layout/EmptyLineBetweenDefs in db/migrate/20170302015225_change_attachments_digest_limit_to_64.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@18515 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- db/migrate/20170302015225_change_attachments_digest_limit_to_64.rb
+++ db/migrate/20170302015225_change_attachments_digest_limit_to_64.rb
@@ -2,6 +2,7 @@ class ChangeAttachmentsDigestLimitTo64 < ActiveRecord::Migration[4.2]
   def up
     change_column :attachments, :digest, :string, limit: 64
   end
+
   def down
     change_column :attachments, :digest, :string, limit: 40
   end
