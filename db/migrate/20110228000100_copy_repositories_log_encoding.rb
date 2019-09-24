e83313240f38dc222923821be057312d5924a966
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2019-09-24 15:41:50 +0000

    code cleanup: rubocop: fix Rails/Presence in db/migrate/20110228000100_copy_repositories_log_encoding.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@18519 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- db/migrate/20110228000100_copy_repositories_log_encoding.rb
+++ db/migrate/20110228000100_copy_repositories_log_encoding.rb
@@ -1,7 +1,7 @@
 class CopyRepositoriesLogEncoding < ActiveRecord::Migration[4.2]
   def self.up
     encoding = Setting.commit_logs_encoding.to_s.strip
-    encoding = encoding.blank? ? 'UTF-8' : encoding
+    encoding = encoding.presence || 'UTF-8'
     # encoding is NULL by default
     Repository.where("type IN ('Bazaar', 'Cvs', 'Darcs')").
                  update_all(["log_encoding = ?", encoding])
