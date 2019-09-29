661d4b42b179c9b16cafcc0e95c025b6f874d28a
Go MAEDA <maeda@farend.jp>
2019-09-29 00:03:34 +0000

    Unify fields of subtasks and related issues on issue page (#25540).
    Patch by Takenori TAKAKI.
    
    
    git-svn-id: http://svn.redmine.org/redmine/trunk@18549 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -116,6 +116,8 @@ module IssuesHelper
              content_tag('td', check_box_tag("ids[]", child.id, false, :id => nil), :class => 'checkbox') +
              content_tag('td', link_to_issue(child, :project => (issue.project_id != child.project_id)), :class => 'subject', :style => 'width: 50%') +
              content_tag('td', h(child.status), :class => 'status') +
+             content_tag('td', format_date(child.start_date), :class => 'start_date') +
+             content_tag('td', format_date(child.due_date), :class => 'due_date') +
              content_tag('td', link_to_user(child.assigned_to), :class => 'assigned_to') +
              content_tag('td', child.disabled_core_fields.include?('done_ratio') ? '' : progress_bar(child.done_ratio), :class=> 'done_ratio') +
              content_tag('td', buttons, :class => 'buttons'),
@@ -154,6 +156,7 @@ module IssuesHelper
              content_tag('td', other_issue.status, :class => 'status') +
              content_tag('td', format_date(other_issue.start_date), :class => 'start_date') +
              content_tag('td', format_date(other_issue.due_date), :class => 'due_date') +
+             content_tag('td', link_to_user(other_issue.assigned_to), :class => 'assigned_to') +
              content_tag('td', other_issue.disabled_core_fields.include?('done_ratio') ? '' : progress_bar(other_issue.done_ratio), :class=> 'done_ratio') +
              content_tag('td', buttons, :class => 'buttons'),
              :id => "relation-#{relation.id}",
