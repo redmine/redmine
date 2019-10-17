04cf448648458bbbe9cfd7b4559522d3a3bb1595
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2019-10-17 11:22:30 +0000

    code cleanup: rubocop: fix Layout/ElseAlignment in lib/redmine/export/pdf/issues_pdf_helper.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@18705 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/export/pdf/issues_pdf_helper.rb
+++ lib/redmine/export/pdf/issues_pdf_helper.rb
@@ -369,27 +369,28 @@ module Redmine
         # fetch row values
         def fetch_row_values(issue, query, level)
           query.inline_columns.collect do |column|
-            s = if column.is_a?(QueryCustomFieldColumn)
-              cv = issue.visible_custom_field_values.detect {|v| v.custom_field_id == column.custom_field.id}
-              show_value(cv, false)
-            else
-              value = issue.send(column.name)
-              case column.name
-              when :subject
-                value = "  " * level + value
-              when :attachments
-                value = value.to_a.map {|a| a.filename}.join("\n")
-              end
-              if value.is_a?(Date)
-                format_date(value)
-              elsif value.is_a?(Time)
-                format_time(value)
-              elsif value.is_a?(Float)
-                sprintf "%.2f", value
+            s =
+              if column.is_a?(QueryCustomFieldColumn)
+                cv = issue.visible_custom_field_values.detect {|v| v.custom_field_id == column.custom_field.id}
+                show_value(cv, false)
               else
-                value
+                value = issue.send(column.name)
+                case column.name
+                when :subject
+                  value = "  " * level + value
+                when :attachments
+                  value = value.to_a.map {|a| a.filename}.join("\n")
+                end
+                if value.is_a?(Date)
+                  format_date(value)
+                elsif value.is_a?(Time)
+                  format_time(value)
+                elsif value.is_a?(Float)
+                  sprintf "%.2f", value
+                else
+                  value
+                end
               end
-            end
             s.to_s
           end
         end
