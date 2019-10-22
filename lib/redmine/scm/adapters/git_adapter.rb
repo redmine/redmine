27dbbee75e9633ab1f597087eab2356c94ded2b0
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2019-10-22 11:30:54 +0000

    code cleanup: rubocop: fix Style/UnneededInterpolation in lib/redmine/scm/adapters/git_adapter.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@18796 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/scm/adapters/git_adapter.rb
+++ lib/redmine/scm/adapters/git_adapter.rb
@@ -199,7 +199,7 @@ module Redmine
           cmd_args = %w|log --no-color --encoding=UTF-8 --raw --date=iso --pretty=fuller --parents --stdin|
           cmd_args << '--no-renames' if self.class.client_version_above?([2, 9])
           cmd_args << "--reverse" if options[:reverse]
-          cmd_args << "-n" << "#{options[:limit].to_i}" if options[:limit]
+          cmd_args << "-n" << options[:limit].to_i.to_s if options[:limit]
           cmd_args << "--" << scm_iconv(@path_encoding, 'UTF-8', path) if path && !path.empty?
           revisions = []
           if identifier_from || identifier_to
@@ -210,7 +210,7 @@ module Redmine
             end
             if identifier_to
               git_identifier_to = scm_iconv(@path_encoding, 'UTF-8', identifier_to)
-              revisions[0] << "#{git_identifier_to}" if identifier_to
+              revisions[0] << git_identifier_to.to_s if identifier_to
             end
           else
             unless options[:includes].blank?
