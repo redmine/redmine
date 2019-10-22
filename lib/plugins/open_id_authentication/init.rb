b67cb67cce6d3f10d853475f35ba90c51125891a
Go MAEDA <maeda@farend.jp>
2019-10-22 01:34:21 +0000

    Remove code for unsupported versions of Rails from open_id_authentication (#32297).
    Patch by Go MAEDA.
    
    
    git-svn-id: http://svn.redmine.org/redmine/trunk@18788 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/plugins/open_id_authentication/init.rb
+++ lib/plugins/open_id_authentication/init.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: false
 
-if Rails.version < '3'
-  config.gem 'rack-openid', :lib => 'rack/openid', :version => '>=0.2.1'
-end
-
 require 'open_id_authentication'
 
 config.middleware.use OpenIdAuthentication
