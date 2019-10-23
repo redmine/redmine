d6a5f8ff1fd4925d0bdb4d60e72a0f9b5a8f3dfb
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2019-10-23 04:39:38 +0000

    fix comment typo in db/migrate/20150208105930_replace_move_issues_permission.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@18844 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- db/migrate/20150208105930_replace_move_issues_permission.rb
+++ db/migrate/20150208105930_replace_move_issues_permission.rb
@@ -2,7 +2,7 @@ class ReplaceMoveIssuesPermission < ActiveRecord::Migration[4.2]
   def self.up
     Role.all.each do |role|
       if role.has_permission?(:edit_issues) && !role.has_permission?(:move_issues)
-        # inserts one ligne per trakcer and status
+        # inserts one line per tracker and status
         rule = WorkflowPermission.connection.quote_column_name('rule') # rule is a reserved keyword in SQLServer
         WorkflowPermission.connection.execute(
           "INSERT INTO #{WorkflowPermission.table_name} (tracker_id, old_status_id, role_id, type, field_name, #{rule})" +
