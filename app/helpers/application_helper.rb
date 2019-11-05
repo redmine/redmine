b959b53dd2f0788e1134ba886b841dbedcb5443f
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2019-11-05 14:09:21 +0000

    code layout clean up ApplicationHelper#render_project_jump_box
    git-svn-id: http://svn.redmine.org/redmine/trunk@18908 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -510,10 +510,13 @@ module ApplicationHelper
     end
     text ||= l(:label_jump_to_a_project)
     url = autocomplete_projects_path(:format => 'js', :jump => current_menu_item)
-
     trigger = content_tag('span', text, :class => 'drdn-trigger')
-    q = text_field_tag('q', '', :id => 'projects-quick-search', :class => 'autocomplete', :data => {:automcomplete_url => url}, :autocomplete => 'off')
-    all = link_to(l(:label_project_all), projects_path(:jump => current_menu_item), :class => (@project.nil? && controller.class.main_menu ? 'selected' : nil))
+    q = text_field_tag('q', '', :id => 'projects-quick-search',
+                       :class => 'autocomplete',
+                       :data => {:automcomplete_url => url},
+                       :autocomplete => 'off')
+    all = link_to(l(:label_project_all), projects_path(:jump => current_menu_item),
+                  :class => (@project.nil? && controller.class.main_menu ? 'selected' : nil))
     content =
       content_tag('div',
                   content_tag('div', q, :class => 'quick-search') +
