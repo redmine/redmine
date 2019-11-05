d97537935bc4c50101cc23661ff9461d22d797ef
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2019-11-05 14:10:11 +0000

    cleanup: rubocop: fix Layout/IndentAssignment in test/unit/lib/redmine/helpers/gantt_test.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@18912 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/lib/redmine/helpers/gantt_test.rb
+++ test/unit/lib/redmine/helpers/gantt_test.rb
@@ -505,17 +505,17 @@ class Redmine::Helpers::GanttHelperTest < Redmine::HelperTest
     issue2 = Issue.generate!(:subject => "test", :project => project,
                              :start_date => (today - 2))
     issue1_child1 =
-             Issue.generate!(:parent_issue_id => issue1.id, :subject => 'child',
-                             :project => project)
+      Issue.generate!(:parent_issue_id => issue1.id, :subject => 'child',
+                      :project => project)
     issue1_child2 =
-             Issue.generate!(:parent_issue_id => issue1.id, :subject => 'child',
-                             :project => project, :start_date => (today - 10))
+      Issue.generate!(:parent_issue_id => issue1.id, :subject => 'child',
+                      :project => project, :start_date => (today - 10))
     issue1_child1_child1 =
-             Issue.generate!(:parent_issue_id => issue1_child1.id, :subject => 'child',
-                             :project => project, :start_date => (today - 8))
+      Issue.generate!(:parent_issue_id => issue1_child1.id, :subject => 'child',
+                      :project => project, :start_date => (today - 8))
     issue1_child1_child2 =
-             Issue.generate!(:parent_issue_id => issue1_child1.id, :subject => 'child',
-                             :project => project, :start_date => (today - 9))
+      Issue.generate!(:parent_issue_id => issue1_child1.id, :subject => 'child',
+                      :project => project, :start_date => (today - 9))
     issue1_child1_child1_logic = Redmine::Helpers::Gantt.sort_issue_logic(issue1_child1_child1)
     assert_equal [[today - 10, issue1.id], [today - 9, issue1_child1.id],
                   [today - 8, issue1_child1_child1.id]],
