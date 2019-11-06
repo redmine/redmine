4ec1376161c519d31355304093b1c0707032a5a9
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2019-11-06 12:03:36 +0000

    cleanup: rubocop: fix Layout/AlignArguments in app/helpers/context_menus_helper.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@18924 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/context_menus_helper.rb
+++ app/helpers/context_menus_helper.rb
@@ -35,16 +35,24 @@ module ContextMenusHelper
   end
 
   def bulk_update_custom_field_context_menu_link(field, text, value)
-    context_menu_link h(text),
-      bulk_update_issues_path(:ids => @issue_ids, :issue => {'custom_field_values' => {field.id => value}}, :back_url => @back),
+    context_menu_link(
+      h(text),
+      bulk_update_issues_path(:ids => @issue_ids,
+                              :issue => {'custom_field_values' => {field.id => value}},
+                              :back_url => @back),
       :method => :post,
       :selected => (@issue && @issue.custom_field_value(field) == value)
+    )
   end
 
   def bulk_update_time_entry_custom_field_context_menu_link(field, text, value)
-    context_menu_link h(text),
-      bulk_update_time_entries_path(:ids => @time_entries.map(&:id).sort, :time_entry => {'custom_field_values' => {field.id => value}}, :back_url => @back),
+    context_menu_link(
+      h(text),
+      bulk_update_time_entries_path(:ids => @time_entries.map(&:id).sort,
+                                    :time_entry => {'custom_field_values' => {field.id => value}},
+                                    :back_url => @back),
       :method => :post,
       :selected => (@time_entry && @time_entry.custom_field_value(field) == value)
+    )
   end
 end
