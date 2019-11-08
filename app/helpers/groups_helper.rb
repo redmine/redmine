f1ad9de8f38997042f9898ad0d2f4c8bddfc92c8
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2019-11-08 00:46:35 +0000

    cleanup: rubocop: fix Layout/AlignArguments in app/helpers/groups_helper.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@18938 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -31,16 +31,14 @@ module GroupsHelper
     principal_count = scope.count
     principal_pages = Redmine::Pagination::Paginator.new principal_count, limit, params['page']
     principals = scope.offset(principal_pages.offset).limit(principal_pages.per_page).to_a
-
-    s = content_tag('div',
+    s = content_tag(
+      'div',
       content_tag('div', principals_check_box_tags('user_ids[]', principals), :id => 'principals'),
       :class => 'objects-selection'
     )
-
     links = pagination_links_full(principal_pages, principal_count, :per_page_links => false) {|text, parameters, options|
       link_to text, autocomplete_for_user_group_path(group, parameters.merge(:q => params[:q], :format => 'js')), :remote => true
     }
-
     s + content_tag('span', links, :class => 'pagination')
   end
 end
