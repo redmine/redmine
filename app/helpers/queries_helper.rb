362e553d5eb611cf1b966e8ea1b6d480500c6f37
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2019-11-08 14:53:34 +0000

    code layout clean up app/helpers/queries_helper.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@18963 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/queries_helper.rb
+++ app/helpers/queries_helper.rb
@@ -396,26 +396,39 @@ module QueriesHelper
   def query_links(title, queries)
     return '' if queries.empty?
     # links to #index on issues/show
-    url_params = controller_name == 'issues' ? {:controller => 'issues', :action => 'index', :project_id => @project} : {}
-
+    url_params =
+      if controller_name == 'issues'
+        {:controller => 'issues', :action => 'index', :project_id => @project}
+      else
+        {}
+      end
     content_tag('h3', title) + "\n" +
       content_tag(
         'ul',
         queries.collect {|query|
-            css = +'query'
-            clear_link = +''
-            if query == @query
-              css << ' selected'
-              clear_link += link_to_clear_query
-            end
-            content_tag('li', link_to(query.name, url_params.merge(:query_id => query), :class => css) + clear_link.html_safe)
+          css = +'query'
+          clear_link = +''
+          if query == @query
+            css << ' selected'
+            clear_link += link_to_clear_query
+          end
+          content_tag('li',
+                      link_to(query.name,
+                              url_params.merge(:query_id => query),
+                              :class => css) +
+                        clear_link.html_safe)
         }.join("\n").html_safe,
         :class => 'queries'
       ) + "\n"
   end
 
   def link_to_clear_query
-    link_to l(:button_clear), { :set_filter => 1, :sort => '', :project_id => @project }, :class => 'icon-only icon-clear-query', :title => l(:button_clear)
+    link_to(
+      l(:button_clear),
+      {:set_filter => 1, :sort => '', :project_id => @project},
+      :class => 'icon-only icon-clear-query',
+      :title => l(:button_clear)
+    )
   end
 
   # Renders the list of queries for the sidebar
