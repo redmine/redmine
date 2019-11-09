4c8b2ea40b022cfd1dfd5ec35a4ff6cf74b713f2
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2019-11-09 16:33:33 +0000

    cleanup: rubocop: fix Layout/AlignArguments in app/helpers/email_addresses_helper.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@19034 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/email_addresses_helper.rb
+++ app/helpers/email_addresses_helper.rb
@@ -22,17 +22,19 @@ module EmailAddressesHelper
   # Returns a link to enable or disable notifications for the address
   def toggle_email_address_notify_link(address)
     if address.notify?
-      link_to l(:label_disable_notifications),
+      link_to(
+        l(:label_disable_notifications),
         user_email_address_path(address.user, address, :notify => '0'),
         :method => :put, :remote => true,
         :title => l(:label_disable_notifications),
-        :class => 'icon-only icon-email'
+        :class => 'icon-only icon-email')
     else
-      link_to l(:label_enable_notifications),
+      link_to(
+        l(:label_enable_notifications),
         user_email_address_path(address.user, address, :notify => '1'),
         :method => :put, :remote => true,
         :title => l(:label_enable_notifications),
-        :class => 'icon-only icon-email-disabled'
+        :class => 'icon-only icon-email-disabled')
     end
   end
 end
