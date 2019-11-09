9a76c5868840553b2b54e705549d8b12b3024264
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2019-11-09 16:33:11 +0000

    cleanup: rubocop: fix Layout/AlignArguments in ApplicationHelperTest#test_attached_images_with_hires_naming
    git-svn-id: http://svn.redmine.org/redmine/trunk@19032 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -184,8 +184,9 @@ class ApplicationHelperTest < Redmine::HelperTest
 
   def test_attached_images_with_hires_naming
     attachment = Attachment.generate!(:filename => 'image@2x.png')
-    assert_equal %(<p><img src="/attachments/download/#{attachment.id}/image@2x.png" srcset="/attachments/download/#{attachment.id}/image@2x.png 2x" alt="" /></p>),
-        textilizable("!image@2x.png!", :attachments => [attachment])
+    assert_equal(
+        %(<p><img src="/attachments/download/#{attachment.id}/image@2x.png" srcset="/attachments/download/#{attachment.id}/image@2x.png 2x" alt="" /></p>),
+        textilizable("!image@2x.png!", :attachments => [attachment]))
   end
 
   def test_attached_images_filename_extension
