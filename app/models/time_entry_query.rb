b131063315b6403745ab272e21cdb045eed24379
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2019-11-20 16:29:41 +0000

    cleanup: rubocop: fix Layout/EmptyLinesAroundClassBody in app/models/time_entry_query.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@19103 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/time_entry_query.rb
+++ app/models/time_entry_query.rb
@@ -18,7 +18,6 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 class TimeEntryQuery < Query
-
   self.queried_class = TimeEntry
   self.view_permission = :view_time_entries
 
