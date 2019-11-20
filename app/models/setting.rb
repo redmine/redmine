ab37571a8e87e1202b063d12a0bc4ee1c2dc0237
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2019-11-20 16:29:52 +0000

    cleanup: rubocop: fix Layout/EmptyLinesAroundClassBody in app/models/setting.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@19104 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -18,7 +18,6 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 class Setting < ActiveRecord::Base
-
   PASSWORD_CHAR_CLASSES = {
         'uppercase'     => /[A-Z]/,
         'lowercase'     => /[a-z]/,
