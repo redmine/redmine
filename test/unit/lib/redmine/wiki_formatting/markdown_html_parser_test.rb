2584615d3718e8e7ac19bbc414f96155ff73f3c7
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2019-11-21 04:05:00 +0000

    cleanup: rubocop: fix Layout/EmptyLinesAroundClassBody in test/unit/lib/redmine/wiki_formatting/markdown_html_parser_test.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@19112 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/lib/redmine/wiki_formatting/markdown_html_parser_test.rb
+++ test/unit/lib/redmine/wiki_formatting/markdown_html_parser_test.rb
@@ -20,7 +20,6 @@
 require File.expand_path('../../../../../test_helper', __FILE__)
 
 class Redmine::WikiFormatting::MarkdownHtmlParserTest < ActiveSupport::TestCase
-
   def setup
     @parser = Redmine::WikiFormatting::Markdown::HtmlParser
   end
