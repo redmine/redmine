466a5bd3a832bbe0d46ee496d62f6a9f37783c3c
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2019-11-22 14:54:54 +0000

    cleanup: rubocop: fix Layout/EmptyLineAfterGuardClause in app/models/journal.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@19171 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -229,6 +229,7 @@ class Journal < ActiveRecord::Base
         before = @attributes_before_change[attribute]
         after = journalized.send(attribute)
         next if before == after || (before.blank? && after.blank?)
+
         add_attribute_detail(attribute, before, after)
       end
     end
