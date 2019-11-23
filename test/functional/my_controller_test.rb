aace24e5619779af926ab241ee6580dbfd2a1b72
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2019-11-23 09:03:22 +0000

    cleanup: rubocop: fix Layout/AlignArguments in test/functional/my_controller_test.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@19184 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/my_controller_test.rb
+++ test/functional/my_controller_test.rb
@@ -20,9 +20,11 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class MyControllerTest < Redmine::ControllerTest
-  fixtures :users, :email_addresses, :user_preferences, :roles, :projects, :members, :member_roles,
-  :issues, :issue_statuses, :trackers, :enumerations, :custom_fields, :auth_sources, :queries, :enabled_modules,
-  :journals
+  fixtures :users, :email_addresses, :user_preferences,
+           :roles, :projects, :members, :member_roles,
+           :issues, :issue_statuses, :trackers, :enumerations,
+           :custom_fields, :auth_sources, :queries, :enabled_modules,
+           :journals
 
   def setup
     @request.session[:user_id] = 2
