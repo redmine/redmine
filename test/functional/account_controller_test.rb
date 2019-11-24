91ea72e2b7ca0deae298146eadb65281753dfead
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2019-11-24 16:44:07 +0000

    cleanup: rubocop: fix Lint/ParenthesesAsGroupedExpression in test/functional/account_controller_test.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@19266 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/account_controller_test.rb
+++ test/functional/account_controller_test.rb
@@ -547,7 +547,7 @@ class AccountControllerTest < Redmine::ControllerTest
     user.reload
     assert user.check_password?('newpass123')
     assert_nil Token.find_by_id(token.id), "Token was not deleted"
-    assert_not_nil (mail = ActionMailer::Base.deliveries.last)
+    assert_not_nil ActionMailer::Base.deliveries.last
     assert_select_email do
       assert_select 'a[href^=?]', 'http://localhost:3000/my/password', :text => 'Change password'
     end
