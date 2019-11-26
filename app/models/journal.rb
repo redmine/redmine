1cca05ecf3aa0b068fd0ef1ec02dd92fb110214a
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2019-11-26 06:30:01 +0000

    cleanup: rubocop: fix Layout/ClosingParenthesisIndentation in app/models/journal.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@19313 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -310,13 +310,15 @@ class Journal < ActiveRecord::Base
   end
 
   def send_notification
-    if notify? && (Setting.notified_events.include?('issue_updated') ||
-        (Setting.notified_events.include?('issue_note_added') && notes.present?) ||
-        (Setting.notified_events.include?('issue_status_updated') && new_status.present?) ||
-        (Setting.notified_events.include?('issue_assigned_to_updated') && detail_for_attribute('assigned_to_id').present?) ||
-        (Setting.notified_events.include?('issue_priority_updated') && new_value_for('priority_id').present?) ||
-        (Setting.notified_events.include?('issue_fixed_version_updated') && detail_for_attribute('fixed_version_id').present?)
-      )
+    if notify? &&
+        (
+          Setting.notified_events.include?('issue_updated') ||
+          (Setting.notified_events.include?('issue_note_added') && notes.present?) ||
+          (Setting.notified_events.include?('issue_status_updated') && new_status.present?) ||
+          (Setting.notified_events.include?('issue_assigned_to_updated') && detail_for_attribute('assigned_to_id').present?) ||
+          (Setting.notified_events.include?('issue_priority_updated') && new_value_for('priority_id').present?) ||
+          (Setting.notified_events.include?('issue_fixed_version_updated') && detail_for_attribute('fixed_version_id').present?)
+        )
       Mailer.deliver_issue_edit(self)
     end
   end
