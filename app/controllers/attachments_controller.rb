c7b6ed09f0fd77c98029f0a68e596c092b6129c3
Go MAEDA <maeda@farend.jp>
2019-12-12 14:56:53 +0000

    Merged r19365 from trunk to 3.4-stable (#20277).
    git-svn-id: http://svn.redmine.org/redmine/branches/3.4-stable@19368 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -60,7 +60,7 @@ class AttachmentsController < ApplicationController
       @attachment.increment_download
     end
 
-    if stale?(:etag => @attachment.digest)
+    if stale?(:etag => @attachment.digest, :template => false)
       # images are sent inline
       send_file @attachment.diskfile, :filename => filename_for_content_disposition(@attachment.filename),
                                       :type => detect_content_type(@attachment),
@@ -70,7 +70,7 @@ class AttachmentsController < ApplicationController
 
   def thumbnail
     if @attachment.thumbnailable? && tbnail = @attachment.thumbnail(:size => params[:size])
-      if stale?(:etag => tbnail)
+      if stale?(:etag => tbnail, :template => false)
         send_file tbnail,
           :filename => filename_for_content_disposition(@attachment.filename),
           :type => detect_content_type(@attachment),
