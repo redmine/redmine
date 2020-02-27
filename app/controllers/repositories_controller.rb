f2524eb07542a2c30dcce494aeab491f80b4c00e
Go MAEDA <maeda@farend.jp>
2020-02-27 01:21:03 +0000

    Merged r19531 from trunk to 4.1-stable (#32449).
    git-svn-id: http://svn.redmine.org/redmine/branches/4.1-stable@19532 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -263,7 +263,7 @@ class RepositoriesController < ApplicationController
       @changeset = @repository.find_changeset_by_name(@rev)
       @changeset_to = @rev_to ? @repository.find_changeset_by_name(@rev_to) : nil
       @diff_format_revisions = @repository.diff_format_revisions(@changeset, @changeset_to)
-      render :diff, :formats => :html
+      render :diff, :formats => :html, :layout => 'base.html.erb'
     end
   end
 
