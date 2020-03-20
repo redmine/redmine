34977f91991258017fcf4f4968f909086e1019db
Go MAEDA <maeda@farend.jp>
2020-03-20 02:25:13 +0000

    Fix that unable to update the values of a custom field for time tracking activities when multiple values option is enabled (#33085).
    Patch by Thomas Löber.
    
    
    git-svn-id: http://svn.redmine.org/redmine/trunk@19588 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/enumerations_controller.rb
+++ app/controllers/enumerations_controller.rb
@@ -109,7 +109,7 @@ class EnumerationsController < ApplicationController
 
   def enumeration_params
     # can't require enumeration on #new action
-    cf_ids = @enumeration.available_custom_fields.map{|c| c.id.to_s}
+    cf_ids = @enumeration.available_custom_fields.map {|c| c.multiple? ? {c.id.to_s => []} : c.id.to_s}
     params.permit(:enumeration => [:name, :active, :is_default, :position, :custom_field_values => cf_ids])[:enumeration]
   end
 end
