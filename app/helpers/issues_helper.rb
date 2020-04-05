f47ed1a5e22b87b304463a9305bd504a49e254e0
Jean-Philippe Lang <jp_lang@yahoo.fr>
2020-04-05 09:51:11 +0000

    Move style to css (#32889).
    Patch by Marius BALTEANU.
    
    git-svn-id: http://svn.redmine.org/redmine/trunk@19658 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -124,7 +124,7 @@ module IssuesHelper
                          link_to_issue(
                            child,
                            :project => (issue.project_id != child.project_id)),
-                         :class => 'subject', :style => 'width: 50%') +
+                         :class => 'subject') +
              content_tag('td', h(child.status), :class => 'status') +
              content_tag('td', format_date(child.start_date), :class => 'start_date') +
              content_tag('td', format_date(child.due_date), :class => 'due_date') +
@@ -179,8 +179,7 @@ module IssuesHelper
                              other,
                              :project => Setting.cross_project_issue_relations?)
                          }.html_safe,
-                         :class => 'subject',
-                         :style => 'width: 50%') +
+                         :class => 'subject') +
              content_tag('td', other_issue.status, :class => 'status') +
              content_tag('td', format_date(other_issue.start_date), :class => 'start_date') +
              content_tag('td', format_date(other_issue.due_date), :class => 'due_date') +
