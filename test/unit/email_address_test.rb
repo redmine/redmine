3079cef213a3f9f0505817677e91cc4a15d054de
Jean-Philippe Lang <jp_lang@yahoo.fr>
2020-04-05 10:00:40 +0000

    Adds a test for r19660 (#32793).
    git-svn-id: http://svn.redmine.org/redmine/trunk@19661 e93f8b46-1217-0410-a6f0-8f06a7374b81

---
+++ test/unit/email_address_test.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# Redmine - project management software
+# Copyright (C) 2006-2020  Jean-Philippe Lang
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+require File.expand_path('../../test_helper', __FILE__)
+
+class EmailAddressTest < ActiveSupport::TestCase
+  fixtures :users
+
+  def setup
+    User.current = nil
+  end
+
+  def test_address_with_punycode_tld_should_be_valid
+    email = EmailAddress.new(address: 'jsmith@example.xn--80akhbyknj4f')
+    assert email.valid?
+  end
+end
