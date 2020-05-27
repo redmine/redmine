f65123df73dfd3bf0780f52c380631dcf671493b
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2020-05-27 01:56:17 +0000

    remove extra empty line from GanttHelper definition
    git-svn-id: http://svn.redmine.org/redmine/trunk@19787 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/gantt_helper.rb
+++ app/helpers/gantt_helper.rb
@@ -18,7 +18,6 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 module GanttHelper
-
   def gantt_zoom_link(gantt, in_or_out)
     case in_or_out
     when :in
