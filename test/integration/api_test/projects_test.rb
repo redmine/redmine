4d3086ef1f6b65b9146e21ca70cb685716e4f952
Go MAEDA <maeda@farend.jp>
2020-07-09 23:34:31 +0000

    Add missing fixture to Redmine::ApiTest::ProjectsTest (#33700).
    Patch by Go MAEDA.
    
    
    git-svn-id: http://svn.redmine.org/redmine/trunk@19877 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/integration/api_test/projects_test.rb
+++ test/integration/api_test/projects_test.rb
@@ -22,7 +22,7 @@ require File.expand_path('../../../test_helper', __FILE__)
 class Redmine::ApiTest::ProjectsTest < Redmine::ApiTest::Base
   fixtures :projects, :versions, :users, :roles, :members, :member_roles, :issues, :journals, :journal_details,
            :trackers, :projects_trackers, :issue_statuses, :enabled_modules, :enumerations, :boards, :messages,
-           :attachments, :custom_fields, :custom_values, :time_entries, :issue_categories
+           :attachments, :custom_fields, :custom_values, :custom_fields_projects, :time_entries, :issue_categories
 
   def setup
     super
