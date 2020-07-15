752fbb160c6fb2b3c896e3a6729004c59bee04f9
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2020-07-15 16:36:55 +0000

    remove spaces inside {} of ApplicationHelperTest#test_render_page_hierarchy_when_action_is_export
    git-svn-id: http://svn.redmine.org/redmine/trunk@19905 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -1552,7 +1552,7 @@ class ApplicationHelperTest < Redmine::HelperTest
   def test_render_page_hierarchy_when_action_is_export
     parent_page = WikiPage.find(1)
     child_page = WikiPage.find_by(parent_id: parent_page.id)
-    pages_by_parent_id = { nil => [parent_page], parent_page.id => [child_page] }
+    pages_by_parent_id = {nil => [parent_page], parent_page.id => [child_page]}
 
     # Change controller and action using stub
     controller.stubs(:controller_name).returns('wiki')
