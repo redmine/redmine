4ef5949574a79a04c0d17e095161908977ada5c8
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2020-07-15 16:36:39 +0000

    fix source indent of app/models/user.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@19903 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,50 +25,50 @@ class User < Principal
   # Different ways of displaying/sorting users
   USER_FORMATS = {
     :firstname_lastname => {
-        :string => '#{firstname} #{lastname}',
-        :order => %w(firstname lastname id),
-        :setting_order => 1
-      },
+      :string => '#{firstname} #{lastname}',
+      :order => %w(firstname lastname id),
+      :setting_order => 1
+    },
     :firstname_lastinitial => {
-        :string => '#{firstname} #{lastname.to_s.chars.first}.',
-        :order => %w(firstname lastname id),
-        :setting_order => 2
-      },
+      :string => '#{firstname} #{lastname.to_s.chars.first}.',
+      :order => %w(firstname lastname id),
+      :setting_order => 2
+    },
     :firstinitial_lastname => {
-        :string => '#{firstname.to_s.gsub(/(([[:alpha:]])[[:alpha:]]*\.?)/, \'\2.\')} #{lastname}',
-        :order => %w(firstname lastname id),
-        :setting_order => 2
-      },
+      :string => '#{firstname.to_s.gsub(/(([[:alpha:]])[[:alpha:]]*\.?)/, \'\2.\')} #{lastname}',
+      :order => %w(firstname lastname id),
+      :setting_order => 2
+    },
     :firstname => {
-        :string => '#{firstname}',
-        :order => %w(firstname id),
-        :setting_order => 3
-      },
+      :string => '#{firstname}',
+      :order => %w(firstname id),
+      :setting_order => 3
+    },
     :lastname_firstname => {
-        :string => '#{lastname} #{firstname}',
-        :order => %w(lastname firstname id),
-        :setting_order => 4
-      },
+      :string => '#{lastname} #{firstname}',
+      :order => %w(lastname firstname id),
+      :setting_order => 4
+    },
     :lastnamefirstname => {
-        :string => '#{lastname}#{firstname}',
-        :order => %w(lastname firstname id),
-        :setting_order => 5
-      },
+      :string => '#{lastname}#{firstname}',
+      :order => %w(lastname firstname id),
+      :setting_order => 5
+    },
     :lastname_comma_firstname => {
-        :string => '#{lastname}, #{firstname}',
-        :order => %w(lastname firstname id),
-        :setting_order => 6
-      },
+      :string => '#{lastname}, #{firstname}',
+      :order => %w(lastname firstname id),
+      :setting_order => 6
+    },
     :lastname => {
-        :string => '#{lastname}',
-        :order => %w(lastname id),
-        :setting_order => 7
-      },
+      :string => '#{lastname}',
+      :order => %w(lastname id),
+      :setting_order => 7
+    },
     :username => {
-        :string => '#{login}',
-        :order => %w(login id),
-        :setting_order => 8
-      },
+      :string => '#{login}',
+      :order => %w(login id),
+      :setting_order => 8
+    },
   }
 
   MAIL_NOTIFICATION_OPTIONS = [
