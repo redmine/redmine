94647052a8e750fdfa4ffed9cec6cf9e506a9ea7
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2020-10-27 15:19:13 +0000

    fix source indent of lib/redmine/scm/adapters/git_adapter.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@20214 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/scm/adapters/git_adapter.rb
+++ lib/redmine/scm/adapters/git_adapter.rb
@@ -190,23 +190,23 @@ module Redmine
           lines = []
           git_cmd(cmd_args) {|io| lines = io.readlines}
           begin
-              id = lines[0].split[1]
-              author = lines[1].match('Author:\s+(.*)$')[1]
-              time = Time.parse(lines[4].match('CommitDate:\s+(.*)$')[1])
-              Revision.
-                new(
-                  {
-                    :identifier => id,
-                    :scmid      => id,
-                    :author     => author,
-                    :time       => time,
-                    :message    => nil,
-                    :paths      => nil
-                  }
-                )
+            id = lines[0].split[1]
+            author = lines[1].match('Author:\s+(.*)$')[1]
+            time = Time.parse(lines[4].match('CommitDate:\s+(.*)$')[1])
+            Revision.
+              new(
+                {
+                  :identifier => id,
+                  :scmid      => id,
+                  :author     => author,
+                  :time       => time,
+                  :message    => nil,
+                  :paths      => nil
+                }
+              )
           rescue NoMethodError => e
-              logger.error("The revision '#{path}' has a wrong format")
-              return nil
+            logger.error("The revision '#{path}' has a wrong format")
+            return nil
           end
         rescue ScmCommandAborted
           nil
