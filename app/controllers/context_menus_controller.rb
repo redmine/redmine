be743ee1b9a8ab1f5c5045670510f541466fa5f9
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2020-10-27 15:18:17 +0000

    fix source indent of ContextMenusController
    git-svn-id: http://svn.redmine.org/redmine/trunk@20207 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/context_menus_controller.rb
+++ app/controllers/context_menus_controller.rb
@@ -31,12 +31,13 @@ class ContextMenusController < ApplicationController
 
     @allowed_statuses = @issues.map(&:new_statuses_allowed_to).reduce(:&)
 
-    @can = {:edit => @issues.all?(&:attributes_editable?),
-            :log_time => (@project && User.current.allowed_to?(:log_time, @project)),
-            :copy => User.current.allowed_to?(:copy_issues, @projects) && Issue.allowed_target_projects.any?,
-            :add_watchers => User.current.allowed_to?(:add_issue_watchers, @projects),
-            :delete => @issues.all?(&:deletable?)
-            }
+    @can = {
+      :edit => @issues.all?(&:attributes_editable?),
+      :log_time => (@project && User.current.allowed_to?(:log_time, @project)),
+      :copy => User.current.allowed_to?(:copy_issues, @projects) && Issue.allowed_target_projects.any?,
+      :add_watchers => User.current.allowed_to?(:add_issue_watchers, @projects),
+      :delete => @issues.all?(&:deletable?)
+    }
 
     @assignables = @issues.map(&:assignable_users).reduce(:&)
     @trackers = @projects.map {|p| Issue.allowed_target_trackers(p)}.reduce(:&)
