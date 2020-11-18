3b36e37fd7841c0973582b3e05c425e7bfd27003
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2020-11-18 16:15:43 +0000

    remove spaces inside {} of app/models/message.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@20419 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/message.rb
+++ app/models/message.rb
@@ -47,7 +47,7 @@ class Message < ActiveRecord::Base
            )
       end
   )
-  acts_as_activity_provider :scope => proc { preload({:board => :project}, :author) },
+  acts_as_activity_provider :scope => proc {preload({:board => :project}, :author)},
                             :author_key => :author_id
   acts_as_watchable
 
