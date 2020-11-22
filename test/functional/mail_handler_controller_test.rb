426777a358677235290fc347fde25cdb04e77c9d
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2020-11-22 12:59:34 +0000

    use with_settings at MailHandlerControllerTest#test_should_create_issue
    git-svn-id: http://svn.redmine.org/redmine/trunk@20477 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/mail_handler_controller_test.rb
+++ test/functional/mail_handler_controller_test.rb
@@ -32,17 +32,22 @@ class MailHandlerControllerTest < Redmine::ControllerTest
 
   def test_should_create_issue
     # Enable API and set a key
-    Setting.mail_handler_api_enabled = 1
-    Setting.mail_handler_api_key = 'secret'
-
-    assert_difference 'Issue.count' do
-      post(
-        :index,
-        :params => {
-          :key => 'secret',
-          :email => IO.read(File.join(FIXTURES_PATH, 'ticket_on_given_project.eml'))
-        }
-      )
+    with_settings(
+      :mail_handler_api_enabled => 1,
+      :mail_handler_api_key => 'secret'
+    ) do
+      assert_difference 'Issue.count' do
+        post(
+          :index,
+          :params => {
+            :key => 'secret',
+            :email =>
+               IO.read(
+                 File.join(FIXTURES_PATH, 'ticket_on_given_project.eml')
+               )
+          }
+        )
+      end
     end
     assert_response 201
   end
