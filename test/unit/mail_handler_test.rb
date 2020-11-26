faaf13aa6a651bf7a7bfecf20336df6469f79df8
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2020-11-26 14:46:05 +0000

    shorten long line of MailHandlerTest#test_email_with_long_subject_line
    git-svn-id: http://svn.redmine.org/redmine/trunk@20496 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/mail_handler_test.rb
+++ test/unit/mail_handler_test.rb
@@ -1227,7 +1227,13 @@ class MailHandlerTest < ActiveSupport::TestCase
   def test_email_with_long_subject_line
     issue = submit_email('ticket_with_long_subject.eml')
     assert issue.is_a?(Issue)
-    assert_equal issue.subject, 'New ticket on a given project with a very long subject line which exceeds 255 chars and should not be ignored but chopped off. And if the subject line is still not long enough, we just add more text. And more text. Wow, this is really annoying. Especially, if you have nothing to say...'[0, 255]
+    str =
+      'New ticket on a given project with a very long subject line' \
+        ' which exceeds 255 chars and should not be ignored but chopped off.' \
+        ' And if the subject line is still not long enough, we just add more text.' \
+        ' And more text. Wow, this is really annoying.' \
+        ' Especially, if you have nothing to say...'
+    assert_equal issue.subject, str[0, 255]
   end
 
   def test_first_keyword_should_be_matched
