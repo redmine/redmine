98da3afcd29b328ad40293c02d961b5b795bd0ca
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2020-11-26 14:47:00 +0000

    add empty line after guard clause to app/models/group_builtin.rb
    git-svn-id: http://svn.redmine.org/redmine/trunk@20503 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/group_builtin.rb
+++ app/models/group_builtin.rb
@@ -39,15 +39,18 @@ class GroupBuiltin < Group
   class << self
     def load_instance
       return nil if self == GroupBuiltin
+
       instance = unscoped.order('id').first || create_instance
     end
 
     def create_instance
       raise 'The builtin group already exists.' if exists?
+
       instance = unscoped.new
       instance.lastname = name
       instance.save :validate => false
       raise 'Unable to create builtin group.' if instance.new_record?
+
       instance
     end
     private :create_instance
