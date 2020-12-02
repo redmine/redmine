2bd5f3ad94dbd7882e4b4e36ad8d4f51ab971b5a
Go MAEDA <maeda@farend.jp>
2020-12-02 03:32:35 +0000

    Fix random test failure due to r20492 (#34258).
    git-svn-id: http://svn.redmine.org/redmine/trunk@20534 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/tracker_test.rb
+++ test/unit/tracker_test.rb
@@ -20,7 +20,7 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class TrackerTest < ActiveSupport::TestCase
-  fixtures :trackers, :workflows, :issue_statuses, :roles, :issues, :projects, :projects_trackers, :enabled_modules
+  fixtures :trackers, :workflows, :issue_statuses, :roles, :issues, :custom_fields, :projects, :projects_trackers, :enabled_modules
 
   def setup
     User.current = nil
