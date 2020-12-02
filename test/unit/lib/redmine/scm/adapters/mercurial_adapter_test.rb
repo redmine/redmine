fd3b7dd4ca8291a67b20c7516be76eecec3c6191
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2020-12-02 13:31:15 +0000

    fix source indent of MercurialAdapterTest
    git-svn-id: http://svn.redmine.org/redmine/trunk@20538 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/lib/redmine/scm/adapters/mercurial_adapter_test.rb
+++ test/unit/lib/redmine/scm/adapters/mercurial_adapter_test.rb
@@ -453,21 +453,21 @@ class MercurialAdapterTest < ActiveSupport::TestCase
         assert_equal 1, nib1.size
         case branch
         when 'branch (1)[2]&,%.-3_4'
-            if @adapter.class.client_version_above?([1, 6])
-              assert_equal 3, nib0.size
-              assert_equal 'afc61e85bde74de930e5846c8451bd55b5bafc9c', nib0[0]
-              nib2 = @adapter.nodes_in_branch(branch, :limit => 2)
-              assert_equal 2, nib2.size
-              assert_equal '933ca60293d78f7c7979dd123cc0c02431683575', nib2[1]
-            end
+          if @adapter.class.client_version_above?([1, 6])
+            assert_equal 3, nib0.size
+            assert_equal 'afc61e85bde74de930e5846c8451bd55b5bafc9c', nib0[0]
+            nib2 = @adapter.nodes_in_branch(branch, :limit => 2)
+            assert_equal 2, nib2.size
+            assert_equal '933ca60293d78f7c7979dd123cc0c02431683575', nib2[1]
+          end
         when @branch_char_1
-            if @adapter.class.client_version_above?([1, 6])
-              assert_equal 2, nib0.size
-              assert_equal '08ff3227303ec0dfcc818efa8e9cc652fe81859f', nib0[1]
-              nib2 = @adapter.nodes_in_branch(branch, :limit => 1)
-              assert_equal 1, nib2.size
-              assert_equal '7bbf4c738e7145149d2e5eb1eed1d3a8ddd3b914', nib2[0]
-            end
+          if @adapter.class.client_version_above?([1, 6])
+            assert_equal 2, nib0.size
+            assert_equal '08ff3227303ec0dfcc818efa8e9cc652fe81859f', nib0[1]
+            nib2 = @adapter.nodes_in_branch(branch, :limit => 1)
+            assert_equal 1, nib2.size
+            assert_equal '7bbf4c738e7145149d2e5eb1eed1d3a8ddd3b914', nib2[0]
+          end
         end
       end
     end
