f50ef21465919b13e94a2e91ff0b149975e56774
Toshi MARUYAMA <marutosijp2@yahoo.co.jp>
2020-12-11 14:18:33 +0000

    use with_settings at IssueRelationsControllerTest#test_should_create_relations_with_visible_issues_only
    git-svn-id: http://svn.redmine.org/redmine/trunk@20630 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/issue_relations_controller_test.rb
+++ test/functional/issue_relations_controller_test.rb
@@ -177,21 +177,22 @@ class IssueRelationsControllerTest < Redmine::ControllerTest
   end
 
   def test_should_create_relations_with_visible_issues_only
-    Setting.cross_project_issue_relations = '1'
-    assert_nil Issue.visible(User.find(3)).find_by_id(4)
+    with_settings :cross_project_issue_relations => '1' do
+      assert_nil Issue.visible(User.find(3)).find_by_id(4)
 
-    assert_no_difference 'IssueRelation.count' do
-      post(
-        :create,
-        :params => {
-          :issue_id => 1,
-          :relation => {
-            :issue_to_id => '4',
-            :relation_type => 'relates',
-            :delay => ''
+      assert_no_difference 'IssueRelation.count' do
+        post(
+          :create,
+          :params => {
+            :issue_id => 1,
+            :relation => {
+              :issue_to_id => '4',
+              :relation_type => 'relates',
+              :delay => ''
+            }
           }
-        }
-      )
+        )
+      end
     end
   end
 
