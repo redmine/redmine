4d3dec2dc0156369b789c8c51557c2c7001854ed
Go MAEDA <maeda@farend.jp>
2021-01-17 02:23:14 +0000

    Web browser freezes when displaying a workflow page with a large number of issue statuses (#34247).
    Patch by Marius BALTEANU.
    
    
    git-svn-id: http://svn.redmine.org/redmine/trunk@20717 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1072,15 +1072,15 @@ function setupWikiTableSortableHeader() {
 }
 
 $(function () {
-    $('[title]').tooltip({
-        show: {
-          delay: 400
-        },
-        position: {
-          my: "center bottom-5",
-          at: "center top"
-        }
-    });
+  $("[title]:not(.no-tooltip)").tooltip({
+    show: {
+      delay: 400
+    },
+    position: {
+      my: "center bottom-5",
+      at: "center top"
+    }
+  });
 });
 
 function inlineAutoComplete(element) {
