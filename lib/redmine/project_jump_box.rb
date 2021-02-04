redmine/redmine

commit 77c25ded048beb34fbcef93cc2029b8a607e7329

Reverts r20735
git-svn-id: http://svn.redmine.org/redmine/trunk@20736 e93f8b46-1217-0410-a6f0-8f06a7374b81

lib/redmine/project_jump_box.rb
@@ -30,10 +30,10 @@ module Redmine
 
     def recently_used_projects
       project_ids = recently_used_project_ids
-      Project.where(id: project_ids).visible
-        index_by(&:id).
-        values_at(*project_ids). # sort according to stored order
-        compact
+      Project.where(id: project_ids).
+      index_by(&:id).
+      values_at(*project_ids). # sort according to stored order
+      compact
     end
 
     def bookmarked_projects

commit 252c28fa269e740402e6b3c4cc1a2230190fb64c

Fix indent broken by r20736.
git-svn-id: http://svn.redmine.org/redmine/trunk@20737 e93f8b46-1217-0410-a6f0-8f06a7374b81

lib/redmine/project_jump_box.rb
@@ -31,9 +31,9 @@ module Redmine
     def recently_used_projects
       project_ids = recently_used_project_ids
       Project.where(id: project_ids).
-      index_by(&:id).
-      values_at(*project_ids). # sort according to stored order
-      compact
+        index_by(&:id).
+        values_at(*project_ids). # sort according to stored order
+        compact
     end
 
     def bookmarked_projects