1fd8d0657f50d24c7b459770102fd2b230913b02
Go MAEDA <maeda@farend.jp>
2021-03-10 00:51:25 +0000

    Merged r20768 from trunk to 4.1-stable (#33459).
    git-svn-id: http://svn.redmine.org/redmine/branches/4.1-stable@20769 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/helpers/journals_helper_test.rb
+++ test/helpers/journals_helper_test.rb
@@ -48,4 +48,30 @@ class JournalsHelperTest < Redmine::HelperTest
     assert_kind_of Attachment, thumbnails.first
     assert_equal 'image.png', thumbnails.first.filename
   end
+
+  def test_journal_thumbnail_attachments_should_be_in_the_same_order_as_the_journal_details
+    skip unless convert_installed?
+    set_tmp_attachments_directory
+    issue = Issue.generate!
+
+    # Thumbnails should be displayed in the same order as Journal.detail, not in attachment id order.
+    attachment1 = Attachment.generate!(:file => mock_file_with_options(:original_filename => 'image1.png'), :author => User.find(1))
+    attachment2 = Attachment.generate!(:file => mock_file_with_options(:original_filename => 'image2.png'), :author => User.find(1))
+    journal = Journal.create!(:journalized => issue, :user_id => 1)
+    JournalDetail.create!(
+      :journal => journal, :property => 'attachment',
+      :prop_key => attachment2.id.to_s,
+      :value => 'image2.png'
+    )
+    JournalDetail.create!(
+      :journal => journal, :property => 'attachment',
+      :prop_key => attachment1.id.to_s,
+      :value => 'image1.png'
+    )
+    journal.reload
+    thumbnails = journal_thumbnail_attachments(journal)
+    assert_equal 2, thumbnails.count
+    assert_equal 2, journal.details.count
+    assert_equal journal.details.map(&:value), thumbnails.map(&:filename)
+  end
 end
