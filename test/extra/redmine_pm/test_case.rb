70adc1d5eda9f3fe7af078faee505ef994e2f259
Michael Glenn <mbg198618@gmail.com>
2021-06-07 19:51:38 -0500

    Update test_case.rb

--- test/extra/redmine_pm/test_case.rb
+++ test/extra/redmine_pm/test_case.rb
@@ -1,21 +1,21 @@
-# frozen_string_literal: false
+ frozen_string_literal: false
 
-# Redmine - project management software
-# Copyright (C) 2006-2021  Jean-Philippe Lang
+Redmine - project management software
+ Copyright (C) 2006-2021  Jean-Philippe Lang
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 require File.expand_path('../../../test_helper', __FILE__)
 
@@ -23,8 +23,8 @@ module RedminePmTest
   class TestCase < ActiveSupport::TestCase
     attr_reader :command, :response, :status, :username, :password
     
-    # Cannot use transactional fixtures here: database
-    # will be accessed from Redmine.pm with its own connection
+    Cannot use transactional fixtures here: database
+    will be accessed from Redmine.pm with its own connection
     self.use_transactional_tests = false
   
     def test_dummy
