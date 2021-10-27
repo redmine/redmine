243c11026525a76d6838fcf165e3864ada9a722b
Marius Balteanu <marius.balteanu@zitec.com>
2021-10-27 20:42:10 +0000

    Move @lib/diff.rb@ to @lib/redmine/string_array_diff@ and extract @Diffable@ class to its own file (#29914, #32938).
    git-svn-id: http://svn.redmine.org/redmine/trunk@21262 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/wiki_page.rb
+++ app/models/wiki_page.rb
@@ -17,7 +17,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-require 'diff'
+require 'redmine/string_array_diff/diff'
 
 class WikiPage < ActiveRecord::Base
   include Redmine::SafeAttributes
