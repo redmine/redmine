e82b0a77e348419bb4c4ed736ac126e22358eac0
Marius Balteanu <marius.balteanu@zitec.com>
2021-11-17 20:52:59 +0000

    Load core plugins using require (#29914, #32938).
    Patch by Takashi Kato.
    
    git-svn-id: http://svn.redmine.org/redmine/trunk@21286 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- config/initializers/00-core_plugins.rb
+++ config/initializers/00-core_plugins.rb
@@ -1,17 +1,12 @@
 # frozen_string_literal: true
 
 # Loads the core plugins located in lib/plugins
-Dir.glob(File.join(Rails.root, "lib/plugins/*")).sort.each do |directory|
-  if File.directory?(directory)
-    lib = File.join(directory, "lib")
-    if File.directory?(lib)
-      $:.unshift lib
-      ActiveSupport::Dependencies.autoload_paths += [lib]
-    end
-    initializer = File.join(directory, "init.rb")
-    if File.file?(initializer)
-      config = RedmineApp::Application.config
-      eval(File.read(initializer), binding, initializer)
-    end
+Dir.glob(Rails.root.join('lib/plugins/*')).sort.each do |directory|
+  next unless File.directory?(directory)
+
+  initializer = File.join(directory, 'init.rb')
+  if File.file?(initializer)
+    config = RedmineApp::Application.config
+    eval(File.read(initializer), binding, initializer)
   end
 end
