34cb871b72063943ef5ae32595a8f7845cd8c7bb
Marius Balteanu <marius.balteanu@zitec.com>
2021-11-17 21:08:23 +0000

    Remove invalid require (#29914, #32938).
    git-svn-id: http://svn.redmine.org/redmine/trunk@21288 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine.rb
+++ lib/redmine.rb
@@ -18,7 +18,6 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 require 'redmine/core_ext'
-require 'diff'
 
 begin
   require 'mini_magick' unless Object.const_defined?(:MiniMagick)
