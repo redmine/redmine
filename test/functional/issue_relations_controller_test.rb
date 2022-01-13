addac907e5b5af75c5be549aec1c9326fd7abe5a
Go MAEDA <maeda@farend.jp>
2022-01-13 01:20:43 +0000

    Explicitly set Setting.cross_project_issue_relations to prevent random test failure (#36361).
    git-svn-id: http://svn.redmine.org/redmine/trunk@21364 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/issue_relations_controller_test.rb
+++ test/functional/issue_relations_controller_test.rb
@@ -267,16 +267,18 @@ class IssueRelationsControllerTest < Redmine::ControllerTest
   end
 
   def test_bulk_create_should_show_errors
-    assert_difference 'IssueRelation.count', +3 do
-      post :create, :params => {
-        :issue_id => 1,
-        :relation => {
-          :issue_to_id => '1,2,3,4,5,7',
-          :relation_type => 'relates',
-          :delay => ''
-        }
-      },
-      :xhr => true
+    with_settings :cross_project_issue_relations => '0' do
+      assert_difference 'IssueRelation.count', +3 do
+        post :create, :params => {
+          :issue_id => 1,
+          :relation => {
+            :issue_to_id => '1,2,3,4,5,7',
+            :relation_type => 'relates',
+            :delay => ''
+          }
+        },
+        :xhr => true
+      end
     end
 
     assert_response :success
