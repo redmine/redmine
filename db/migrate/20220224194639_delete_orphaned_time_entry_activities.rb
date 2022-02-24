3627a29fa6ddc97b51dcec5517da5531fc56d368
Marius Balteanu <marius.balteanu@zitec.com>
2022-02-24 19:55:42 +0000

    Add migration to delete orphaned time entry activities (#36416).
    Patch by Holger Just.
    
    
    git-svn-id: http://svn.redmine.org/redmine/trunk@21438 e93f8b46-1217-0410-a6f0-8f06a7374b81

---
+++ db/migrate/20220224194639_delete_orphaned_time_entry_activities.rb
@@ -0,0 +1,9 @@
+class DeleteOrphanedTimeEntryActivities < ActiveRecord::Migration[6.1]
+  def self.up
+    TimeEntryActivity.left_outer_joins(:project).where(projects:{id: nil}).where.not(project_id: nil).delete_all
+  end
+
+  def self.down
+    # no-op
+  end
+end
