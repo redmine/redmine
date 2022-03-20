b6fc3b22b4b18eea84bb8ee7a18c774bd93d6cd8
Go MAEDA <maeda@farend.jp>
2022-03-20 08:23:53 +0000

    Merged r21461 from trunk to 4.2-stable (#36766).
    git-svn-id: http://svn.redmine.org/redmine/branches/4.2-stable@21476 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- db/migrate/20090503121505_populate_member_roles.rb
+++ db/migrate/20090503121505_populate_member_roles.rb
@@ -2,7 +2,7 @@ class PopulateMemberRoles < ActiveRecord::Migration[4.2]
   def self.up
     MemberRole.delete_all
     Member.all.each do |member|
-      MemberRole.create!(:member_id => member.id, :role_id => member.role_id)
+      MemberRole.insert!({:member_id => member.id, :role_id => member.role_id})
     end
   end
 
