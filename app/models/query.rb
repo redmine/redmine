44332d44c4e16c06eaf49ca6e19acf2c95a5c043
Marius Balteanu <marius.balteanu@zitec.com>
2022-06-28 06:48:06 +0000

    Merged r21684 and r21685 to 4.2-stable (#37349).
    git-svn-id: https://svn.redmine.org/redmine/branches/4.2-stable@21689 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/query.rb
+++ app/models/query.rb
@@ -1176,7 +1176,7 @@ class Query < ActiveRecord::Base
       "  AND CAST(CASE value WHEN '' THEN '0' ELSE value END AS decimal(30,0)) IN (" +
       "  SELECT customized_id FROM #{CustomValue.table_name}" +
       "  WHERE customized_type='#{target_class}' AND custom_field_id=#{chained_custom_field_id}" +
-      "  AND #{sql_for_field(field, operator, value, CustomValue.table_name, 'value')}))"
+      "  AND #{sql_for_field(field, operator, value, CustomValue.table_name, 'value', true)}))"
 
   end
 
