0afe0cf609b67f50e2758ec03f4dce0a322f91cb
Go MAEDA <maeda@farend.jp>
2022-07-17 00:25:16 +0000

    Add missing fixture to IssuesPdfHelperTest (#37451).
    git-svn-id: https://svn.redmine.org/redmine/trunk@21710 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/lib/redmine/export/pdf/issues_pdf_test.rb
+++ test/unit/lib/redmine/export/pdf/issues_pdf_test.rb
@@ -21,7 +21,7 @@ require File.expand_path('../../../../../../test_helper', __FILE__)
 
 class IssuesPdfHelperTest < ActiveSupport::TestCase
   fixtures :users, :projects, :roles, :members, :member_roles,
-           :enabled_modules, :issues, :trackers, :enumerations
+           :enabled_modules, :issues, :trackers, :enumerations, :versions
 
   include Redmine::Export::PDF::IssuesPdfHelper
 
