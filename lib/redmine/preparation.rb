117553bb4902a1123a94214e9021884cfbf4e555
Go MAEDA <maeda@farend.jp>
2022-07-31 13:09:09 +0000

    Merged r21741 and r21742 from trunk to 5.0-stable (#33953).
    git-svn-id: https://svn.redmine.org/redmine/branches/5.0-stable@21747 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/preparation.rb
+++ lib/redmine/preparation.rb
@@ -367,7 +367,7 @@ module Redmine
         menu.push :repository,
                   {:controller => 'repositories', :action => 'show',
                    :repository_id => nil, :path => nil, :rev => nil},
-                  :if => Proc.new {|p| p.repository && !p.repository.new_record?}
+                  :if => Proc.new {|p| p.repositories.any? {|r| !r.new_record?}}
         menu.push :settings, {:controller => 'projects', :action => 'settings'},
                   :last => true
       end
