8cbe5014b18dd88c4224a9e579d11ed5e5f6a17b
Go MAEDA <maeda@farend.jp>
2022-09-01 06:47:06 +0000

    Merged r21792 from trunk to 5.0-stable (#36258).
    git-svn-id: https://svn.redmine.org/redmine/branches/5.0-stable@21794 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/repositories_mercurial_controller_test.rb
+++ test/functional/repositories_mercurial_controller_test.rb
@@ -27,7 +27,7 @@ class RepositoriesMercurialControllerTest < Redmine::RepositoryControllerTest
 
   REPOSITORY_PATH = Rails.root.join('tmp/test/mercurial_repository').to_s
   PRJ_ID     = 3
-  NUM_REV    = 40
+  NUM_REV    = 43
 
   def setup
     super
