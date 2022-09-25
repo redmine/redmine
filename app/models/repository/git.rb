f92477a4236d7f8058aa0473b8f0aab63e81a849
Go MAEDA <maeda@farend.jp>
2022-09-25 05:08:32 +0000

    Cleanup Repository::Git#clear_extra_info_of_changesets (#37614).
    Patch by Go MAEDA.
    
    
    git-svn-id: https://svn.redmine.org/redmine/trunk@21845 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/repository/git.rb
+++ app/models/repository/git.rb
@@ -247,9 +247,7 @@ class Repository::Git < Repository
 
     v = extra_info["extra_report_last_commit"]
     write_attribute(:extra_info, nil)
-    h = {}
-    h["extra_report_last_commit"] = v
-    merge_extra_info(h)
+    merge_extra_info({"extra_report_last_commit" => v})
     save(:validate => false)
   end
   private :clear_extra_info_of_changesets
