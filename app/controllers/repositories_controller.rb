bf161ee2335cc39c4e19ef72a9b4674f690d601a
Marius Balteanu <marius.balteanu@zitec.com>
2022-10-02 11:49:11 +0000

    Merge r21883-r21884 from trunk to 5.0-stable (#37626).
    git-svn-id: https://svn.redmine.org/redmine/branches/5.0-stable@21885 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -286,7 +286,8 @@ class RepositoriesController < ApplicationController
       @changeset = @repository.find_changeset_by_name(@rev)
       @changeset_to = @rev_to ? @repository.find_changeset_by_name(@rev_to) : nil
       @diff_format_revisions = @repository.diff_format_revisions(@changeset, @changeset_to)
-      render :diff, :formats => :html, :layout => 'base'
+      # ToDo: Fix DEPRECATION WARNING: Rendering actions with '.' in the name is deprecated
+      render :diff, :formats => :html, :layout => 'base.html.erb'
     end
   end
 
