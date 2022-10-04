9ef056d615453cd9901cf9d0226d444745625f5f
Marius Balteanu <marius.balteanu@zitec.com>
2022-10-04 19:41:07 +0000

    Mark legacy Markdown text formatting option as deprecated (#36807).
    git-svn-id: https://svn.redmine.org/redmine/trunk@21898 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/preparation.rb
+++ lib/redmine/preparation.rb
@@ -395,7 +395,7 @@ module Redmine
 
       WikiFormatting.map do |format|
         format.register :textile
-        format.register :markdown if Object.const_defined?(:Redcarpet)
+        format.register :markdown, label: 'Markdown (deprecated)' if Object.const_defined?(:Redcarpet)
         if Object.const_defined?(:CommonMarker)
           format.register :common_mark, label: 'CommonMark Markdown (GitHub Flavored)'
         end
