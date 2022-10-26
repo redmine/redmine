41bb28cae5e22f292c3e86d8df2ff0d9d9e37243
Go MAEDA <maeda@farend.jp>
2022-10-26 13:19:52 +0000

    Fix RuboCop offense Lint/RedundantRequireStatement (#36919).
    git-svn-id: https://svn.redmine.org/redmine/trunk@21921 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/repository_cvs_test.rb
+++ test/unit/repository_cvs_test.rb
@@ -18,7 +18,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 require File.expand_path('../../test_helper', __FILE__)
-require 'pp'
+
 class RepositoryCvsTest < ActiveSupport::TestCase
   fixtures :projects
 
