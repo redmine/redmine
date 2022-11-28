258d2fe87f91502367d47c6ca1e32ced01dd2c54
Marius Balteanu <marius.balteanu@zitec.com>
2022-11-28 22:53:56 +0000

    Remove unused mention_users permission (#37938).
    git-svn-id: https://svn.redmine.org/redmine/trunk@21966 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/preparation.rb
+++ lib/redmine/preparation.rb
@@ -72,7 +72,6 @@ module Redmine
           map.permission :view_private_notes, {}, :read => true, :require => :member
           map.permission :set_notes_private, {}, :require => :member
           map.permission :delete_issues, {:issues => :destroy}, :require => :member
-          map.permission :mention_users, {}
           # Watchers
           map.permission :view_issue_watchers, {}, :read => true
           map.permission :add_issue_watchers, {:watchers => [:new, :create, :append, :autocomplete_for_user, :autocomplete_for_mention]}
