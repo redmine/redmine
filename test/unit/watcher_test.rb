26c392d3c368af689df8c0c95863e3e0298d462a
Go MAEDA <maeda@farend.jp>
2023-10-15 01:31:29 +0000

    Fix a test failure due to the auto watch on issue creation (#38238).
    Patch by Go MAEDA.
    
    
    git-svn-id: https://svn.redmine.org/redmine/trunk@22342 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/watcher_test.rb
+++ test/unit/watcher_test.rb
@@ -110,7 +110,9 @@ class WatcherTest < ActiveSupport::TestCase
   end
 
   def test_watcher_user_ids_should_make_ids_uniq
-    issue = Issue.new(:project => Project.find(1), :tracker_id => 1, :subject => "test", :author => User.find(2))
+    author = User.find(2)
+    author.pref.auto_watch_on = []
+    issue = Issue.new(:project => Project.find(1), :tracker_id => 1, :subject => "test", :author => author)
     issue.watcher_user_ids = ['1', '3', '1']
     issue.save!
     assert_equal 2, issue.watchers.count
