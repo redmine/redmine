b2b2dff94179f12a7839a7226c2455010484446d
Marius Balteanu <marius.balteanu@zitec.com>
2023-11-26 13:57:25 +0000

    Merged r22478 to r22479from trunk to 5.1-stable (#39714).
    git-svn-id: https://svn.redmine.org/redmine/branches/5.1-stable@22481 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/queries_helper.rb
+++ app/helpers/queries_helper.rb
@@ -26,15 +26,15 @@ module QueriesHelper
     ungrouped = []
     grouped = {label_string: [], label_date: [], label_time_tracking: [], label_attachment: []}
     query.available_filters.map do |field, field_options|
-      if field =~ /^(.+)\./
+      if /^cf_\d+\./.match?(field)
+        group = (field_options[:through] || field_options[:field]).try(:name)
+      elsif field =~ /^(.+)\./
         # association filters
         group = "field_#{$1}".to_sym
       elsif field_options[:type] == :relation
         group = :label_relations
       elsif field_options[:type] == :tree
         group = query.is_a?(IssueQuery) ? :label_relations : nil
-      elsif /^cf_\d+\./.match?(field)
-        group = (field_options[:through] || field_options[:field]).try(:name)
       elsif %w(member_of_group assigned_to_role).include?(field)
         group = :field_assigned_to
       elsif field_options[:type] == :date_past || field_options[:type] == :date
