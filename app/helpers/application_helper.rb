8f3a8c4dbad04588434165c5f4b31e89e22c73eb
Marius Balteanu <marius.balteanu@zitec.com>
2023-12-20 04:40:26 +0000

    Update Rails UJS to 6.1.7.6 (#39875).
    git-svn-id: https://svn.redmine.org/redmine/branches/5.1-stable@22527 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1730,7 +1730,7 @@ module ApplicationHelper
   # Returns the javascript tags that are included in the html layout head
   def javascript_heads
     tags = javascript_include_tag(
-      'jquery-3.6.1-ui-1.13.2-ujs-6.1.7',
+      'jquery-3.6.1-ui-1.13.2-ujs-6.1.7.6',
       'tribute-5.1.3.min',
       'tablesort-5.2.1.min.js',
       'tablesort-5.2.1.number.min.js',
