c17b42509b2a79e53d19facfa00fcc16ec401cf9
Marius Balteanu <marius.balteanu@zitec.com>
2023-12-22 02:08:53 +0000

    Fixes attachments functionality for (custom) plugins broken since fix for CVE-2022-44030 by adding a dynamic routing constraint which can be modified by plugins (#39862).
    Patch by @jkraemer.
    
    git-svn-id: https://svn.redmine.org/redmine/trunk@22551 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/fixtures/plugins/redmine_test_plugin_foo/init.rb
+++ test/fixtures/plugins/redmine_test_plugin_foo/init.rb
@@ -5,6 +5,8 @@ Redmine::Plugin.register :redmine_test_plugin_foo do
   version '0.0.1'
 end
 
+Redmine::Acts::Attachable::ObjectTypeConstraint.register_object_type('plugin_articles')
+
 Pathname(__dir__).glob("app/**/*.rb").sort.each do |path|
   require path
 end
