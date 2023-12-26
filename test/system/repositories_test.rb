5a5c621f43ab70331494e3e84527ba72484b416b
Marius Balteanu <marius.balteanu@zitec.com>
2023-12-26 15:43:25 +0000

    Add system test for #39747.
    git-svn-id: https://svn.redmine.org/redmine/trunk@22562 e93f8b46-1217-0410-a6f0-8f06a7374b81

---
+++ test/system/repositories_test.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+# Redmine - project management software
+# Copyright (C) 2006-2023  Jean-Philippe Lang
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+require_relative '../application_system_test_case'
+
+class RepositoriesTest < ApplicationSystemTestCase
+
+  fixtures :projects, :users, :email_addresses, :roles, :members, :member_roles, :enabled_modules,
+           :repositories, :issues, :issue_statuses, :changesets, :changes,
+           :issue_categories, :enumerations, :custom_fields, :custom_values, :trackers
+
+  def setup
+    @project = Project.find(1)
+    @repository = Repository::Subversion.create(:project => @project,
+                                                :url => self.class.subversion_repository_url)
+    assert @repository
+  end
+
+  if repository_configured?('subversion')
+    def test_revision_diff_for_javascript_file_should_render_layout
+      log_user('admin', 'admin')
+
+      visit('projects/ecookbook/repository')
+      click_link('Revision 16', match: :first)
+
+      click_link('diff')
+
+      # Assert page is rendered using base layout
+      assert page.has_text?("eCookbook")
+      assert page.has_css?("div[id=main-menu]")
+      assert page.has_css?("a.administration")
+    end
+  end
+end
