a94d869db3256fb479b3c11ac35dc587a11f3eee
silentm0de <rytis@wisemonks.com>
2024-05-16 08:51:08 +0300

    time_entries page bugfix

---
+++ plugins/redmine_spent_time/app/helpers/timelog_helper.rb
@@ -0,0 +1,18 @@
+
+module TimelogHelper
+  include ApplicationHelper
+
+  def render_timelog_breadcrumb
+    links = []
+    links << link_to(l(:label_project_all), {:project_id => nil, :issue_id => nil})
+    links << link_to(h(@project), {:project_id => @project, :issue_id => nil}) if @project
+    if @issue
+      if @issue.visible?
+        links << link_to_issue(@issue, :subject => false)
+      else
+        links << "##{@issue.id}"
+      end
+    end
+    breadcrumb links
+  end
+end
