5159d31a8bd83da1f7c587634cacdf8faa44555d
Go MAEDA <maeda@farend.jp>
2024-06-05 07:55:45 +0000

    Remove unsupported :after option from add_column in AddQueriesDescription migration (#9309).
    git-svn-id: https://svn.redmine.org/redmine/trunk@22859 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- db/migrate/20240213101801_add_queries_description.rb
+++ db/migrate/20240213101801_add_queries_description.rb
@@ -1,6 +1,6 @@
 class AddQueriesDescription < ActiveRecord::Migration[7.1]
   def up
-    add_column :queries, :description, :string, :after => :name
+    add_column :queries, :description, :string
   end
 
   def down
