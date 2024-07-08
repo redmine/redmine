f9f486bdd091fcfdc351b188981688dccffed210
Marius Balteanu <marius.balteanu@zitec.com>
2024-07-08 21:30:36 +0000

    Check view watchers permission when copying issues (#40946).
    Patch by Jens Kraemer (@jkraemer).
    
    
    git-svn-id: https://svn.redmine.org/redmine/trunk@22914 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -314,9 +314,9 @@ class Issue < ApplicationRecord
         attachement.copy(:container => self)
       end
     end
+
     unless options[:watchers] == false
-      self.watcher_user_ids =
-        issue.watcher_users.select{|u| u.status == User::STATUS_ACTIVE}.map(&:id)
+      self.watcher_user_ids = issue.visible_watcher_users.select{|u| u.status == User::STATUS_ACTIVE}.map(&:id)
     end
     @copied_from = issue
     @copy_options = options
