000af6668c694a9986ada71cb4761e9a76a19c7c
silentm0de <rytis@wisemonks.com>
2024-07-31 08:56:30 +0300

    Add DataStringIO class

---
+++ plugins/mail_tracker/lib/data_string_io.rb
@@ -0,0 +1,10 @@
+class DataStringIO < StringIO
+  attr_accessor :content_type, :original_filename
+
+  def initialize filename, content_type, data
+    super data
+
+    self.original_filename = filename
+    self.content_type = content_type
+  end
+end
