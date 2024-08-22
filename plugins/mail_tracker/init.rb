c4c7c62d154fc538a4732923fa0c86d74fda82b2
silentm0de <rytis@wisemonks.com>
2024-08-22 18:02:26 +0300

    Add sla translations and issue tracking permissions

--- plugins/mail_tracker/init.rb
+++ plugins/mail_tracker/init.rb
@@ -7,6 +7,13 @@ Redmine::Plugin.register :mail_tracker do
   settings :default => {
     :allowed_users => User.table_exists? ? User.where(["users.login IS NOT NULL AND users.login <> ''"]).collect {|x| x.id.to_s} : [] },
     :partial => 'settings/mail_tracker_settings'
+  project_module :issue_tracking do
+    permission :edit_after_close_issues, {:issues => [:edit, :update, :bulk_edit, :bulk_update], :journals => [:new], :attachments => :upload}
+    permission :view_only_watcher_issues, {:issues => [:edit, :update, :bulk_edit, :bulk_update], :journals => [:new], :attachments => :upload}
+  end
+  project_module :project_management do
+    permission :edit_project_email, {:projects => :settings, :project_emails => [:update, :destroy, :watchers]}
+  end
 end
 
 Proc.new do
