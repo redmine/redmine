9dae7a75f595c4d96abc8efb8671f4bb198a7735
Go MAEDA <maeda@farend.jp>
2024-09-23 01:33:40 +0000

    Gantt progress line misrendering for 0% progress issues/versions with future start dates beyond chart range (#41263).
    Contributed by Minoru Maeda (user:maeda-m).
    
    
    git-svn-id: https://svn.redmine.org/redmine/trunk@23068 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- lib/redmine/helpers/gantt.rb
+++ lib/redmine/helpers/gantt.rb
@@ -723,7 +723,7 @@ module Redmine
             progress_date = calc_progress_date(issue.start_date,
                                                issue.due_before, issue.done_ratio)
             css_classes << ' behind-start-date' if progress_date < self.date_from
-            css_classes << ' over-end-date' if progress_date > self.date_to
+            css_classes << ' over-end-date' if progress_date > self.date_to && issue.done_ratio > 0
           end
           s = (+"").html_safe
           s << view.sprite_icon('issue').html_safe
@@ -744,7 +744,7 @@ module Redmine
             progress_date = calc_progress_date(version.start_date,
                                                version.due_date, version.visible_fixed_issues.completed_percent)
             html_class << ' behind-start-date' if progress_date < self.date_from
-            html_class << ' over-end-date' if progress_date > self.date_to
+            html_class << ' over-end-date' if progress_date > self.date_to && version.visible_fixed_issues.completed_percent > 0
           end
           s = (+"").html_safe
           s << view.sprite_icon('package').html_safe
