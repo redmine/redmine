f6b835119fe3c87b7e77a9ed6c4283c1f83643ce
Go MAEDA <maeda@farend.jp>
2024-10-02 23:46:17 +0000

    Update MiniMagick to 5.0 (#41312).
    Patch by Katsuya HIDAKA (user:hidakatsuya).
    
    
    git-svn-id: https://svn.redmine.org/redmine/trunk@23099 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- Gemfile
+++ Gemfile
@@ -41,7 +41,7 @@ end
 
 # Optional gem for exporting the gantt to a PNG file
 group :minimagick do
-  gem 'mini_magick', '~> 4.13.0'
+  gem 'mini_magick', '~> 5.0.1'
 end
 
 # Optional Markdown support
--- lib/redmine/helpers/gantt.rb
+++ lib/redmine/helpers/gantt.rb
@@ -394,11 +394,11 @@ module Redmine
         font_path =
           Redmine::Configuration['minimagick_font_path'].presence ||
             Redmine::Configuration['rmagick_font_path'].presence
-        img = MiniMagick::Image.create(".#{format}", false)
+        img = MiniMagick::Image.create(".#{format}")
         if Redmine::Configuration['imagemagick_convert_command'].present?
           MiniMagick.cli_path = File.dirname(Redmine::Configuration['imagemagick_convert_command'])
         end
-        MiniMagick::Tool::Convert.new do |gc|
+        MiniMagick.convert do |gc|
           gc.size('%dx%d' % [subject_width + g_width + 1, height])
           gc.xc('white')
           gc.font(font_path) if font_path.present?
