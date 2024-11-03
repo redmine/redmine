bb0e2114a0977a04fc3579f22e5f9eae58a0dee4
Marius Balteanu <marius.balteanu@zitec.com>
2024-11-03 11:53:53 +0000

    Adds test for #41678 (#23954).
    Patch by Mizuki ISHIKAWA (user:ishikawa999).
    
    
    git-svn-id: https://svn.redmine.org/redmine/trunk@23186 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/project_query_test.rb
+++ test/unit/project_query_test.rb
@@ -26,7 +26,8 @@ class ProjectQueryTest < ActiveSupport::TestCase
            :groups_users,
            :enabled_modules,
            :custom_fields, :custom_values,
-           :queries
+           :queries,
+           :issues, :journals, :journal_details
 
   include Redmine::I18n
 
@@ -148,5 +149,17 @@ class ProjectQueryTest < ActiveSupport::TestCase
     q = ProjectQuery.new
     q.admin_projects = 1
     assert_equal Project.all, q.base_scope
+  end
+
+  def test_results_scope_has_last_activity_date
+    q = ProjectQuery.generate!(column_names: [:last_activity_date])
+    result_projects = q.results_scope({})
+
+    assert_kind_of ActiveRecord::Relation, result_projects
+    assert_equal Project, result_projects.klass
+
+    last_activitiy_date = result_projects.find{|p| p.id == 1}.instance_variable_get(:@last_activity_date)
+    assert_not_nil last_activitiy_date
+    assert_equal Redmine::Activity::Fetcher.new(User.current).events(nil, nil, :project => Project.find(1)).first.updated_on, last_activitiy_date
   end
 end
