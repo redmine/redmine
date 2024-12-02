935df66094332d509a2021de86966a7d88ec6ab0
Go MAEDA <maeda@farend.jp>
2024-12-02 00:18:36 +0000

    Fix random test failure in UserTest#test_validate_password_complexity due to missing explicit language setting (#41914).
    Patch by Go MAEDA (user:maeda).
    
    
    git-svn-id: https://svn.redmine.org/redmine/trunk@23345 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -564,6 +564,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   def test_validate_password_complexity
+    set_language_if_valid 'en'
     user = users(:users_002)
     bad_passwords = [
       user.login,
