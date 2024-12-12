065b858d4e3451ecfe2a29b7ce75f10b84cc6cbe
RokasAbrutis <rokas.abrutis@gmail.com>
2024-12-12 16:41:25 +0200

    14386 supply fields show up when changed to supply

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -165,7 +165,7 @@ class IssuesController < ApplicationController
   def new
     respond_to do |format|
       format.html {render :action => 'new', :layout => !request.xhr?}
-      format.js
+      format.js { render 'issues/new.js.erb', :layout => false }
     end
   end
 
