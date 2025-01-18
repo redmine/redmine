f27570120b7a672249bfebfe4d62da506785e146
Go MAEDA <maeda@farend.jp>
2025-01-18 04:59:40 +0000

    Explicitly load 'logger' to avoid NameError with concurrent-ruby 1.3.5 in 5.0-stable (#42113).
    git-svn-id: https://svn.redmine.org/redmine/branches/5.0-stable@23444 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- config/boot.rb
+++ config/boot.rb
@@ -4,3 +4,7 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+
+# Explicitly load 'logger' to avoid NameError with concurrent-ruby 1.3.5.
+# Reference: https://github.com/rails/rails/issues/54272
+require 'logger'
