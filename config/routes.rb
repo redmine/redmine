1557701e17103098c65fe82665b29be8d2cc3b3a
Go MAEDA <maeda@farend.jp>
2025-02-04 09:54:57 +0000

    Fix RuboCop Style/HashSyntax (#42008).
    git-svn-id: https://svn.redmine.org/redmine/trunk@23488 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- config/routes.rb
+++ config/routes.rb
@@ -414,7 +414,7 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get "up" => "rails/health#show", :as => :rails_health_check
 
   Redmine::Plugin.directory.glob("*/config/routes.rb").sort.each do |plugin_routes_path|
     instance_eval(plugin_routes_path.read, plugin_routes_path.to_s)
