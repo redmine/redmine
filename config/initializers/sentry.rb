a2b61745e332ec0da24d9ac938fdae4bec630ef5
rokas.abrutis <rokas.abrutis@gmail.com>
2025-02-12 14:54:57 +0200

    turn off sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,7 @@
 Sentry.init do |config|
   config.dsn = 'https://41a3267f75c64181be164a26e9c327c3:9da66cee28ae4d5ca309b0bc40ee5333@sentry.io/157674'
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
-  config.enabled_environments = %w[production]
+  config.enabled_environments = %w[]
 
   # Set tracesSampleRate to 1.0 to capture 100%
   # of transactions for performance monitoring.
