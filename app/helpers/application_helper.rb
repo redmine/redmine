f8292b56dfae14da2af7fe765f97015f1559e0fa
Marius Balteanu <marius.balteanu@zitec.com>
2025-03-26 20:45:43 +0000

    Replaces legacy icons in flash notices with SVG icons (#41952).
    git-svn-id: https://svn.redmine.org/redmine/trunk@23562 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -518,7 +518,7 @@ module ApplicationHelper
   def render_flash_messages
     s = +''
     flash.each do |k, v|
-      s << content_tag('div', v.html_safe, :class => "flash #{k}", :id => "flash_#{k}")
+      s << content_tag('div', notice_icon(k) + v.html_safe, :class => "flash #{k}", :id => "flash_#{k}")
     end
     s.html_safe
   end
