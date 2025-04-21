0f95ff4acd0c206be892371332197913e573f0a4
Go MAEDA <maeda@farend.jp>
2025-04-21 08:01:45 +0000

    Skip some Mercurial tests on 6.0-stable branch when Mercurial 5.1 or later is used (#42597).
    git-svn-id: https://svn.redmine.org/redmine/branches/6.0-stable@23700 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/repository_mercurial_test.rb
+++ test/unit/repository_mercurial_test.rb
@@ -165,6 +165,10 @@ class RepositoryMercurialTest < ActiveSupport::TestCase
     end
 
     def test_fetch_changesets_from_scratch
+      # This test fails when using Mercurial >= 5.1 due to a change in behavior.
+      # See https://repo.mercurial-scm.org/hg/rev/0c72eddb4be5 for details.
+      skip "Test skipped because Mercurial >= 5.1 is used" if @repository.scm.class.client_version_above?([5, 1])
+
       assert_equal 0, @repository.changesets.count
       @repository.fetch_changesets
       @project.reload
@@ -248,6 +252,10 @@ class RepositoryMercurialTest < ActiveSupport::TestCase
     end
 
     def test_latest_changesets
+      # This test fails when using Mercurial >= 5.1 due to a change in behavior.
+      # See https://repo.mercurial-scm.org/hg/rev/0c72eddb4be5 for details.
+      skip "Test skipped because Mercurial >= 5.1 is used" if @repository.scm.class.client_version_above?([5, 1])
+
       assert_equal 0, @repository.changesets.count
       @repository.fetch_changesets
       @project.reload
