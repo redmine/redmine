3644cb5cf27fbdc7c6cf26c2974f8bf3618e42e8
Go MAEDA <maeda@farend.jp>
2025-04-27 08:18:02 +0000

    Merged r23712 from trunk to 5.1-stable (#42622).
    git-svn-id: https://svn.redmine.org/redmine/branches/5.1-stable@23721 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/models/user.rb
+++ app/models/user.rb
@@ -87,8 +87,8 @@ class User < Principal
                           :after_remove => Proc.new {|user, group| group.user_removed(user)}
   has_many :changesets, :dependent => :nullify
   has_one :preference, :dependent => :destroy, :class_name => 'UserPreference'
-  has_one :atom_token, lambda {where "action='feeds'"}, :class_name => 'Token'
-  has_one :api_token, lambda {where "action='api'"}, :class_name => 'Token'
+  has_one :atom_token, lambda {where "#{table.name}.action='feeds'"}, :class_name => 'Token'
+  has_one :api_token, lambda {where "#{table.name}.action='api'"}, :class_name => 'Token'
   has_one :email_address, lambda {where :is_default => true}, :autosave => true
   has_many :email_addresses, :dependent => :delete_all
   belongs_to :auth_source
