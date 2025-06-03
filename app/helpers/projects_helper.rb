674d564045f83c33fece65b9d0f903c27f8496d2
ishikawa999 <remember929@gmail.com>
2025-06-03 08:31:37 +0000

    Fix project list icons

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -80,8 +80,8 @@ module ProjectsHelper
       classes += %w(icon icon-bookmarked-project) if bookmarked_project_ids.include?(project.id)
 
       s = link_to_project(project, {}, :class => classes.uniq.join(' '))
-      s << sprite_icon('user', l(:label_my_projects), icon_only: true) if User.current.member_of?(project)
-      s << sprite_icon('bookmarked', l(:label_my_bookmarks), icon_only: true) if bookmarked_project_ids.include?(project.id)
+      s << tag.span(sprite_icon('user', l(:label_my_projects), icon_only: true), class: 'icon-only icon-user my-project') if User.current.member_of?(project)
+      s << tag.span(sprite_icon('bookmarked', l(:label_my_bookmarks), icon_only: true), class: 'icon-only icon-bookmarked-project') if bookmarked_project_ids.include?(project.id)
       if project.description.present?
         s << content_tag('div', textilizable(project.short_description, :project => project), :class => 'wiki description')
       end
