686c04dd8559eaafd52dc38803250071b86ed1ed
Marius Balteanu <marius.balteanu@zitec.com>
2025-08-13 05:26:20 +0000

    Remove redundant fixtures declaration from OauthProviderSystemTest (#43125, 24808).
    Patch by Katsuya HIDAKA (user:hidakatsuya).
    
    git-svn-id: https://svn.redmine.org/redmine/trunk@23914 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/system/oauth_provider_test.rb
+++ test/system/oauth_provider_test.rb
@@ -6,11 +6,6 @@ require 'rack'
 require 'puma'
 
 class OauthProviderSystemTest < ApplicationSystemTestCase
-  fixtures :projects, :users, :email_addresses, :roles, :members, :member_roles,
-           :trackers, :projects_trackers, :enabled_modules, :issue_statuses, :issues,
-           :enumerations, :custom_fields, :custom_values, :custom_fields_trackers,
-           :watchers, :journals, :journal_details, :versions,
-           :workflows
 
   test 'application creation and authorization' do
     #
