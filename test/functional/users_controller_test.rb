f3b528cd51b247397ed4bd5215878b7094152c37
Marius Balteanu <marius.balteanu@zitec.com>
2025-09-07 06:24:07 +0000

    Merged r23941, r23942, r23944 and r23947 from trunk to 6.0-stable (#42998).
    git-svn-id: https://svn.redmine.org/redmine/branches/6.0-stable@23948 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -438,6 +438,8 @@ class UsersControllerTest < Redmine::ControllerTest
   def test_new
     get :new
     assert_response :success
+    assert_includes @response.headers['Cache-Control'], 'no-store'
+
     assert_select 'input[name=?]', 'user[login]'
     assert_select 'label[for=?]>span.required', 'user_password', 1
   end
@@ -554,6 +556,8 @@ class UsersControllerTest < Redmine::ControllerTest
       post :create, :params => {:user => {:login => 'foo'}}
     end
     assert_response :success
+    assert_includes @response.headers['Cache-Control'], 'no-store'
+
     assert_select_error /Email cannot be blank/
   end
 
@@ -640,6 +644,8 @@ class UsersControllerTest < Redmine::ControllerTest
     end
 
     assert_response :success
+    assert_includes @response.headers['Cache-Control'], 'no-store'
+
     assert_select 'h2>a+img.gravatar'
     assert_select 'input[name=?][value=?]', 'user[login]', 'jsmith'
     assert_select 'label[for=?]>span.required', 'user_password', 0
@@ -695,6 +701,8 @@ class UsersControllerTest < Redmine::ControllerTest
       }
     end
     assert_response :success
+    assert_includes @response.headers['Cache-Control'], 'no-store'
+
     assert_select_error /First name cannot be blank/
   end
 
