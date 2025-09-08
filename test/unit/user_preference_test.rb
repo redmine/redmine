48e2621fa66e05466460713bafb7ef59805e8df6
Marius Balteanu <marius.balteanu@zitec.com>
2025-09-08 06:12:16 +0000

    Fixes failing test introduced in r23956 (#42880).
    git-svn-id: https://svn.redmine.org/redmine/trunk@23958 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/user_preference_test.rb
+++ test/unit/user_preference_test.rb
@@ -67,10 +67,8 @@ class UserPreferenceTest < ActiveSupport::TestCase
   end
 
   def test_auto_watch_on_should_default_to_options
-    with_settings :default_users_auto_watch_on => nil do
-      preference = UserPreference.new
-      assert_equal %w[issue_created issue_contributed_to], preference.auto_watch_on
-    end
+    preference = UserPreference.new
+    assert_equal %w[issue_created issue_contributed_to], preference.auto_watch_on
   end
 
   def test_create
