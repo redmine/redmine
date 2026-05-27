92ff4b12d195fbba81aa15849bd62efbbf9284cb
Go MAEDA <maeda@farend.jp>
2026-05-27 01:07:39 +0000

    Persist default `default_issue_start_date_to_creation_date` setting for new installs (#43895).
    git-svn-id: https://svn.redmine.org/redmine/trunk@24704 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- db/migrate/017_create_settings.rb
+++ db/migrate/017_create_settings.rb
@@ -9,6 +9,11 @@ class CreateSettings < ActiveRecord::Migration[4.2]
     Setting.create!(name: 'default_notification_option', value: Setting.default_notification_option)
     Setting.create!(name: 'text_formatting', value: Setting.text_formatting)
     Setting.create!(name: 'wiki_tablesort_enabled', value: Setting.wiki_tablesort_enabled)
+
+    # `default_issue_start_date_to_creation_date` should also be inserted here
+    # for new installations, but its name exceeds the 30-character limit of the
+    # settings.name column at this point. It is inserted after the column is
+    # extended in 20090318181151_extend_settings_name.rb.
   end
 
   def self.down
--- db/migrate/20090318181151_extend_settings_name.rb
+++ db/migrate/20090318181151_extend_settings_name.rb
@@ -1,6 +1,15 @@
 class ExtendSettingsName < ActiveRecord::Migration[4.2]
   def self.up
     change_column :settings, :name, :string, :limit => 255, :default => '', :null => false
+
+    # This setting is a default setting for new installations. It should be
+    # inserted in 017_create_settings.rb with the other default settings, but
+    # its name exceeds the original 30-character limit of the settings.name
+    # column.
+    Setting.create!(
+      :name => 'default_issue_start_date_to_creation_date',
+      :value => Setting.default_issue_start_date_to_creation_date
+    )
   end
 
   def self.down
