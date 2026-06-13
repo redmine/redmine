30c5af9d53526fdb0c14bb2093db2d5adb4da6df
Marius Balteanu <marius.balteanu@zitec.com>
2026-06-13 17:52:16 +0000

    Merge r24728, r24730 from trunk to 6.1-stable (#43951).
    git-svn-id: https://svn.redmine.org/redmine/branches/6.1-stable@24750 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -234,6 +234,11 @@ class AttachmentsController < ApplicationController
   end
 
   def find_downloadable_attachments
+    if @container.respond_to?(:attachments_visible?) && !@container.attachments_visible?
+      deny_access
+      return
+    end
+
     @attachments = @container.attachments.select(&:readable?)
     bulk_download_max_size = Setting.bulk_download_max_size.to_i.kilobytes
     if @attachments.sum(&:filesize) > bulk_download_max_size
