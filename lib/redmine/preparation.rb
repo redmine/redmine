redmine/redmine

commit 6b559326fa48f0d5e4ea1cab1ec633205b63b940

Show repositories tab only if there are enabled repositories (#43209).
Patch Holger Just (user:hjust).


git-svn-id: https://svn.redmine.org/redmine/trunk@24865 e93f8b46-1217-0410-a6f0-8f06a7374b81

lib/redmine/preparation.rb
@@ -393,7 +393,11 @@ module Redmine
         menu.push :repository,
                   {:controller => 'repositories', :action => 'show',
                    :repository_id => nil, :path => nil, :rev => nil},
-                  :if => Proc.new {|p| p.repositories.exists?}
+                  :if => Proc.new do |p|
+                    repository_types = p.repositories.pluck(:type)
+                    enabled_types = Setting.enabled_scm.map { |name| "Repository::#{name}" }
+                    (repository_types & enabled_types).any?
+                  end
         menu.push :settings, {:controller => 'projects', :action => 'settings'},
                   :last => true
       end

test/integration/lib/redmine/menu_manager_test.rb
@@ -72,7 +72,9 @@ class MenuManagerTest < Redmine::IntegrationTest
     end
   end
 
-  def test_project_menu_should_display_repository_tab_when_exists_repository
+  def test_project_menu_should_display_repository_tab_when_enabled_repository
+    skip "Subversion is unavailable" unless Repository::Subversion.scm_available
+
     project = Project.find('ecookbook')
     repos = project.repositories
     assert_equal true, repos.exists?
@@ -83,6 +85,13 @@ class MenuManagerTest < Redmine::IntegrationTest
     get '/projects/ecookbook'
     assert_select '#main-menu' do
       assert_select 'a.repository', :count => 1
+    end
+
+    with_settings :enabled_scm => ['Filesystem'] do
+      get '/projects/ecookbook'
+      assert_select '#main-menu' do
+        assert_select 'a.repository', :count => 0
+      end
     end
 
     repos.update_all(:is_default => false)

commit c5a4e9722c9eecde170a8de195a49fa7d08f3961

Fixes "'Proc.new': tried to create Proc object without a block (ArgumentError)" error introduced by r24865 (#43209).
git-svn-id: https://svn.redmine.org/redmine/trunk@24866 e93f8b46-1217-0410-a6f0-8f06a7374b81

lib/redmine/preparation.rb
@@ -393,11 +393,11 @@ module Redmine
         menu.push :repository,
                   {:controller => 'repositories', :action => 'show',
                    :repository_id => nil, :path => nil, :rev => nil},
-                  :if => Proc.new do |p|
+                  :if => Proc.new { |p|
                     repository_types = p.repositories.pluck(:type)
                     enabled_types = Setting.enabled_scm.map { |name| "Repository::#{name}" }
                     (repository_types & enabled_types).any?
-                  end
+                  }
         menu.push :settings, {:controller => 'projects', :action => 'settings'},
                   :last => true
       end