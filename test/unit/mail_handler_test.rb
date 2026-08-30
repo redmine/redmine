681b0b84c1b3641c927bd80368d002d90a187384
Go MAEDA <maeda@farend.jp>
2026-08-30 06:23:07 +0000

    Merged r25018 from trunk into 7.0-stable (#44392).
    git-svn-id: https://svn.redmine.org/redmine/branches/7.0-stable@25021 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- test/unit/mail_handler_test.rb
+++ test/unit/mail_handler_test.rb
@@ -907,7 +907,7 @@ class MailHandlerTest < ActiveSupport::TestCase
       "Auto-Submitted: auto-generated",
       'X-Autoreply: yes'
     ].each do |header|
-      raw = IO.read(File.join(FIXTURES_PATH, 'ticket_on_given_project.eml'))
+      raw = File.read(File.join(FIXTURES_PATH, 'ticket_on_given_project.eml'))
       raw = header + "\n" + raw
 
       assert_no_difference 'Issue.count' do
@@ -920,7 +920,7 @@ class MailHandlerTest < ActiveSupport::TestCase
     [
       "Auto-Submitted: auto-forwarded"
     ].each do |header|
-      raw = IO.read(File.join(FIXTURES_PATH, 'ticket_on_given_project.eml'))
+      raw = File.read(File.join(FIXTURES_PATH, 'ticket_on_given_project.eml'))
       raw = header + "\n" + raw
 
       assert_difference 'Issue.count', 1 do
@@ -1493,7 +1493,7 @@ class MailHandlerTest < ActiveSupport::TestCase
   private
 
   def submit_email(filename, options={})
-    raw = IO.read(File.join(FIXTURES_PATH, filename))
+    raw = File.read(File.join(FIXTURES_PATH, filename))
     yield raw if block_given?
     MailHandler.receive(raw, options)
   end
